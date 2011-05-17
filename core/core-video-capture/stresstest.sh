3f988a0e24ecc83d2a27cccbab260fa532794908
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2011-05-17 23:26:31 +0000

    removed stresstest script

--- core/core-video-capture/stresstest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for i in {1..100}
-do
-    echo $i
-    java -cp ../core-feature/target/classes:../core-image/target/classes:../core-math/target/classes:../core-video-capture/target/classes:../core-video/target/classes:../core/target/classes:.target/classes/:/Users/jsh2/.m2/repository/com/nativelibs4java/bridj/0.4.1/bridj-0.4.1.jar:/Users/jsh2/.m2/repository/log4j/log4j/1.2.8/log4j-1.2.8.jar org.openimaj.video.Test
-done
