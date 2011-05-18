693425606b46a93ff3eddba50cc6ed7e4ac78cd3
Sina Samangooei <sinjax@gmail.com>
2011-05-18 17:16:22 +0000

    windows native grabber code

---
+++ core/core-video-capture/src-native/win64/OpenIMAJGrabber/OpenIMAJGrabber.h
@@ -0,0 +1,67 @@
+/*
+ *  OpenIMAJGrabber.h
+ *  OpenIMAJGrabber
+ *
+ *  Created by Jonathon Hare on 14/05/2011.
+ *  Copyright 2011 University of Southampton. All rights reserved.
+ *
+ */
+
+#ifndef OpenIMAJGrabber_
+#define OpenIMAJGrabber_
+
+#include "videoInput.h"
+
+class Device {
+    private:
+        const char* name;
+        const char* identifier;
+    public:
+        __declspec(dllexport) Device(const char* name, const char* identifier);
+        __declspec(dllexport) ~Device();
+        __declspec(dllexport) const char* getName();
+        __declspec(dllexport) const char* getIdentifier();
+};
+
+class DeviceList {
+private:
+    int nDevices;
+    Device** devices;
+public:
+    __declspec(dllexport) DeviceList(Device ** devices, int nDevices);
+    __declspec(dllexport) ~DeviceList();
+    __declspec(dllexport) int getNumDevices();
+    __declspec(dllexport) Device * getDevice(int i);
+};
+
+struct videoData{
+	int height, width, size;
+	videoInput * VI;
+	int device;
+	unsigned char * buffer;
+};
+
+class OpenIMAJGrabber
+{
+    public:
+        __declspec(dllexport) OpenIMAJGrabber();
+        __declspec(dllexport) OpenIMAJGrabber(Device * device);
+        __declspec(dllexport) ~OpenIMAJGrabber();
+
+		__declspec(dllexport)  DeviceList * getVideoDevices();
+        
+        __declspec(dllexport) unsigned char* getImage();
+        __declspec(dllexport) void nextFrame();
+        __declspec(dllexport) bool startSession(int width, int height,Device * device);
+		__declspec(dllexport) bool startSession(int width, int height);
+        __declspec(dllexport) void stopSession();
+        
+        __declspec(dllexport) int getWidth();
+        __declspec(dllexport) int getHeight();
+    
+    private:
+		void* data;
+        
+};
+
+#endif
