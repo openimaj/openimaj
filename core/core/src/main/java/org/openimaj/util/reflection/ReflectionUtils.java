21e83f57d2623e08b1458617e8c215f93b6a576d
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2011-07-21 17:12:54 +0000

    Added more reflection helpers

---
+++ core/core/src/main/java/org/openimaj/util/reflection/ReflectionUtils.java
@@ -0,0 +1,42 @@
+package org.openimaj.util.reflection;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+/**
+ * Utility methods for java reflection
+ * 
+ * @author Jonathon Hare <jsh2@ecs.soton.ac.uk>
+ *
+ */
+public class ReflectionUtils {
+	/**
+	 * Test if a class has a "public static main(String [] args)" method 
+	 * @param clz the class
+	 * @return true if main method exists; false otherwise
+	 */
+	public static boolean hasMain(Class<?> clz) {
+		return getMain(clz) != null;
+	}
+	
+	/**
+	 * Get the "public static main(String [] args)" method of the class,
+	 * or null if it doesn't have one.
+	 * 
+	 * @param clz the class
+	 * @return the main method, or null if it doesn't exist
+	 */
+	public static Method getMain(Class<?> clz) {
+		try {
+			Method m =  clz.getMethod("main", String[].class);
+			
+			if (m.isAccessible() && 
+				Modifier.isPublic(m.getModifiers()) && 
+				Modifier.isStatic(m.getModifiers()))
+				return m;
+		} catch (NoSuchMethodException e1) {
+			//do nothing
+		}
+		return null;
+	}
+}
