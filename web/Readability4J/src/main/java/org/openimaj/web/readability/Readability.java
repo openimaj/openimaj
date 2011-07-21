65a9c7933138ad11a2306f4daad4f4c243870b9b
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2011-07-21 18:15:24 +0000

    added licensing info. fixed ignores.

--- web/Readability4J/src/main/java/org/openimaj/web/readability/Readability.java
+++ web/Readability4J/src/main/java/org/openimaj/web/readability/Readability.java
@@ -1,10 +1,18 @@
-/*
- * Readability. An Arc90 Lab Experiment. 
- * Website: http://lab.arc90.com/experiments/readability
- * Source:  http://code.google.com/p/arc90labs-readability
+/**
+ * Copyright 2010 The University of Southampton, Yahoo Inc., and the
+ * individual contributors. All rights reserved.
  *
- * Copyright (c) 2009 Arc90 Inc
- * Readability is licensed under the Apache License, Version 2.0.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.openimaj.web.readability;
 
