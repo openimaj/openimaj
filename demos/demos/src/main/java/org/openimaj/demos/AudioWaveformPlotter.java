75034dc4bc58215baab9149db4e924416f815281
David Duplaw <david.dupplaw@wds.co>
2011-07-21 17:43:10 +0000

    Updated AudioWaveformPlotter to use a standard technique for displaying that takes the maximum over a window in the audio. The OpenIMAJ polygon plotter is slow but it works.

--- demos/demos/src/main/java/org/openimaj/demos/AudioWaveformPlotter.java
+++ demos/demos/src/main/java/org/openimaj/demos/AudioWaveformPlotter.java
@@ -3,15 +3,19 @@
  */
 package org.openimaj.demos;
 
+import gnu.trove.TIntArrayList;
+
 import java.io.File;
 import java.nio.ShortBuffer;
+import java.util.ArrayList;
 
 import org.openimaj.audio.SampleChunk;
 import org.openimaj.audio.processor.AudioProcessor;
 import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.MBFImage;
-import org.openimaj.image.colour.RGBColour;
+import org.openimaj.math.geometry.point.Point2d;
 import org.openimaj.math.geometry.point.Point2dImpl;
+import org.openimaj.math.geometry.shape.Polygon;
 import org.openimaj.video.xuggle.XuggleAudio;
 
 /**
@@ -23,70 +27,215 @@ import org.openimaj.video.xuggle.XuggleAudio;
  */
 public class AudioWaveformPlotter
 {
+	/**
+	 * 	Generates an audio overview. This is a lower-resolution version of
+	 * 	the audio waveform. It takes the maximum value from a set of
+	 * 	values and stores this as the overview. By default the processor
+	 * 	takes the maximum value from every 5000 samples.  The method
+	 * 	{@link #getAudioOverview(int, int)} allows resampling of that 
+	 * 	overview.
+	 *
+	 *	@author David Dupplaw <dpd@ecs.soton.ac.uk>
+	 *  @created 21 Jul 2011
+	 *	@version $Author$, $Revision$, $Date$
+	 */
+	public static class AudioOverviewGenerator extends AudioProcessor
+	{
+    	/** Number of bins in the overview */
+    	private int nSamplesPerBin = 5000;
+
+    	/** The maximum in the current bin for each channel */
+    	private int[] channelMax = null;
+    	
+    	/** The number of samples so far in the current bin being processed */
+    	private int nSamplesInBin = 0;
+    	
+    	/** The overview data */
+    	private TIntArrayList[] audioOverview = null;
+
+    	/** The number of channels in the audio stream */
+		private int nChannels = 0;
+    	
+    	/**
+    	 * 
+    	 *	@param nSamplesPerBin
+    	 *	@param nChannels
+    	 */
+    	public AudioOverviewGenerator( int nSamplesPerBin, int nChannels )
+		{
+			this.nSamplesPerBin = nSamplesPerBin;
+			this.nChannels = nChannels;
+			this.audioOverview = new TIntArrayList[nChannels];
+			this.channelMax = new int[nChannels];
+			
+			for( int i = 0; i < nChannels; i++ )
+				this.audioOverview[i] = new TIntArrayList();
+		}
+
+    	/**
+    	 *	@inheritDoc
+    	 * 	@see org.openimaj.audio.processor.AudioProcessor#process(org.openimaj.audio.SampleChunk)
+    	 */
+		@Override
+		public SampleChunk process( SampleChunk samples )
+		{
+			// The number of samples (per channel) in this sample chunk
+			int nSamples = samples.getSamples().length/nChannels/2;
+			
+			// Get the sample data
+			ShortBuffer b = samples.getSamplesAsByteBuffer().asShortBuffer();
+			
+			for( int x = 0; x < nSamples; x++ )			
+			{
+				for( int c = 0; c < nChannels; c++ )
+				{
+					// Store the maximum for the current bin
+					channelMax[c] = Math.max( channelMax[c], 
+							b.get(x*nChannels+c) );
+				}
+
+				// If we're still within the bin
+				if( nSamplesInBin < nSamplesPerBin )
+					nSamplesInBin++;
+				else
+				{
+					// We've overflowed the bin
+					for( int c = 0; c < nChannels; c++ )
+					{
+						// Store the current bin
+						audioOverview[c].add( channelMax[c] );
+						channelMax[c] = Integer.MIN_VALUE;
+					}
+					
+					// Reset for the next bin
+					nSamplesInBin = 0;
+				}
+			}
+			
+			return samples;
+		}
+		
+		/**
+		 * 	Get the overview data.
+		 *	@return
+		 */
+		public TIntArrayList[] getAudioOverview()
+		{
+			return this.audioOverview;
+		}
+		
+		/**
+		 * 	Refactors the overview to given another overview. If the number
+		 * 	of bins specified an overview that's finer than the actual overview
+		 * 	the original overview is returned. The output of this function will
+		 * 	then only return an array list of nBins or less. 
+		 * 
+		 * 	@param channel The channel to get
+		 *	@param nBins The number of bins in the overview
+		 *	@return A refactors overview
+		 */
+		public TIntArrayList getAudioOverview( int channel, int nBins )
+		{
+			System.out.println( "Refactoring overview of channel "+channel
+					+" to "+nBins+" bins");
+			
+			if( nBins > audioOverview[channel].size() )
+				return audioOverview[channel];
+			
+			TIntArrayList ii = new TIntArrayList();
+			double scalar = (double)audioOverview[channel].size() / (double)nBins;
+			for( int xx = 0; xx < nBins; xx++ )
+			{
+				int startBin = (int)(xx * scalar);
+				int endBin = (int)((xx+1) * scalar);
+				int m = Integer.MIN_VALUE;
+				for( int yy = startBin; yy < endBin; yy++ )
+					m = Math.max( m, audioOverview[channel].get(yy) );
+				ii.add( m );
+			}
+			return ii;
+		}
+		
+		/**
+		 * 	Returns a polygon representing the channel overview.
+		 *	@param channel The channel to get the polygon for
+		 *	@param mirror whether to mirror the polygon
+		 *	@param width The width of the overview to return
+		 *	@return A polygon
+		 */
+		public Polygon getChannelPolygon( int channel, boolean mirror, int width )
+		{
+			TIntArrayList overview = getAudioOverview( channel, width );
+			int len = overview.size();
+			
+			ArrayList<Point2d> l = new ArrayList<Point2d>();
+			for( int x = 0; x < len; x++ )
+				l.add( new Point2dImpl( x, overview.get(x) ) );
+			
+			if( mirror )
+			{
+				for( int x = 1; x <= len; x++ )
+					l.add( new Point2dImpl( len-x,
+						-overview.get(len-x) ) );
+			}
+			
+			return new Polygon( l );
+		}
+	}
+	
 	public static void main( String[] args )
     {
+		// Open the audio stream
 	    final XuggleAudio a = new XuggleAudio( 
-	    //		new File( "src/test/resources/06041609-rttr-16k-news18-rttr-16k.mpg") );
-	    		new File( "/Users/ss/dwhelper/OldNeil.mpg") );
-	    // AudioPlayer ap = AudioPlayer.createAudioPlayer( a );
+	    		new File( "src/test/resources/bbc.mp4") );
 	    
-	    final int w = 1000;
-	    final int h = 400;
+	    // This is how wide we're going to draw the display
+	    final int w = 1064;
+	    
+	    // This is how high we'll draw the display
+	    final int h = 200;
+	    
+	    // How many pixels we'll overview per pixel
 	    final int nSamplesPerPixel = 5000; // TODO: This is currently fixed
+	    
+	    // Work out how high each channel will be
 	    final int channelSize = h/a.getFormat().getNumChannels();
-	    final double ampScalar = (double)channelSize / Short.MAX_VALUE / 2;
+	    
+	    // This is the scalar from audio amplitude to pixels
+	    final double ampScalar = (double)channelSize / Short.MAX_VALUE;
 	    
 	    System.out.println( "Samples per pixel: "+nSamplesPerPixel );
 	    System.out.println( "Channel height: "+channelSize );
 	    System.out.println( "Amplitude scalar: "+ampScalar );
 	    
-	    final MBFImage m = new MBFImage( w, h, 3 );
-	    AudioProcessor aap = new AudioProcessor()
-		{	
-	    	private int offset = 0;
-	    	private int nSampleToDraw = 0;
-	    	
-			@Override
-			public SampleChunk process( SampleChunk samples )
-			{
-				if( offset > w ) return null;
+	    // Create the image we're going to draw on to - RGBA
+	    final MBFImage m = new MBFImage( w, h, 4 );
+	    m.fill( new Float[]{0f,0f,0f,1f} );
 
-				int nChans = a.getFormat().getNumChannels();
-				int nSamples = samples.getSamples().length/nChans;
-				
-				// If the sample we need to draw is within this chunk...
-				if( nSampleToDraw < nSamples )
-				{
-					ShortBuffer b = samples.getSamplesAsByteBuffer().asShortBuffer();
-					
-					int xx = 0;
-					for( int x = nSampleToDraw; x < nSamples; x += nSamplesPerPixel  )
-					{
-						for( int c = 0; c < a.getFormat().getNumChannels(); c++ )
-						{
-								m.drawLine( 
-									(int)(xx+offset), 
-									(int)(channelSize*c+channelSize/2),
-									(int)(xx+offset), 
-									(int)(channelSize*c+channelSize/2 +
-										b.get(x+c)*ampScalar), RGBColour.WHITE );
-								m.drawPoint( new Point2dImpl( (int)(xx+offset), 
-									(int)(channelSize*c+channelSize/2 +
-										b.get(x+c)*ampScalar) ), RGBColour.RED, 1 );
-						}
-						
-						xx++;
-					}
-					
-					offset += xx;
-					nSampleToDraw += nSamplesPerPixel;
-				}
-				else	nSampleToDraw -= nSamples;
-				
-				return samples;
-			}
-		};
+	    // Generate the audio overview
+	    System.out.println( "Processing audio..." );
+	    AudioOverviewGenerator aap = new AudioOverviewGenerator( 
+	    		nSamplesPerPixel, a.getFormat().getNumChannels() );
 		aap.process( a );
+		
+		// Draw the polygon onto the image
+		for( int i = 0; i < a.getFormat().getNumChannels(); i++ )
+		{
+			System.out.println( "Getting channel polygon..." );
+			Polygon p = aap.getChannelPolygon( i, true, w/2 );
+			
+			System.out.println( "Drawing polygon for channel "+i+"..." );
+			p.scaleXY( 2f, -(float)ampScalar );
+			p.translate( 0f, -(float)p.minY() + channelSize*i );
+
+			System.out.println( "Polygon has "+p.nVertices()+" vertices" );
+			System.out.println( "Bounding box: "+p.minX()+","+p.minY()+" "
+					+p.getWidth()+"x"+p.getHeight() );
+			
+			m.drawPolygonFilled( p, new Float[]{1f,1f,1f,1f} );
+		}
+		
+		// Display the image
 		DisplayUtilities.display( m );
     }
 }
