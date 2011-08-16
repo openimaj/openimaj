09be403e6d5ad4b03d8fe9d0190a005ee67b7c51
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2011-08-16 12:17:07 +0000

    Added tests for CIE deltaE, and fixed the implementation so the tests now pass

--- image/image-processing/src/main/java/org/openimaj/image/colour/processing/CIEDE2000.java
+++ image/image-processing/src/main/java/org/openimaj/image/colour/processing/CIEDE2000.java
@@ -30,39 +30,47 @@ public class CIEDE2000 implements ImageProcessor<MBFImage> {
 	}
 	
 	public static double calculateDeltaE(double L1, double a1, double b1, double L2, double a2, double b2) {
-		double Lmean = (L1 + L2) / 2;
-		double C1 =  Math.sqrt(a1*a1 + b1*b1);
-		double C2 =  Math.sqrt(a2*a2 + b2*b2);
-		double Cmean = (C1 + C2) / 2;
-		double G =  (( 1 - Math.sqrt( Math.pow(Cmean, 7) / (Math.pow(Cmean, 7) + Math.pow(25, 7)) ) ) / 2);
-		double a1sharp = a1 * (1 + G);
-		double a2sharp = a2 * (1 + G);
-		double C1sharp =  Math.sqrt(a1sharp*a1sharp + b1*b1);
-		double C2sharp =  Math.sqrt(a2sharp*a2sharp + b2*b2);
-		double CmeanSharp = (C1sharp + C2sharp) / 2;
-		double h1sharp =  ((Math.atan2(b1, a1sharp) >= 0) ? Math.atan2(b1, a1sharp) : Math.atan2(b1, a1sharp) + 2*Math.PI);
-		double h2sharp =  ((Math.atan2(b2, a2sharp) >= 0) ? Math.atan2(b2, a2sharp) : Math.atan2(b2, a2sharp) + 2*Math.PI);
-		double HmeanSharp =  ((Math.abs(h1sharp - h2sharp) > Math.PI) ? (h1sharp + h2sharp + 2*Math.PI) / 2 : (h1sharp + h2sharp) / 2);
-		double T =  (1 - 0.17 * Math.cos(HmeanSharp - 30 * Math.PI / 180) + 0.24 * Math.cos(2*HmeanSharp) + 0.32 * Math.cos(3 * HmeanSharp + 6*Math.PI/180) - 0.2 * Math.cos(4 * HmeanSharp - 63 * Math.PI / 180));
-		double deltahsharp =  ((Math.abs(h2sharp - h1sharp) <= Math.PI) ? h2sharp - h1sharp : (h2sharp < h1sharp) ? h2sharp - h1sharp + 2*Math.PI : h2sharp - h1sharp - 2*Math.PI);
-		double deltaL = L2 - L1;
-		double deltaCsharp = C2sharp - C1sharp;
-		double deltaHsharp =  (2 * Math.sqrt(C1sharp*C2sharp) * Math.sin(deltahsharp / 2));
-		double SL =  (1 + ( ( 0.015*(Lmean - 50)*(Lmean - 50) ) / ( Math.sqrt( 20 + (Lmean - 50)*(Lmean - 50) ) ) ));
-		double SC =  (1 + 0.045 * CmeanSharp);
-		double SH =  (1 + 0.015 * CmeanSharp * T);
-		double deltaTheta =  ((30 * Math.PI / 180) * Math.exp(-( ((HmeanSharp - 275*Math.PI/180) / 25 ) * ((HmeanSharp - 275*Math.PI/180) / 25 ))));
-		double RC =  (2 * Math.sqrt(Math.pow(CmeanSharp, 7) / (Math.pow(CmeanSharp, 7) * Math.pow(25, 7))));
+		double Lmean = (L1 + L2) / 2.0; //ok
+		double C1 =  Math.sqrt(a1*a1 + b1*b1); //ok
+		double C2 =  Math.sqrt(a2*a2 + b2*b2); //ok
+		double Cmean = (C1 + C2) / 2.0; //ok
+		
+		double G =  ( 1 - Math.sqrt( Math.pow(Cmean, 7) / (Math.pow(Cmean, 7) + Math.pow(25, 7)) ) ) / 2; //ok
+		double a1prime = a1 * (1 + G); //ok
+		double a2prime = a2 * (1 + G); //ok
+		
+		double C1prime =  Math.sqrt(a1prime*a1prime + b1*b1); //ok
+		double C2prime =  Math.sqrt(a2prime*a2prime + b2*b2); //ok
+		double Cmeanprime = (C1prime + C2prime) / 2; //ok 
+		
+		double h1prime =  Math.atan2(b1, a1prime) + 2*Math.PI * (Math.atan2(b1, a1prime)<0 ? 1 : 0);
+		double h2prime =  Math.atan2(b2, a2prime) + 2*Math.PI * (Math.atan2(b2, a2prime)<0 ? 1 : 0);
+		double Hmeanprime =  ((Math.abs(h1prime - h2prime) > Math.PI) ? (h1prime + h2prime + 2*Math.PI) / 2 : (h1prime + h2prime) / 2); //ok
+		
+		double T =  1.0 - 0.17 * Math.cos(Hmeanprime - Math.PI/6.0) + 0.24 * Math.cos(2*Hmeanprime) + 0.32 * Math.cos(3*Hmeanprime + Math.PI/30) - 0.2 * Math.cos(4*Hmeanprime - 21*Math.PI/60); //ok
+		
+		double deltahprime =  ((Math.abs(h1prime - h2prime) <= Math.PI) ? h2prime - h1prime : (h2prime <= h1prime) ? h2prime - h1prime + 2*Math.PI : h2prime - h1prime - 2*Math.PI); //ok
+		
+		double deltaLprime = L2 - L1; //ok
+		double deltaCprime = C2prime - C1prime; //ok
+		double deltaHprime =  2.0 * Math.sqrt(C1prime*C2prime) * Math.sin(deltahprime / 2.0); //ok
+		double SL =  1.0 + ( (0.015*(Lmean - 50)*(Lmean - 50)) / (Math.sqrt( 20 + (Lmean - 50)*(Lmean - 50) )) ); //ok
+		double SC =  1.0 + 0.045 * Cmeanprime; //ok
+		double SH =  1.0 + 0.015 * Cmeanprime * T; //ok
+		
+		double deltaTheta =  (30 * Math.PI / 180) * Math.exp(-((180/Math.PI*Hmeanprime-275)/25)*((180/Math.PI*Hmeanprime-275)/25));
+		double RC =  (2 * Math.sqrt(Math.pow(Cmeanprime, 7) / (Math.pow(Cmeanprime, 7) + Math.pow(25, 7))));
 		double RT =  (-RC * Math.sin(2 * deltaTheta));
+		
 		double KL = 1;
 		double KC = 1;
 		double KH = 1;
 		
 		double deltaE = Math.sqrt(
-				((deltaL/(KL*SL)) * (deltaL/(KL*SL))) +
-				((deltaCsharp/(KC*SC)) * (deltaCsharp/(KC*SC))) +
-				((deltaHsharp/(KH*SH)) * (deltaHsharp/(KH*SH))) +
-				(RT * (deltaCsharp/(KC*SC)) * (deltaHsharp/(KH*SH)))
+				((deltaLprime/(KL*SL)) * (deltaLprime/(KL*SL))) +
+				((deltaCprime/(KC*SC)) * (deltaCprime/(KC*SC))) +
+				((deltaHprime/(KH*SH)) * (deltaHprime/(KH*SH))) +
+				(RT * (deltaCprime/(KC*SC)) * (deltaHprime/(KH*SH)))
 				);
 				
 		return deltaE;
