4bfc784f5cdf31c822070234ce4565bf5ba22a74
David Duplaw <david.dupplaw@wds.co>
2011-08-19 21:20:54 +0000

    * Added new tools subproject: OCRTools      * Implementation of a FontSimulator that renders a string with a random font under random transforms for simulating real-world characters      * KNNCharacterClassifier that used KNN for classifying images of single characters. Simple train/classify/test routines.         - Automatic training using the FontSimulator to create training and test sets         - Currently uses the pixels of the (normalised) image as a feature vector but that's easily overridden.

---
+++ tools/OCRTools/src/main/java/org/openimaj/tools/ocr/FontSimulator.java
@@ -0,0 +1,563 @@
+/**
+ * 
+ */
+package org.openimaj.tools.ocr;
+
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.FImage;
+import org.openimaj.image.Image;
+import org.openimaj.image.processing.transform.ProjectionProcessor;
+import org.openimaj.image.renderer.ImageRenderer;
+import org.openimaj.image.typography.general.GeneralFont;
+import org.openimaj.image.typography.general.GeneralFontRenderer;
+import org.openimaj.image.typography.general.GeneralFontStyle;
+import org.openimaj.math.geometry.point.Point2d;
+import org.openimaj.math.geometry.point.Point2dImpl;
+import org.openimaj.math.geometry.shape.Rectangle;
+import org.openimaj.math.geometry.transforms.TransformUtilities;
+import org.openimaj.util.pair.IndependentPair;
+
+import Jama.Matrix;
+
+/**
+ *	Class that will generate images containing a rendering of a String in
+ *	a random font that has been randomly affected - the idea is to simulate 
+ *	real-world images in a controlled way for training or testing OCR.
+ *
+ *	@author David Dupplaw <dpd@ecs.soton.ac.uk>
+ *  @created 19 Aug 2011
+ *	@version $Author$, $Revision$, $Date$
+ *
+ *	@param <Q> The pixel type
+ *	@param <I> The concrete image type
+ */
+public class FontSimulator<Q,I extends Image<Q,I>>
+{
+	/**
+	 *	This is an interface for objects that are interested in listening
+	 *	for when the {@link FontSimulator} creates an image during one of its
+	 *	runs.
+	 *
+	 *	@author David Dupplaw <dpd@ecs.soton.ac.uk>
+	 *  @created 19 Aug 2011
+	 *	@version $Author$, $Revision$, $Date$
+	 */
+	public interface FontSimListener<I>
+	{
+		/**
+		 * 	Called when an image is created during one of its runs.
+		 *	@param img The image containing the rendering of the text string
+		 */
+		public void imageCreated( I img );
+	}
+	
+	/** The text string to draw on each simulation */
+	private String textString = null;
+	
+	/** The number of characters being drawn */
+	private int nChars = 0;
+	
+	/** 
+	 * The amount of size jitter to impose on the simulation.
+	 * This value gives the number of points either side of the standard
+	 * point size by which the size may be altered. i.e. if this value was
+	 * 10 and the standard point size 72, the size may randomly alter between
+	 * 62 and 82. 
+	 */
+	private double sizeJitter = 0;
+	
+	/** 
+	 * The amount of angle jitter to impose on the simulation (in degrees).
+	 * This value gives the maximum angle that may be applied to a transform
+	 * of the font in either direction (clockwise or anti-clockwise). i.e.
+	 * if this is given the value 45, the text may be rotated -45 to +45
+	 * degrees (around its centre point).
+	 */
+	private double angleJitter = 0;
+	
+	/** 
+	 * The amount of shear jitter to impose on the simulation (in degrees)
+	 * The value of this is the maximum angle by which the font may be randomly 
+	 * sheared in either direction. So if this variable is 35 degrees, the shear
+	 * may randomly alter between -35 and +35 degrees. 
+	 */
+	private double shearJitter = 0;
+	
+	/** 
+	 * The amount of perspective jitter to impose on the simulation.
+	 * If this value is not zero, perspective is randomly added to the render.
+	 * This is done by distorting the rectangle to a parallelogram. The side
+	 * which is altered is chosen randomly and is altered randomly up to a
+	 * maximum percentage reduction given by this variable. So if this variable
+	 * is 0.5 (50%) then one side of the image will end up at a minimum size
+	 * of 50% of its original size. The image is always shrunk. 
+	 */
+	private double perspectiveJitter = 0;
+
+	/** Whether to randomise the type of the font (plain,bold,italic) */
+	private boolean typeJitter = false;
+	
+	/** A list of font family names to avoid (perhaps they contain symbols only? */
+	private List<String> fontsToAvoid = new ArrayList<String>();
+	
+	/** The font size to draw the text in */
+	private int fontPointSize = 18;
+	
+	/** The amount of padding (in pixels) to add around the edge of the render */
+	private int padding = 5;
+	
+	/**
+	 * 	Create a font simulator that will output the given text string.
+	 *	@param textString The text string to draw
+	 */
+	public FontSimulator( String textString )
+	{
+		this.setTextString( textString );
+	}
+
+	/**
+	 * 	Make a number of runs of creating text renders
+	 * 
+	 *	@param nRuns The number of runs to make
+	 *	@param fsl The listener that will receive the images 
+	 *	@param imgExample An example of the type of image <I>
+	 */
+	public void makeRuns( int nRuns, FontSimListener<I> fsl, I imgExample )
+	{
+		for( int i = 0; i < nRuns; i++ )
+		{
+			fsl.imageCreated( generate( imgExample ) );
+		}
+	}
+
+	/**
+	 * 	Generates a render of the text string in some random font with some
+	 * 	random transform.  The input image is not affected or used - it is only
+	 * 	used as a seed for generating new instances of that type of image. A new
+	 * 	image of the same type is returned. 
+	 * 
+	 *	@param imgExample An example of the image to which to draw the text.
+	 *	@return An image containing the rendered text
+	 */
+	public I generate( I imgExample )
+	{
+		// Get a random font from the system
+		GeneralFont font = pickRandomFont();
+		font = getJitteredFont( font );
+		
+		// Create an image of the right size to fit the text into.
+		// To do that we must create an instance of an image renderer
+		// so that we can create a FontStyle. With the style we can create
+		// a FontRenderer which can give us the bounds of the text to draw.
+		// From that we can create an image into which the text will fit.
+		ImageRenderer<Q, I> r = imgExample.createRenderer();
+		
+		GeneralFontStyle<Q> gfs = 
+			new GeneralFontStyle<Q>( font, r, false );
+		gfs = getJitteredFontStyle( gfs );
+			
+		GeneralFontRenderer<Q> gfr = new GeneralFontRenderer<Q>();
+		Rectangle b = gfr.getBounds( textString, gfs );
+		
+		// Create an image into which the text will fit.
+		I img = imgExample.newInstance( 
+				(int)b.width + padding*2, 
+				(int)b.height + padding*2 );
+		
+		// Draw the characters to the image
+		img.drawText( textString, padding, (int)(padding + b.height), gfs );
+		
+		// Transform the image
+		Matrix transform = getJitterTransform( img );
+		img = ProjectionProcessor.project( img, transform );
+		
+		return img;
+	}
+	
+	/**
+	 * 	Picks a random font from your system and returns it. Checks
+	 * 	it against the fontsToAvoid list and will not return a font
+	 * 	that is on that list. If a font cannot be returned, the method
+	 * 	will return null. The returned font will have plain type and
+	 * 	a size of 18 point.
+	 * 
+	 *	@return A random Font from the system.
+	 */
+	public GeneralFont pickRandomFont()
+	{
+		List<String> fontNames = new ArrayList<String>();
+		GraphicsEnvironment e = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		for( String font : e.getAvailableFontFamilyNames() )
+			if( !fontsToAvoid.contains( font ) )
+				fontNames.add( font );
+			
+		if( fontNames.size() == 0 )
+			return null;
+		
+		int r = (int)(Math.random() * fontNames.size());
+		return new GeneralFont( fontNames.get(r), 
+				java.awt.Font.PLAIN, getFontPointSize() );
+	}
+	
+	/**
+	 * 	Returns a font that is somewhat randomised from the 
+	 * 	initial font. Side-affects the incoming font and returns it.
+	 * 
+	 *	@param font The font to jitter
+	 *	@return The incoming font altered by jitter specification
+	 */
+	public GeneralFont getJitteredFont( GeneralFont font )
+	{
+		if( isTypeJitter() )
+		{
+			double r = Math.random();
+			if( r > 0.3 )
+				if( r > 0.6 )
+						font.setType( Font.PLAIN );
+				else	font.setType( Font.BOLD );
+			else		font.setType( Font.ITALIC );
+		}
+		
+		if( sizeJitter > 0 )
+		{
+			double r = Math.random() * sizeJitter - sizeJitter/2;
+			font.setSize( (int)(fontPointSize + r) );
+		}
+		
+		return font;
+	}
+	
+	/**
+	 * 	Returns an affine transform matrix based on the jitter 
+	 * 	specifications.
+	 *  
+	 *	@return An affine transform matrix
+	 */
+	public Matrix getJitterTransform( I img )
+	{
+		Matrix m = Matrix.identity( 3, 3 );
+		
+		if( angleJitter > 0 )
+		{
+			double r = angleJitter*(Math.random()*2-1) / 57.2957795;
+			Matrix rm = TransformUtilities.centeredRotationMatrix( 
+					r, img.getWidth(), img.getHeight() );
+			m = m.times( rm );
+		}
+		
+		if( shearJitter > 0 )
+		{
+			double r = shearJitter*(Math.random()*2-1) / 57.2957795;
+			Matrix rm = new Matrix( new double[][]{
+					{1, Math.tan(r), 0},
+					{0,1,0},
+					{0,0,1}
+			});
+			m = m.times( rm );
+		}
+		
+		if( perspectiveJitter > 0 )
+		{
+			// Get the image bounds to distort			
+			Rectangle rect = img.getBounds();
+			
+			// Start points
+			Point2d p1 = new Point2dImpl( rect.x, rect.y );
+			Point2d p2 = new Point2dImpl( rect.x, rect.y+rect.height );
+			Point2d p3 = new Point2dImpl( rect.x+rect.width, rect.y+rect.height );
+			Point2d p4 = new Point2dImpl( rect.x+rect.width, rect.y );
+			
+			// End points
+			Point2d p1p = p1;
+			Point2d p2p = p2;
+			Point2d p3p = p3;
+			Point2d p4p = p4;
+			
+			float s = (float)(perspectiveJitter/2f);
+			
+			// Randomly choose a side to reduce
+			switch( (int)(Math.random()*4) )
+			{
+				// top
+				case 0:
+					p1p = new Point2dImpl( rect.x+rect.width*s, rect.y );
+					p4p = new Point2dImpl( rect.x+rect.width-rect.width*s, rect.y );
+					break;
+				// left
+				case 1:
+					p1p = new Point2dImpl( rect.x, rect.y+rect.height*s );
+					p2p = new Point2dImpl( rect.x, rect.y+rect.height-rect.height*s );
+					break;
+				// bottom
+				case 2:
+					p2p = new Point2dImpl( rect.x+rect.width*s, rect.y+rect.height );
+					p3p = new Point2dImpl( rect.x+rect.width-rect.width*s, rect.y+rect.height );
+					break;
+				// right
+				case 3:
+					p3p = new Point2dImpl( rect.x+rect.width, rect.y+rect.height*s );
+					p4p = new Point2dImpl( rect.x+rect.width, rect.y+rect.height-rect.height*s );
+			}
+			
+			List<IndependentPair<Point2d, Point2d>> d = new
+					ArrayList<IndependentPair<Point2d,Point2d>>();
+			
+			d.add( new IndependentPair<Point2d,Point2d>( p1, p1p ) );
+			d.add( new IndependentPair<Point2d,Point2d>( p2, p2p ) );
+			d.add( new IndependentPair<Point2d,Point2d>( p3, p3p ) );
+			d.add( new IndependentPair<Point2d,Point2d>( p4, p4p ) );
+			
+			Matrix hm = TransformUtilities.homographyMatrix( d );
+			m = m.times( hm );
+		}
+		
+		return m;
+	}
+	
+	/**
+	 * 	Get a jittered font style
+	 *	@param gfs The input font style
+	 *	@return The jittered font style
+	 */
+	public GeneralFontStyle<Q> getJitteredFontStyle( GeneralFontStyle<Q> gfs )
+	{
+//		if( colourJitter > 0 )
+//		{
+//			double r = Math.random() * colourJitter;
+//			Q col = gfs.getColour();
+//		}
+		
+		return gfs;
+	}
+	
+	/**
+	 * 	Set the text string to use. Can be set to null for random
+	 * 	characters.
+	 *	@param textString the text string to use
+	 */
+	public void setTextString( String textString )
+	{
+		this.textString = textString;
+		this.nChars = textString.length();
+	}
+
+	/**
+	 * 	Get the text string in use.
+	 *	@return the text string in use
+	 */
+	public String getTextString()
+	{
+		return textString;
+	}
+
+	/**
+	 * 	Set the amount of size jitter to use.
+	 *	@param sizeJitter the amount of size jitter to use
+	 */
+	public void setSizeJitter( double sizeJitter )
+	{
+		this.sizeJitter = sizeJitter;
+	}
+
+	/**
+	 * 	Get the amount of size jitter in use.
+	 *	@return the amount of size jitter in use.
+	 */
+	public double getSizeJitter()
+	{
+		return sizeJitter;
+	}
+
+	/**
+	 * 	Set the amount of angle jitter to use.
+	 *	@param angleJitter the amount of angle jitter to use
+	 */
+	public void setAngleJitter( double angleJitter )
+	{
+		this.angleJitter = angleJitter;
+	}
+
+	/**
+	 * 	Get the amount of angle jitter in use.
+	 *	@return the amount of angle jitter in use.
+	 */
+	public double getAngleJitter()
+	{
+		return angleJitter;
+	}
+
+	/**
+	 * 	Set the amount of shear jitter
+	 *	@param shearJitter the amount of shear jitter to use
+	 */
+	public void setShearJitter( double shearJitter )
+	{
+		this.shearJitter = shearJitter;
+	}
+
+	/**
+	 * 	Get the amount of shear jitter in use.
+	 *	@return the amount of shear jitter in use
+	 */
+	public double getShearJitter()
+	{
+		return shearJitter;
+	}
+
+	/**
+	 * 	Set the amount of perspective jitter to use.
+	 *	@param perspectiveJitter the amount of perspective jitter to use
+	 */
+	public void setPerspectiveJitter( double perspectiveJitter )
+	{
+		this.perspectiveJitter = perspectiveJitter;
+	}
+
+	/**
+	 * 	Get the amount of perspective jitter being simulated
+	 *	@return the amount of perspective jitter
+	 */
+	public double getPerspectiveJitter()
+	{
+		return perspectiveJitter;
+	}
+
+	/**
+	 * 	Set the list of fonts to avoid.
+	 *	@param fontsToAvoid the list of fonts to avoid
+	 */
+	public void setFontsToAvoid( List<String> fontsToAvoid )
+	{
+		this.fontsToAvoid = fontsToAvoid;
+	}
+
+	/**
+	 * 	Get the list of fonts to avoid.
+	 * 
+	 *	@return the fonts to avoid
+	 */
+	public List<String> getFontsToAvoid()
+	{
+		return fontsToAvoid;
+	}
+	
+	/**
+	 * 	Avoid the font with the given name.
+	 *	@param f The font to avoid
+	 */
+	public void addFontToAvoid( String f )
+	{
+		this.fontsToAvoid.add( f );
+	}
+
+	/**
+	 * 	Set the number of characters to randomly generate.
+	 *	@param nChars the number of chars to generate
+	 */
+	public void setnChars( int nChars )
+	{
+		this.nChars = nChars;
+	}
+
+	/**
+	 * 	Get the number of characters being generated.
+	 *	@return the number of characters to generate
+	 */
+	public int getnChars()
+	{
+		return nChars;
+	}
+
+	/**
+	 * 	Set whether to jitter the font type or not.
+	 * 
+	 *	@param typeJitter Whether to jitter the font type
+	 */
+	public void setTypeJitter( boolean typeJitter )
+	{
+		this.typeJitter = typeJitter;
+	}
+
+	/**
+	 * 	Get whether the font type is being randomised.
+	 * 
+	 *	@return Whether the font type is being randomised.
+	 */
+	public boolean isTypeJitter()
+	{
+		return typeJitter;
+	}
+
+	/**
+	 * 	Set the point size of the font to draw
+	 *	@param fontPointSize the font point size to use
+	 */
+	public void setFontPointSize( int fontPointSize )
+	{
+		this.fontPointSize = fontPointSize;
+	}
+
+	/**
+	 * 	Get the size of the font that is being drawn
+	 *	@return the size of the font being drawn
+	 */
+	public int getFontPointSize()
+	{
+		return fontPointSize;
+	}
+
+	/**
+	 * 	Set the padding around the outside of the image.
+	 *	@param padding the padding amount
+	 */
+	public void setPadding( int padding )
+	{
+		this.padding = padding;
+	}
+
+	/**
+	 * 	Get the amount of padding around the image.
+	 *	@return the padding amount
+	 */
+	public int getPadding()
+	{
+		return padding;
+	}
+	
+	/**
+	 * 	Simple main that runs a simulation of 5 runs with the text "ABC" or
+	 * 	text supplied on the command line.
+	 * 
+	 *	@param args
+	 */
+	public static void main( String[] args )
+	{
+		String text = "ABC";
+		if( args.length > 0 )
+			text = args[0];
+		
+		// Create an FImage font simulator 
+		FontSimulator<Float,FImage> fs = 
+			new FontSimulator<Float,FImage>( text );
+		fs.setFontPointSize( 72 );
+		
+		fs.setAngleJitter( 10 );
+		fs.setShearJitter( 20 );
+		fs.setPerspectiveJitter( 0.5 );
+		
+		// Make 5 runs
+		fs.makeRuns( 5, new FontSimListener<FImage>()
+		{
+			public void imageCreated( FImage img )
+			{
+				// display the result
+				DisplayUtilities.display( img );
+			}
+		}, new FImage(1,1) );
+	}
+}
---
+++ tools/OCRTools/src/main/java/org/openimaj/tools/ocr/KNNCharacterClassifier.java
@@ -0,0 +1,242 @@
+/**
+ * 
+ */
+package org.openimaj.tools.ocr;
+
+import org.openimaj.image.FImage;
+import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.knn.DoubleNearestNeighbours;
+import org.openimaj.knn.approximate.DoubleNearestNeighboursKDTree;
+import org.openimaj.tools.ocr.FontSimulator.FontSimListener;
+import org.openimaj.util.pair.Pair;
+
+/**
+ *	A class and tester for testing KNN classification of characters 
+ *	which are built randomly from the {@link FontSimulator}.
+ *	<p> 
+ *	In this base implementation, the pixels of the character images are 
+ *	used as the vector for classification which will achieve somewhere in the
+ *	region of 60% correct classification for the characters 0-9 and somewhat
+ *	worse for larger character ranges. 
+ *	<p>
+ *	Override the {@link #getImageVector(FImage)} method to try different 
+ *	features. This class provides the training, classification and testing.
+ *
+ *	@author David Dupplaw <dpd@ecs.soton.ac.uk>
+ *  @created 19 Aug 2011
+ *	@version $Author$, $Revision$, $Date$
+ */
+public class KNNCharacterClassifier
+{
+	/**
+	 *	This class is the {@link FontSimListener} implementation that receives
+	 *	each character as an image in some randomised font. When it receives
+	 *	an image, it creates the feature vector by calling
+	 *	{@link KNNCharacterClassifier#getImageVector(FImage)} and stores that
+	 *	into a double array that can be used as input to the nearest neighbour
+	 *	classifier for training. That array can be retrieved using 
+	 *	{@link #getVector()}. 
+	 *
+	 *	@author David Dupplaw <dpd@ecs.soton.ac.uk>
+	 *  @created 19 Aug 2011
+	 *	@version $Author$, $Revision$, $Date$
+	 */
+	protected class ImageTrainer implements FontSimListener<FImage>
+	{
+		/** The output vector that can be used as input to the NN classifier */
+		private double[][] vector = null;
+		
+		/** The current index being processed */
+		private int index = 0;
+
+		/**
+		 * 	Constructor that takes the size of the output vector (that is the
+		 * 	total number of training instances).
+		 * 
+		 *	@param n The number of training instances
+		 */
+		public ImageTrainer( int n )
+		{
+			vector = new double[n][];
+		}
+
+		/**
+		 * 	Implementation of the {@link FontSimListener} interface.
+		 *	@param img The character image.
+		 */
+		public void imageCreated( FImage img )
+		{
+			vector[index++] = getImageVector(img);
+			if( index%25 == 0 )
+				System.out.print("..."+index );
+		}
+		
+		/**
+		 * 	Retrieve the set of training data as a double array.
+		 *	@return the training data
+		 */
+		public double[][] getVector()
+		{
+			return vector;
+		}
+	}
+	
+	private int NTREES = 768;
+	private int NCHECKS = 8;
+	
+	/** The nearest neighbour classifier */
+	private DoubleNearestNeighbours nn = null;
+	
+	/** What size to resize the images to for the feature vector */
+	private int resize = 32;
+	
+	/** Number of examples per class to generate */
+	private int nExamplesPerClass = 100;
+	
+	/** Start character in the range */
+	private int startChar = '0';
+	
+	/** End character in the range */
+	private int endChar = '9';
+	
+	/**
+	 * 	Default constructor
+	 */
+	public KNNCharacterClassifier()
+	{
+	}
+	
+	/**
+	 * 	Get the feature vector for a single image. Can be overridden to 	
+	 * 	try different feature vectors for classification.	
+	 * 
+	 *	@param img The character image.
+	 *	@return The feature vector for this image.
+	 */
+	public double[] getImageVector( FImage img )
+	{
+		// Resize the image (stretch) to a standard shape
+		FImage ff = ResizeProcessor.resample( img, resize, resize );
+		return ff.getDoublePixelVector();		
+	}
+	
+	/**
+	 * 	Train the classifier by generating a bunch of random training examples
+	 * 	of characters in various fonts (using the {@link FontSimulator}) 
+	 * 	and using the features extracted from those images, train a nearest
+	 * 	neighbour classifier.
+	 */
+	public void train()
+	{
+		// Create the FontSimListener that receives images from the FontSimulator
+		ImageTrainer it = new ImageTrainer(
+				(endChar-startChar)*nExamplesPerClass);
+		
+		System.out.println( "Created vector of length "+
+				((endChar-startChar)*nExamplesPerClass) );
+		System.out.println( "Creating character data" );
+		
+		// For each of the characters produce nExamplesPerCase training examples
+		for( int i = startChar; i < endChar; i++ )
+		{
+			FontSimulator<Float, FImage> fs = 
+				new FontSimulator<Float,FImage>( 
+						Character.toString( (char)i ) );
+			fs.setFontPointSize( 48 );
+			fs.makeRuns( nExamplesPerClass, it, new FImage(1,1) );
+		}
+
+		// Train the classifier
+		System.out.println( "\nCreating KDTree...");
+		nn = new DoubleNearestNeighboursKDTree( it.getVector(), NTREES, NCHECKS );
+	}
+	
+	/**
+	 * 	Classify the given image with the nearest neighbour classifier.
+	 * 
+	 *	@param img the character image to classify
+	 *	@return The classified character.
+	 */
+	public char classify( FImage img )
+	{
+		// Create the input vector
+		double[][] v = new double[1][];
+		v[0] = getImageVector( img );
+		
+		// Setup the output variables
+		int k = 1;
+		int[][] indices  = new int[1][k];
+		double[][] dists = new double[1][k];
+		
+		// Do the search
+		nn.searchKNN( v, k, indices, dists );
+		
+		// Work out what character the best index represents
+		System.out.println( "Best index: "+indices[0][0] );
+		char c = (char)(48+indices[0][0]/nExamplesPerClass);
+		System.out.println( "So, I think it's "+c );
+		
+		return c;
+	}
+	
+	/**
+	 * 	Run a bunch of tests to determine how good the classifier is. It does
+	 * 	this by creating a load of random examples of random characters using
+	 * 	the {@link FontSimulator} and classifies them.
+	 * 
+	 *	@param nTestRuns The number of tests to run
+	 */
+	public void test( int nTestRuns )
+	{
+		// First in the pair is the incorrectly classified count,
+		// the second is the correctly classified count.
+		final Pair<Integer> results = new Pair<Integer>(0,0);
+		
+		// Loop through all the tests
+		for( int j = 0; j < nTestRuns; j++ )
+		{
+			// Choose a randome character that's in our training set
+			final int i = startChar + (int)(Math.random()*(endChar-startChar));
+			
+			// Create a single random image of the chosen character
+			FontSimulator<Float, FImage> fs = new FontSimulator<Float,FImage>( 
+						Character.toString( (char)i ) );
+			fs.setFontPointSize( 48 );
+			FImage x = fs.generate( new FImage(1,1) );
+			
+			// Classify that character
+			System.out.println( "Classifying character "+(char)i );
+			char c = classify( x );
+			
+			// Update the test count
+			if( c != i )
+					results.setFirstObject( results.firstObject().intValue()+1 );
+			else	results.setSecondObject( results.secondObject().intValue()+1 );
+		}
+		
+		// Show the test results
+		int nWrong = results.firstObject();
+		int nCorrect = results.secondObject();
+		
+		System.out.println( "===========================================");
+		System.out.println( "           T E S T   R E S U L T S         ");
+		System.out.println( "===========================================");
+		System.out.println( "Number of runs: "+nTestRuns );
+		System.out.println( "Correct: "+nCorrect+" ("+(100*nCorrect/nTestRuns)+"%)" );
+		System.out.println( "Wrong:   "+nWrong+" ("+(100*nWrong/nTestRuns)+"%)" );
+		System.out.println( "===========================================");
+		
+	}
+	
+	/**
+	 * 	The main does a training run then a test run.
+	 *  
+	 *	@param args
+	 */
+	public static void main( String[] args )
+	{
+		final KNNCharacterClassifier ocr = new KNNCharacterClassifier();
+		ocr.train();
+		ocr.test( 100 );
+	}
+}
