5b4f9ea778dd3a365e1fc0a52ad530312590492a
Sina Samangooei <sinjax@gmail.com>
2011-10-04 12:47:51 +0000

    Fixed the windows DLLs

--- core/core-video-capture/src-native/win32/OpenIMAJGrabber/OpenIMAJGrabber.cpp
+++ core/core-video-capture/src-native/win32/OpenIMAJGrabber/OpenIMAJGrabber.cpp
@@ -1,32 +1,3 @@
-/**
- * Copyright (c) 2011, The University of Southampton and the individual contributors.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *   * 	Redistributions of source code must retain the above copyright notice,
- * 	this list of conditions and the following disclaimer.
- *
- *   *	Redistributions in binary form must reproduce the above copyright notice,
- * 	this list of conditions and the following disclaimer in the documentation
- * 	and/or other materials provided with the distribution.
- *
- *   *	Neither the name of the University of Southampton nor the names of its
- * 	contributors may be used to endorse or promote products derived from this
- * 	software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 #include <iostream>
 
 #include "OpenIMAJGrabber.h"
@@ -115,15 +86,17 @@ DeviceList * OpenIMAJGrabber::getVideoDevices(){
     return new DeviceList(devices, count);
 }
 
-bool OpenIMAJGrabber::startSession(int width, int height)
+bool OpenIMAJGrabber::startSession(int width, int height,double rate)
 {
+	if(this->getVideoDevices()->getNumDevices() <= ((videoData*)this->data)->device)
+		return false;
 	return this->startSession(
-		width,height,
+		width,height,rate,
 		this->getVideoDevices()->getDevice(((videoData*)this->data)->device)
 	);
 }
 
-bool OpenIMAJGrabber::startSession(int width, int height,Device* dev){
+bool OpenIMAJGrabber::startSession(int width, int height,double rate, Device* dev){
 	if(((videoData*)this->data)->buffer != NULL){
 		delete [] ((videoData*)this->data)->buffer;
 		((videoData*)this->data)->VI->stopDevice(((videoData*)this->data)->device);
@@ -131,6 +104,9 @@ bool OpenIMAJGrabber::startSession(int width, int height,Device* dev){
 	}
 	// cout << "Checked the buffer, creating new buffer" << endl;
 	((videoData*)this->data)->device = atoi(dev->getIdentifier());
+
+	if(this->getVideoDevices()->getNumDevices() <= ((videoData*)this->data)->device)
+		return false;
 	//cout << "Current device is: " << ((videoData*)this->data)->device << endl;
 	((videoData*)this->data)->VI->setupDevice(((videoData*)this->data)->device,width,height);
 	//cout << "Device set up, initialising" << endl;
