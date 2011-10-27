7f7f5c7fe64a39ca2a2879742b2ddd3759860ca3
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2011-10-27 15:06:21 +0000

    added clustering of binary similarity

--- tools/GlobalFeaturesTool/src/main/java/org/openimaj/tools/globalfeature/CollectionComparisonTool.java
+++ tools/GlobalFeaturesTool/src/main/java/org/openimaj/tools/globalfeature/CollectionComparisonTool.java
@@ -12,7 +12,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.jgrapht.UndirectedGraph;
+import org.jgrapht.alg.ConnectivityInspector;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
@@ -50,16 +55,16 @@ public class CollectionComparisonTool
 	 */
 	@Option(name="--dir", aliases="-d", usage="Directory of images", 
 			required=true )
-	private String dir = null;
-	
+			private String dir = null;
+
 	/**
 	 * 	This is a mandatory argument that is used to provide the feature
 	 * 	comparison metric.
 	 */
 	@Option(name="--metric", aliases="-m", usage="Comparison metric", 
 			required=true)
-	private FeatureComparison compare = null;
-	
+			private FeatureComparison compare = null;
+
 	/**
 	 * 	This is a mandatory argument that is used to provide the feature
 	 * 	type to generate for each image.
@@ -67,8 +72,8 @@ public class CollectionComparisonTool
 	@Option(name="--feature-type", aliases="-f", 
 			handler=ProxyOptionHandler.class, usage="Feature type", 
 			required=true)
-	private GlobalFeatures feature = null;
-	
+			private GlobalFeatures feature = null;
+
 	/**
 	 * 	This optional argument provides the ability to 'binarise' the output
 	 * 	such that distance values over the threshold will be set to 0,
@@ -76,55 +81,64 @@ public class CollectionComparisonTool
 	 */
 	@Option(name="--threshold", aliases="-t", usage="Threshold distances",
 			required=false)
-	private double threshold = -1;
-	
+			private double threshold = -1;
+
 	/**
 	 * 	This optional argument provides a regular expression against which
 	 * 	files in the directory will be filtered against.
 	 */
 	@Option(name="--regex", aliases="-r", usage="Filename regex pattern",
 			required=false )
-	private String regex = null;
-	
+			private String regex = null;
+
 	/**
 	 * 	This is an optional parameter that allows the user to provide a single
 	 * 	image against which all the other images will be compared. 
 	 */
 	@Option(name="--image", aliases="-i", usage="Single comparison image", 
-			 required=false)
-	private String image = null;
-	
+			required=false)
+			private String image = null;
+
 	/**
 	 * 	This optional parameter that shows verbose output
 	 */
 	@Option(name="--verbose", aliases="-v", usage="Verbose output",
 			required=false)
-	private boolean verbose = false;
-	
+			private boolean verbose = false;
+
 	/**
 	 * 	This optional parameter allows features to be cached
 	 */
 	@Option(name="--cache", usage="Cache features in RAM",
 			required=false)
-	private boolean cache = false;
+			private boolean cache = false;
+
+	/**
+	 * 	This optional parameter allows the clusters of 'similar' images
+	 *  to be output rather than the matrix. The threshold must be set
+	 *  for this to work.
+	 */
+	@Option(name="--printClusters", aliases="-pc", usage="Print the clusters rather than the matrix",
+			required=false)
+			private boolean printClusters = false;
 
 	private Map<File, FeatureVector> cacheData = new HashMap<File, FeatureVector>();
 
 	private FeatureVector getFeatureVector(File file) throws IOException {
 		FeatureVector fv = cacheData.get(file);
-		
+
 		if (fv == null) {
 			MBFImage im1 = ImageUtilities.readMBF( file );
 			fv = feature.execute(im1);
-			
+
 			if (cache) {
 				cacheData.put(file, fv);
 			}
 		}
-		
+
 		return fv;
 	}
-	
+
 	/**
 	 * 	Execute the tool.
 	 */
@@ -134,7 +148,7 @@ public class CollectionComparisonTool
 
 		// Get a list of files
 		List<String> dir2 = getListOfFiles( dir, true );
-		
+
 		// If we're going to compare a single image against a collection,
 		// then we will take a different route here.
 		if( image != null )
@@ -144,7 +158,7 @@ public class CollectionComparisonTool
 		}
 		// Otherwise we'll compare all against all.
 		else dir1 = dir2;
-		
+
 		Matrix m = new Matrix( dir1.size(), dir2.size() );
 		for( int y = 0; y < dir1.size(); y++ )
 		{
@@ -156,27 +170,27 @@ public class CollectionComparisonTool
 
 				int xx = 0;
 				if( dir1 == dir2 )
-						xx = y;	
+					xx = y;	
 				for( int x = xx; x < dir2.size(); x++ )
 				{
 					String s2 = dir2.get(x);
 					if( y == 0 )
 						System.out.println( ""+x+": "+s2 );
-					
+
 					if( verbose )
 						System.out.println( "Comparing "+s1+" against "+s2 );
-					
+
 					try
 					{
 						FeatureVector fv2 = getFeatureVector( new File(s2) );
-						
+
 						double d = 0;
 						if( compare == FeatureComparison.EQUALS ) 
 						{
 							if( Arrays.equals( 
-										fv1.asDoubleVector(), 
-										fv2.asDoubleVector() ) )
-									d = 1;
+									fv1.asDoubleVector(), 
+									fv2.asDoubleVector() ) )
+								d = 1;
 							else	d = 0;
 						} 
 						else 
@@ -200,13 +214,65 @@ public class CollectionComparisonTool
 				continue;
 			}
 		}
-		
-		StringWriter sw = new StringWriter();
-		PrintWriter  pw = new PrintWriter( sw );
-		m.print( pw, 3, 4 );
-		return sw.toString();
+
+		if (printClusters && this.threshold != -1) {
+			return getClusterInfo(dir1, dir2, m);
+		} else {
+			StringWriter sw = new StringWriter();
+			PrintWriter  pw = new PrintWriter( sw );
+			m.print( pw, 3, 4 );
+			return sw.toString();
+		}
 	}
-	
+
+	private String getClusterInfo(List<String> dir1, List<String> dir2, Matrix scores) {
+		List<Set<String>> clusters = getClusters(dir1, dir2, scores);
+
+		StringBuffer sb = new StringBuffer();
+
+		sb.append("<html>\n\t<body>\n");
+		for (Set<String> set : clusters) {
+			if (set.size() > 1) {
+				sb.append("\t\t<div>\n");
+				
+				for (String s : set) {
+					sb.append("\t\t\t<img src=\""+s+"\" width=\"100\"/>\n");
+//					String id = s.replace("/Users/jsh2/youtube/", "").replace("/hqdefault.jpg", "");
+//					sb.append("\t\t\t<a href=\"http://www.youtube.com/watch?v="+id+"\">\n");
+//					sb.append("\t\t\t\t<img src=\"http://i.ytimg.com/vi/"+id+"/hqdefault.jpg\" width=\"100\"/>\n");
+//					sb.append("\t\t\t</a>\n");
+				}
+				
+				sb.append("\t\t</div>");
+				sb.append("\t\t<hr/>");
+			}
+		}
+		sb.append("\t</body>\n</html>\n");
+		
+		return sb.toString();
+	}
+
+	private List<Set<String>> getClusters(List<String> dir1, List<String> dir2, Matrix scores) {
+		UndirectedGraph<String, DefaultEdge> graph = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+
+		for (String f : dir1) graph.addVertex(f);
+		if (dir1 != dir2) for (String f : dir2) graph.addVertex(f);
+
+		final double[][] matrixData = scores.getArray();
+		for (int r=0; r<matrixData.length; r++) {
+			for (int c=r; c<matrixData[0].length; c++) {
+				String f1 = dir1.get(r);
+				String f2 = dir2.get(c);
+				if ( matrixData[r][c] != 0 && f1!=f2 ) {
+					graph.addEdge(f1, f2);
+				}
+			}
+		}
+
+		ConnectivityInspector<String, DefaultEdge> conn = new ConnectivityInspector<String, DefaultEdge>(graph);
+		return conn.connectedSets();
+	}
+
 	/**
 	 * 	Returns a list of files that exist in the given dir.
 	 *	@param dir The directory to start at
@@ -217,7 +283,7 @@ public class CollectionComparisonTool
 	{
 		// We'll stick all the files in here.
 		List<String> files = new ArrayList<String>(); 
-		
+
 		// First get a list of all files. We don't filter them as we're
 		// trying to get all the subdirs.
 		File[] f = new File(dir).listFiles();
@@ -227,17 +293,17 @@ public class CollectionComparisonTool
 			if( file.isDirectory() )
 			{
 				files.addAll( 
-					getListOfFiles( file.getAbsolutePath(), subdirs ) );
+						getListOfFiles( file.getAbsolutePath(), subdirs ) );
 			}
 			else
-			// If there's no regex or the file matches our regex
-			if( regex == null || 
-				(regex != null && file.getName().matches( regex ) ) )
-			{
-				files.add( file.getAbsolutePath() );
-			}
+				// If there's no regex or the file matches our regex
+				if( regex == null || 
+						(regex != null && file.getName().matches( regex ) ) )
+				{
+					files.add( file.getAbsolutePath() );
+				}
 		}
-		
+
 		// Convert to array
 		return files;
 	}
@@ -250,7 +316,7 @@ public class CollectionComparisonTool
 	 */
 	@SuppressWarnings("unchecked")
 	protected <T extends FeatureVector> FVComparable<T> 
-		getComp( T fv, FeatureComparison type ) 
+	getComp( T fv, FeatureComparison type ) 
 	{
 		if (fv instanceof ByteFV) return (FVComparable<T>) ByteFVComparison.valueOf(type.name());
 		if (fv instanceof ShortFV) return (FVComparable<T>) ShortFVComparison.valueOf(type.name());
@@ -259,7 +325,7 @@ public class CollectionComparisonTool
 		if (fv instanceof DoubleFV) return (FVComparable<T>) DoubleFVComparison.valueOf(type.name());
 		return null;
 	}
-	
+
 	/**
 	 * 	Main method.
 	 * 
@@ -273,7 +339,7 @@ public class CollectionComparisonTool
 		// Instantiate the tool and parse the arguments
 		CollectionComparisonTool cct = new CollectionComparisonTool();
 		CmdLineParser parser = new CmdLineParser( cct );
-		
+
 		try
 		{
 			parser.parseArgument( args );			
