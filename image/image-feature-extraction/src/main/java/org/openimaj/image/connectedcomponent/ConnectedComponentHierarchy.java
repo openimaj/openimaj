72b6f674dff551c2a73e70bb312f290121539945
Sina Samangooei <sinjax@gmail.com>
2011-10-31 20:34:06 +0000

    some work on the hiearchy connected component - broken

--- image/image-feature-extraction/src/main/java/org/openimaj/image/connectedcomponent/ConnectedComponentHierarchy.java
+++ image/image-feature-extraction/src/main/java/org/openimaj/image/connectedcomponent/ConnectedComponentHierarchy.java
@@ -3,6 +3,7 @@ package org.openimaj.image.connectedcomponent;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.openimaj.image.DisplayUtilities;
@@ -20,46 +21,47 @@ import org.openimaj.util.pair.Pair;
 import org.openimaj.util.tree.TreeNode;
 
 public class ConnectedComponentHierarchy {
-	private static final int MINIMUM_AREA = 5;
-	private ConnectedComponentLabeler labler;
-	
-	public ConnectedComponentHierarchy(ConnectedComponentLabeler labeler, float threshold){
-		this.labler = labeler;
-	}
-	
-	public TreeNode<ConnectedComponent> hierarchy(FImage markerImage){
-		markerImage.threshold(0.5f);
-		List<ConnectedComponent> components = labler.findComponents(markerImage);
-		List<ConnectedComponent> invComponents = labler.findComponents(markerImage.inverse());
-		components.addAll(invComponents);
-		List<Pair<ConnectedComponent>> containsList = new ArrayList<Pair<ConnectedComponent>>();
-		long start = System.currentTimeMillis();
-		for(ConnectedComponent outer: components){
-			Rectangle outerBox = outer.calculateRegularBoundingBox();
-			if(outer.calculateArea() < MINIMUM_AREA) continue;
-//			output.drawShape(outerBox, RGBColour.RED);
-			for(ConnectedComponent inner : components){
-				if(inner == outer) continue;
-				if(inner.calculateArea() < MINIMUM_AREA) continue;
-				Rectangle innerBox = inner.calculateRegularBoundingBox();
-				if(innerBox.isInside(outerBox)){
-					containsList.add(new Pair<ConnectedComponent>(outer, inner));
-				}
-			}
-		}
-		long end = System.currentTimeMillis();
-		System.out.println("Total time: " + (end - start));
-		for (Pair<ConnectedComponent> pair : containsList) {
-			System.out.println(pair);
-		}
-		DisplayUtilities.displayName(output, "labeled");
-	}
-	
-	public static void main(String[] args) throws IOException {
-		String markerFile = "/Users/ss/Desktop/marker-profile.jpeg";
-		FImage markerImage = ImageUtilities.readF(new File(markerFile));
-		
-		ConnectedComponentLabeler.DEFAULT_ALGORITHM = ConnectedComponentLabeler.Algorithm.FLOOD_FILL;
-		ConnectedComponentLabeler labler = new ConnectedComponentLabeler(ConnectMode.CONNECT_8);
-	}
+//	private static final int MINIMUM_AREA = 5;
+//	private ConnectedComponentLabeler labler;
+//	
+//	public ConnectedComponentHierarchy(ConnectedComponentLabeler labeler, float threshold){
+//		this.labler = labeler;
+//	}
+//	
+//	public TreeNode<ConnectedComponent> hierarchy(FImage markerImage){
+//		markerImage.threshold(0.5f);
+//		List<ConnectedComponent> components = labler.findComponents(markerImage);
+//		List<ConnectedComponent> invComponents = labler.findComponents(markerImage.inverse());
+//		components.addAll(invComponents);
+//		List<Pair<ConnectedComponent>> containsList = new ArrayList<Pair<ConnectedComponent>>();
+//		long start = System.currentTimeMillis();
+//		for(ConnectedComponent outer: components){
+//			Rectangle outerBox = outer.calculateRegularBoundingBox();
+//			if(outer.calculateArea() < MINIMUM_AREA) continue;
+////			output.drawShape(outerBox, RGBColour.RED);
+//			TreeNode<ConnectedComponent> outerNode = null;
+//			for(ConnectedComponent inner : components){
+//				if(inner == outer) continue;
+//				if(inner.calculateArea() < MINIMUM_AREA) continue;
+//				Rectangle innerBox = inner.calculateRegularBoundingBox();
+//				if(innerBox.isInside(outerBox)){
+//					TreeNode<ConnectedComponent> innerNode = null;
+//				}
+//			}
+//		}
+//		long end = System.currentTimeMillis();
+//		System.out.println("Total time: " + (end - start));
+//		for (Pair<ConnectedComponent> pair : containsList) {
+//			System.out.println(pair);
+//		}
+////		DisplayUtilities.displayName(output, "labeled");
+//	}
+//	
+//	public static void main(String[] args) throws IOException {
+//		String markerFile = "/Users/ss/Desktop/marker-profile.jpeg";
+//		FImage markerImage = ImageUtilities.readF(new File(markerFile));
+//		
+//		ConnectedComponentLabeler.DEFAULT_ALGORITHM = ConnectedComponentLabeler.Algorithm.FLOOD_FILL;
+//		ConnectedComponentLabeler labler = new ConnectedComponentLabeler(ConnectMode.CONNECT_8);
+//	}
 }
