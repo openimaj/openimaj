0fd0043fe8f3777fe3cac2de6dc2bd6fe18d4d46
Sina Samangooei <sinjax@gmail.com>
2011-10-31 23:25:51 +0000

    fixed connected component hierarchy

--- image/image-feature-extraction/src/main/java/org/openimaj/image/connectedcomponent/ConnectedComponentHierarchy.java
+++ image/image-feature-extraction/src/main/java/org/openimaj/image/connectedcomponent/ConnectedComponentHierarchy.java
@@ -3,6 +3,7 @@ package org.openimaj.image.connectedcomponent;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -19,49 +20,116 @@ import org.openimaj.math.geometry.shape.Polygon;
 import org.openimaj.math.geometry.shape.Rectangle;
 import org.openimaj.util.pair.Pair;
 import org.openimaj.util.tree.TreeNode;
+import org.openimaj.util.tree.TreeNodeImpl;
+
+
+class ChildWeightedTreeNode<T> extends TreeNodeImpl<T> implements Comparable<ChildWeightedTreeNode<T>>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5430956987739017813L;
+	List<ChildWeightedTreeNode<T>> parents = new ArrayList<ChildWeightedTreeNode<T>>();
+
+	@Override
+	public int compareTo(ChildWeightedTreeNode<T> that) {
+		return this.children.size() > that.children.size() ? -1 : 1;
+	}
+	
+	public String toString(){
+		return String.format("Got %d children", this.children.size());
+	}
+
+	public void addParent(ChildWeightedTreeNode<T> outerNode) {
+		this.parents.add(outerNode);
+	}
+}
 
 public class ConnectedComponentHierarchy {
-//	private static final int MINIMUM_AREA = 5;
-//	private ConnectedComponentLabeler labler;
-//	
-//	public ConnectedComponentHierarchy(ConnectedComponentLabeler labeler, float threshold){
-//		this.labler = labeler;
-//	}
-//	
-//	public TreeNode<ConnectedComponent> hierarchy(FImage markerImage){
-//		markerImage.threshold(0.5f);
-//		List<ConnectedComponent> components = labler.findComponents(markerImage);
-//		List<ConnectedComponent> invComponents = labler.findComponents(markerImage.inverse());
-//		components.addAll(invComponents);
+	private static final float MINIMUM_HEIGHT = 5;
+	private static final float MINIMUM_WIDTH = 5;
+	private int MINIMUM_AREA = 5;
+	private ConnectedComponentLabeler labler;
+	
+	public ConnectedComponentHierarchy(ConnectedComponentLabeler labeler, int threshold){
+		this.labler = labeler;
+		this.MINIMUM_AREA = threshold;
+	}
+	
+	public TreeNode<ConnectedComponent> hierarchy(FImage markerImage){
+		long start = System.currentTimeMillis();
+		markerImage.threshold(0.5f);
+		List<ConnectedComponent> components = labler.findComponents(markerImage);
+		List<ConnectedComponent> invComponents = labler.findComponents(markerImage.inverse());
+		components.addAll(invComponents);
 //		List<Pair<ConnectedComponent>> containsList = new ArrayList<Pair<ConnectedComponent>>();
-//		long start = System.currentTimeMillis();
-//		for(ConnectedComponent outer: components){
-//			Rectangle outerBox = outer.calculateRegularBoundingBox();
-//			if(outer.calculateArea() < MINIMUM_AREA) continue;
-////			output.drawShape(outerBox, RGBColour.RED);
-//			TreeNode<ConnectedComponent> outerNode = null;
-//			for(ConnectedComponent inner : components){
-//				if(inner == outer) continue;
-//				if(inner.calculateArea() < MINIMUM_AREA) continue;
-//				Rectangle innerBox = inner.calculateRegularBoundingBox();
-//				if(innerBox.isInside(outerBox)){
-//					TreeNode<ConnectedComponent> innerNode = null;
-//				}
-//			}
-//		}
-//		long end = System.currentTimeMillis();
-//		System.out.println("Total time: " + (end - start));
-//		for (Pair<ConnectedComponent> pair : containsList) {
-//			System.out.println(pair);
-//		}
-////		DisplayUtilities.displayName(output, "labeled");
-//	}
-//	
-//	public static void main(String[] args) throws IOException {
-//		String markerFile = "/Users/ss/Desktop/marker-profile.jpeg";
-//		FImage markerImage = ImageUtilities.readF(new File(markerFile));
-//		
+		List<ChildWeightedTreeNode<ConnectedComponent>> nodeList = new ArrayList<ChildWeightedTreeNode<ConnectedComponent>>();
+		for (int i = 0; i < components.size(); i++) {
+			ConnectedComponent ith = components.get(i);
+			Rectangle ithPoly = ith.calculateRegularBoundingBox();
+			if(ithPoly.calculateArea() < MINIMUM_AREA || ithPoly.x < MINIMUM_WIDTH || ithPoly.y < MINIMUM_HEIGHT) {
+				components.remove(i);
+				i--;
+				continue;
+			}
+			ChildWeightedTreeNode<ConnectedComponent> node = new ChildWeightedTreeNode<ConnectedComponent>();
+			node.value = ith;
+			nodeList.add(node);
+		}
+		
+//		MBFImage forDisplay = new MBFImage(markerImage.clone(),markerImage.clone(),markerImage.clone());
+//		SegmentationUtilities.renderSegments(forDisplay, components);
+//		DisplayUtilities.display(forDisplay);
+		
+		int i = 0;
+		for(ConnectedComponent outer: components){
+			Rectangle outerBox = outer.calculateRegularBoundingBox();
+			ChildWeightedTreeNode<ConnectedComponent> outerNode = nodeList.get(i);
+			int j = 0;
+			for(ConnectedComponent inner : components){
+				if(inner != outer){
+					Rectangle innerBox = inner.calculateRegularBoundingBox();
+					if(outerBox.isInside(innerBox)){
+						ChildWeightedTreeNode<ConnectedComponent> innerNode = nodeList.get(j);
+						outerNode.addChild(innerNode);
+						innerNode.addParent(outerNode);	
+					}
+				}
+				j++;
+			}
+			i++;
+		}
+		
+		Collections.sort(nodeList);
+		TreeNode<ConnectedComponent> root = new ChildWeightedTreeNode<ConnectedComponent>();
+		for (ChildWeightedTreeNode<ConnectedComponent> node : nodeList) {
+			// part of the root if it has no parents
+			if(node.parents.size() == 0){
+				root.addChild(node);
+			}
+			for(i = 0; i < node.children.size(); i++){
+				ChildWeightedTreeNode<ConnectedComponent> child = (ChildWeightedTreeNode<ConnectedComponent>) node.children.get(i); 
+				if(child.parents.size() == 1){
+					// This child is a direct child, do nothing
+				}
+				else{
+					child.parents.remove(node); // EXPENSIVE (FIXME)
+					node.children.remove(i--); // cheap
+				}
+			}
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("Total time: " + (end - start));
+		return root;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		String markerFile = "/Users/ss/Desktop/marker-profile.jpeg";
+		FImage markerImage = ImageUtilities.readF(new File(markerFile));
+		
 //		ConnectedComponentLabeler.DEFAULT_ALGORITHM = ConnectedComponentLabeler.Algorithm.FLOOD_FILL;
-//		ConnectedComponentLabeler labler = new ConnectedComponentLabeler(ConnectMode.CONNECT_8);
-//	}
+		ConnectedComponentLabeler labler = new ConnectedComponentLabeler(ConnectMode.CONNECT_4);
+		ConnectedComponentHierarchy hier = new ConnectedComponentHierarchy(labler,20);
+		hier.hierarchy(markerImage);
+	}
 }
