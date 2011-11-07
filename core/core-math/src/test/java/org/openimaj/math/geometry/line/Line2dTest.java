ed011914ad9b52032e0c6e30c1da6be2453cdbdc
David Duplaw <david.dupplaw@wds.co>
2011-11-07 13:08:49 +0000

    * Fixed Line2dTest

--- core/core-math/src/test/java/org/openimaj/math/geometry/line/Line2dTest.java
+++ core/core-math/src/test/java/org/openimaj/math/geometry/line/Line2dTest.java
@@ -107,14 +107,14 @@ public class Line2dTest {
 				line1Norm.calculateHorizontalAngle() )*RAD2DEG, 1d );
 		
 		// Get the normal of the first line but going through a specific point
-		Line2d line1Norm2 = line1.getNormal( new Point2dImpl( 50, 0 ) );
+		Line2d line1Norm2 = line1.getNormal( new Point2dImpl( 10, 10 ) );
 		System.out.println( line1Norm2 );
 		
 		// The normal should be at 90 degrees...
-//		assertEquals( 90, Math.abs( line1.calculateHorizontalAngle() - 
-//				line1Norm2.calculateHorizontalAngle() )*RAD2DEG, 1d );
-//		// .. and go through that given point
-//		assertTrue( line1Norm2.isOnLine( new Point2dImpl(50,0), 01.f ) );
+		assertEquals( 90, Math.abs( line1.calculateHorizontalAngle() - 
+				line1Norm2.calculateHorizontalAngle() )*RAD2DEG, 1d );
+		// .. and go through that given point
+		assertTrue( line1Norm2.isOnLine( new Point2dImpl(10,10), 01.f ) );
 		
 		// Line at some random angle
 		Line2d line2 = new Line2d( new Point2dImpl(0,0), new Point2dImpl( 50,72 ) );
