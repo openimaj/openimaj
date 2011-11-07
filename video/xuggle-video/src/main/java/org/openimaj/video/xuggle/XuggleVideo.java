c6d6b15a4dd235fe28ca8062d3f04a7ab4432967
Sina Samangooei <sinjax@gmail.com>
2011-11-07 19:46:06 +0000

    Fixed the XuggleVideo seek method, should seek properly to a frame very close to the seconds requested. not perfect.

--- video/xuggle-video/src/main/java/org/openimaj/video/xuggle/XuggleVideo.java
+++ video/xuggle-video/src/main/java/org/openimaj/video/xuggle/XuggleVideo.java
@@ -404,6 +404,8 @@ public class XuggleVideo extends Video<MBFImage>
 	
 	@Override
 	public void seek(double timestamp){
+		
+		// Based on the code of this class: http://www.google.com/codesearch#DzBPmFOZfmA/trunk/0.5/unstable/videoplayer/src/classes/org/jdesktop/wonderland/modules/videoplayer/client/VideoPlayerImpl.java&q=seekKeyFrame%20position&type=cs
 		// using the timebase, calculate the time in timebase units requested
 		double timebase = this.reader.getContainer().getStream(this.streamIndex).getTimeBase().getDouble();
 		long position = (long) (timestamp/timebase) ;
