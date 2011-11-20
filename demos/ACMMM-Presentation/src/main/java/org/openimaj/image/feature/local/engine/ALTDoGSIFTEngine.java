d9bc44e3d1e16c71ab1cf3c702e99ac421d6afb6
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2011-11-20 12:08:04 +0000

    code tidy

--- demos/ACMMM-Presentation/src/main/java/org/openimaj/image/feature/local/engine/ALTDoGSIFTEngine.java
+++ demos/ACMMM-Presentation/src/main/java/org/openimaj/image/feature/local/engine/ALTDoGSIFTEngine.java
@@ -2,21 +2,17 @@ package org.openimaj.image.feature.local.engine;
 
 import org.openimaj.feature.local.list.LocalFeatureList;
 import org.openimaj.image.FImage;
-import org.openimaj.image.feature.local.descriptor.gradient.IrregularBinningSIFTFeatureProvider;
 import org.openimaj.image.feature.local.descriptor.gradient.SIFTFeatureProvider;
 import org.openimaj.image.feature.local.detector.dog.collector.Collector;
 import org.openimaj.image.feature.local.detector.dog.collector.OctaveKeypointCollector;
-import org.openimaj.image.feature.local.detector.dog.extractor.DominantOrientationExtractor;
 import org.openimaj.image.feature.local.detector.dog.extractor.GradientFeatureExtractor;
 import org.openimaj.image.feature.local.detector.dog.extractor.NullOrientationExtractor;
-import org.openimaj.image.feature.local.detector.dog.extractor.OrientationHistogramExtractor;
 import org.openimaj.image.feature.local.detector.dog.pyramid.BasicOctaveExtremaFinder;
 import org.openimaj.image.feature.local.detector.dog.pyramid.DoGOctaveExtremaFinder;
 import org.openimaj.image.feature.local.detector.dog.pyramid.OctaveInterestPointFinder;
 import org.openimaj.image.feature.local.keypoints.Keypoint;
 import org.openimaj.image.processing.pyramid.gaussian.GaussianOctave;
 import org.openimaj.image.processing.pyramid.gaussian.GaussianPyramid;
-
 
 public class ALTDoGSIFTEngine extends DoGSIFTEngine {
 	@Override
