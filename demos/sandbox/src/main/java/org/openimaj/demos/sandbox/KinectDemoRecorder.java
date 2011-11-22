63946381663c00616aa15e23c22f1c9f981bba79
Sina Samangooei <sinjax@gmail.com>
2011-11-22 17:44:46 +0000

    added a class which saves a kinectdemo to a video file using XuggleVideoWriter

---
+++ demos/sandbox/src/main/java/org/openimaj/demos/sandbox/KinectDemoRecorder.java
@@ -0,0 +1,59 @@
+package org.openimaj.demos.sandbox;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.SwingUtilities;
+
+import org.openimaj.hardware.kinect.KinectDemo;
+import org.openimaj.image.MBFImage;
+import org.openimaj.video.VideoDisplay;
+import org.openimaj.video.VideoDisplayListener;
+import org.openimaj.video.xuggle.XuggleVideoWriter;
+
+public class KinectDemoRecorder implements KeyListener, VideoDisplayListener<MBFImage> {
+	
+	private KinectDemo demo;
+	private XuggleVideoWriter writer;
+	private boolean close = false;
+
+	public KinectDemoRecorder(){
+		demo = new KinectDemo(0);
+		writer = new XuggleVideoWriter("kinect.mpg",demo.getCurrentFrame().getWidth(),demo.getCurrentFrame().getHeight(),22);
+		demo.getDisplay().addVideoListener(this);
+		SwingUtilities.getRoot(demo.getDisplay().getScreen()).addKeyListener(this);
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		close = arg0.getKeyCode() == KeyEvent.VK_ESCAPE;
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		new KinectDemoRecorder();
+	}
+
+	@Override
+	public void afterUpdate(VideoDisplay<MBFImage> display) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeUpdate(MBFImage frame) {
+		if(!close)writer.addFrame(frame);
+	}
+}
