2a6731fbe1ec3d95fbfca7c3e3364dab7653b83a
Sina Samangooei <sinjax@gmail.com>
2011-11-25 15:13:51 +0000

    added the image terrier slide, similar to the openimaj slide, first complete version, woo.

--- demos/ACMMM-Presentation/src/main/java/org/openimaj/demos/acmmm11/presentation/OpenIMAJ_ACMMM2011.java
+++ demos/ACMMM-Presentation/src/main/java/org/openimaj/demos/acmmm11/presentation/OpenIMAJ_ACMMM2011.java
@@ -24,17 +24,17 @@ public class OpenIMAJ_ACMMM2011 {
 		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.001.png"))); // title slide
 		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.002.png"))); // What is OpenIMAJ
 		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.003.png"))); // What is ImageTerrier
-		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.004.png"))); // History of development
-		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.005.png"))); // Why another set of libraries?
-		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.006.png"))); // Designed to be EXTENSIBLE
-		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.007.png"))); // Alt SIFT: DoG/SIFT
-		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.008.png"))); // Alt SIFT: highlight dom ori
-		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.009.png"))); // Alt SIFT: replace with Null ori
-		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.010.png"))); // Alt SIFT: highlight SIFT feature
-		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.011.png"))); // Alt SIFT: replace with Irregular binning SIFT
-		slides.add(new SIFTAltSIFTSlide()); // Normal vs ALT demo
-		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.012.png"))); // All in java, isnt it slow?!
+		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.005.png"))); // History of development
+		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.006.png"))); // All in java, isnt it slow?!
 		slides.add(new SIFTTrackerSlide()); // live DoG/SIFT demo
+		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.007.png"))); // Why another set of libraries?
+		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.008.png"))); // Designed to be EXTENSIBLE
+		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.009.png"))); // Alt SIFT: DoG/SIFT
+		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.010.png"))); // Alt SIFT: highlight dom ori
+		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.011.png"))); // Alt SIFT: replace with Null ori
+		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.012.png"))); // Alt SIFT: highlight SIFT feature
+		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.013.png"))); // Alt SIFT: replace with Irregular binning SIFT
+		slides.add(new SIFTAltSIFTSlide()); // Normal vs ALT demo
 		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.014.png"))); // Street View Cam
 		slides.add(new PictureSlide(OpenIMAJ_ACMMM2011.class.getResource("slide.015.png")));
 		slides.add(new VideoSlide(
