9b89f552bd4e4f2deb464804d3e11039e9f0faf3
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2012-03-01 13:31:04 +0000

    Sample applet for the nine-screens

---
+++ demos/ACMMM-Presentation/src/main/java/org/openimaj/demos/acmmm11/presentation/NineScreensApplet.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2011, The University of Southampton and the individual contributors.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *   * 	Redistributions of source code must retain the above copyright notice,
+ * 	this list of conditions and the following disclaimer.
+ *
+ *   *	Redistributions in binary form must reproduce the above copyright notice,
+ * 	this list of conditions and the following disclaimer in the documentation
+ * 	and/or other materials provided with the distribution.
+ *
+ *   *	Neither the name of the University of Southampton nor the names of its
+ * 	contributors may be used to endorse or promote products derived from this
+ * 	software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.openimaj.demos.acmmm11.presentation;
+
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.JApplet;
+
+import org.openimaj.content.slideshow.Slide;
+import org.openimaj.content.slideshow.SlideshowApplet;
+import org.openimaj.demos.acmmm11.presentation.slides.TutorialSlide;
+
+/**
+ * Applet for the Nine-Screens
+ * 
+ * @author Jonathon Hare <jsh2@ecs.soton.ac.uk>
+ */
+public class NineScreensApplet extends JApplet {
+	private static final long serialVersionUID = 1L;
+
+	/* (non-Javadoc)
+	 * @see java.applet.Applet#init()
+	 */
+	@Override
+	public void init() {
+		try {
+			List<Slide> slides = new ArrayList<Slide>();
+			slides.add(new TutorialSlide()); // Student Tutorial demo
+			
+			new SlideshowApplet(this, slides, 1024, 768, ImageIO.read(NineScreensApplet.class.getResourceAsStream("background.png"))) {
+				@Override
+				public void setFullscreen(final boolean fullscreen) {
+					//do nothing
+				}
+				
+				@Override
+				public void keyReleased(KeyEvent e) {
+					//do nothing
+				}
+			};
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
