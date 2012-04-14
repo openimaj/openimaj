2fab12e7f27c3535933eb0364b3bb09d8c6230ed
Sina Samangooei <sinjax@gmail.com>
2012-04-14 21:30:10 +0000

    missing file

---
+++ machine-learning/machine-learning/src/main/java/org/openimaj/ml/timeseries/series/DoubleTimeSeriesCollection.java
@@ -0,0 +1,80 @@
+package org.openimaj.ml.timeseries.series;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+import org.openimaj.io.ReadWriteableASCII;
+import org.openimaj.ml.timeseries.TimeSeriesCollection;
+import org.openimaj.ml.timeseries.TimeSeriesSetException;
+
+/**
+ * A set of time {@link DoubleTimeSeries} which may not be synchronised.
+ * @author ss
+ *
+ */
+public class DoubleTimeSeriesCollection extends TimeSeriesCollection<double[], Double, DoubleTimeSeriesCollection,DoubleTimeSeries> implements ReadWriteableASCII{
+
+	@Override
+	public DoubleTimeSeries internalNewInstance() {
+		return new DoubleTimeSeries();
+	}
+
+	@Override
+	public DoubleTimeSeriesCollection newInstance() {
+		return new DoubleTimeSeriesCollection();
+	}
+	
+	@Override
+	public Map<String, double[]> getData() {
+		Map<String, double[]> ret = new HashMap<String, double[]>();
+		for (Entry<String, DoubleTimeSeries>  es : this.timeSeriesHolder.entrySet()) {
+			ret.put(es.getKey(), es.getValue().getData());
+		}
+		return ret;
+	}
+	
+	@Override
+	public void internalAssign(DoubleTimeSeriesCollection interpolate) {
+		this.timeSeriesHolder = interpolate.timeSeriesHolder;
+		
+	}
+
+	@Override
+	public String toString() {
+		String retstr = "A set time series: " + this.timeSeriesHolder.size() + "\n";
+		for (Entry<String, DoubleTimeSeries> dts : this.timeSeriesHolder.entrySet()) {
+			retstr += dts.getKey() + "\n";
+			retstr += dts.getValue() + "\n";
+		}
+		return retstr;
+	}
+
+	@Override
+	public void readASCII(Scanner in) throws IOException {
+		
+		while(in.hasNext()){
+			String name = in.nextLine();
+			DoubleTimeSeries v = internalNewInstance();
+			v.readASCII(in);
+			in.nextLine();
+			this.timeSeriesHolder.put(name,v);
+		}
+	}
+
+	@Override
+	public String asciiHeader() {
+		return "";
+	}
+
+	@Override
+	public void writeASCII(PrintWriter out) throws IOException {
+		for (Entry<String, DoubleTimeSeries> es : this.timeSeriesHolder.entrySet()) {
+			out.println(es.getKey());
+			es.getValue().writeASCII(out);
+		}
+	}
+}
