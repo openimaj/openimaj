a71a1680c22543a062e91ca6e85f72e7519e8219
Sina Samangooei <sinjax@gmail.com>
2012-05-21 11:36:00 +0000

    fixesd a UTF problem in the documentation

--- web/twitter/src/main/java/org/openimaj/ml/sketch/Sketch.java
+++ web/twitter/src/main/java/org/openimaj/ml/sketch/Sketch.java
@@ -3,7 +3,7 @@ package org.openimaj.ml.sketch;
 /**
  * As described by: http://lkozma.net/blog/sketching-data-structures/
  * 
- * “Sketching” data structures store a summary of a data set in situations where the whole 
+ * "Sketching" data structures store a summary of a data set in situations where the whole 
  * data would be prohibitively costly to store (at least in a fast-access place like the memory 
  * as opposed to the hard disk).
  * 
