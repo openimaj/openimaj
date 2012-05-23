08e1fdd45a9bf3eba4d27b3e50e47a09d9ec8ae6
Sina Samangooei <sinjax@gmail.com>
2012-05-23 16:18:12 +0000

    missing file

---
+++ thirdparty/klt-tracker/src/main/java/org/openimaj/video/tracking/klt/PyramidSet.java
@@ -0,0 +1,49 @@
+package org.openimaj.video.tracking.klt;
+
+import org.openimaj.image.FImage;
+import org.openimaj.image.processing.convolution.FGaussianConvolve;
+
+/**
+ * A common set of objects, namely the gaussian pyramid for an image and the gradients of each
+ * level of the pyramid
+ * @author ss
+ *
+ */
+public class PyramidSet{
+	/**
+	 * @param image
+	 * @param tc
+	 */
+	public PyramidSet(FImage image, TrackingContext tc) {
+		int nrows = image.height, ncols = image.width;
+		FImage floatimg2 = image.process(new FGaussianConvolve(tc.computeSmoothSigma()));
+		this.imgPyr = new Pyramid(ncols, nrows, (int) tc.subsampling, tc.nPyramidLevels);
+		this.imgPyr.computePyramid(floatimg2, tc.pyramid_sigma_fact);
+		this.gradx = new Pyramid(ncols, nrows, (int) tc.subsampling, tc.nPyramidLevels);
+		this.grady = new Pyramid(ncols, nrows, (int) tc.subsampling, tc.nPyramidLevels);
+		for (int i = 0 ; i < tc.nPyramidLevels ; i++)
+			tc.computeGradients(imgPyr.img[i], tc.grad_sigma, gradx.img[i], grady.img[i]);
+	}
+	/**
+	 * @param imgPyr
+	 * @param gradx
+	 * @param grady
+	 */
+	public PyramidSet(Pyramid imgPyr, Pyramid gradx,Pyramid grady) {
+		this.imgPyr = imgPyr;
+		this.gradx = gradx;
+		this.grady = grady;
+	}
+	/**
+	 * the image pyramid
+	 */
+	public Pyramid imgPyr;
+	/**
+	 * the x gradient pyramid
+	 */
+	public Pyramid gradx;
+	/**
+	 * the y gradient pyramid
+	 */
+	public Pyramid grady;
+}
