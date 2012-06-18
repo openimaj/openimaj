1bf2992fa3574d472ee7ff163e57d265b66de1e5
David Duplaw <david.dupplaw@wds.co>
2012-06-18 14:24:28 +0000

    * Added an implementation of the SUSAN edge detector * Also added a simple test into the sandbox for it.

---
+++ demos/sandbox/src/main/java/org/openimaj/demos/sandbox/image/SusanTest.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package org.openimaj.demos.sandbox.image;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.processing.edges.SUSANEdgeDetector;
+import org.openimaj.video.VideoDisplay;
+import org.openimaj.video.VideoDisplayListener;
+import org.openimaj.video.capture.VideoCapture;
+
+/**
+ * 	Simple example of the SUSAN edge detector.
+ * 
+ *  @author David Dupplaw <dpd@ecs.soton.ac.uk>
+ *	@version $Author$, $Revision$, $Date$
+ *	@created 18 Jun 2012
+ */
+public class SusanTest
+{
+	/**
+	 *  @param args
+	 * 	@throws IOException 
+	 */
+	public static void main( String[] args ) throws IOException
+    {
+		final SUSANEdgeDetector ss = new SUSANEdgeDetector();
+		
+		// Process an image
+		FImage fi = ImageUtilities.readF( new URL( 
+			"http://upload.wikimedia.org/wikipedia/commons/6/6e/Similar-geometric-shapes.png" ) );
+		DisplayUtilities.display( fi.process( ss ) );
+		DisplayUtilities.display( SUSANEdgeDetector.smoothCircularSusan( fi, 0.01, 24, 3.8 ) );
+		
+		// Process a video with the simple SUSAN detector
+		VideoCapture vc = new VideoCapture( 640, 480 );
+		VideoDisplay<MBFImage> vd = VideoDisplay.createVideoDisplay( vc );
+		vd.addVideoListener( new VideoDisplayListener<MBFImage>()
+		{
+			@Override
+			public void beforeUpdate( MBFImage frame )
+			{
+				frame.processInline( ss );
+			}
+			
+			@Override
+			public void afterUpdate( VideoDisplay<MBFImage> display )
+			{
+			}
+		});		
+    }
+}
---
+++ image/image-processing/src/main/java/org/openimaj/image/processing/edges/SUSANEdgeDetector.java
@@ -0,0 +1,270 @@
+/**
+ * 
+ */
+package org.openimaj.image.processing.edges;
+
+import gnu.trove.list.array.TFloatArrayList;
+
+import org.openimaj.citation.annotation.Reference;
+import org.openimaj.citation.annotation.ReferenceType;
+import org.openimaj.image.FImage;
+import org.openimaj.image.processor.SinglebandImageProcessor;
+
+/**
+ * 	Implementations of the SUSAN edge detection algorithm. The default
+ * 	processor uses the simple version; there are static methods for the
+ * 	other versions which are pretty slow. However, the circular version allows
+ * 	you to detect "fat" lines which would otherwise be detected as two
+ * 	separate lines.
+ * 
+ *  @author David Dupplaw <dpd@ecs.soton.ac.uk>
+ *	@version $Author$, $Revision$, $Date$
+ *	@created 18 Jun 2012
+ */
+@Reference(
+		author = { "S.M. Smith" }, 
+		title = "A new class of corner finder",
+		type = ReferenceType.Article,
+		url = "http://users.fmrib.ox.ac.uk/~steve/susan/susan/node4.html",
+		year = "1992",
+		booktitle = "Proc. 3rd British Machine Vision Conference",
+		pages = "139-148"
+)
+public class SUSANEdgeDetector implements
+        SinglebandImageProcessor<Float, FImage>
+{
+	/**
+	 * 	An enumerator of different SUSAN edge detector types	
+	 * 
+	 *  @author David Dupplaw <dpd@ecs.soton.ac.uk>
+	 *	@version $Author$, $Revision$, $Date$
+	 *	@created 18 Jun 2012
+	 */
+	private enum SUSANDetector
+	{
+		/**
+		 * 	The simple, fast SUSAN detector
+		 */
+		SIMPLE
+		{
+			@Override
+            public FImage process( FImage img )
+            {
+				return SUSANEdgeDetector.simpleSusan( img, threshold, nmax );
+            }
+		},
+		/**
+		 * 	The smooth SUSAN detector
+		 */
+		SMOOTH
+		{
+			@Override
+            public FImage process( FImage img )
+            {
+				return SUSANEdgeDetector.smoothSusan( img, threshold, nmax );
+            }
+		},
+		/**
+		 * 	The smooth, circular detector
+		 */
+		CIRCULAR
+		{
+			@Override
+            public FImage process( FImage img )
+            {
+				return SUSANEdgeDetector.smoothCircularSusan( img, threshold, nmax, radius );
+            }			
+		}
+		;
+		
+		protected double threshold = 0.08;
+		protected double nmax = 9;
+		protected double radius = 3.4;
+		
+		public abstract FImage process( FImage img );
+	}
+	
+	/** The SUSAN detector in use */
+	private SUSANDetector susan = SUSANDetector.SIMPLE;
+
+	/**
+	 * 	Default constructor that instantiates a simple SUSAN edge detector
+	 * 	with threshold 0.08 and global threshold weighting 9.
+	 */
+	public SUSANEdgeDetector()
+    {
+		this.susan = SUSANDetector.SIMPLE;
+    }
+	
+	/**
+	 * 	@param s The susan detector to use
+	 *  @param threshold The threshold to use
+	 *  @param nmax The global threshold weighting
+	 */
+	public SUSANEdgeDetector( SUSANDetector s, double threshold, 
+			double nmax )
+    {
+		this.susan = s;
+		susan.threshold = threshold;
+		susan.nmax = nmax;
+    }
+
+	/**
+	 * 	@param s The susan detector to use
+	 *  @param threshold The threshold to use
+	 *  @param nmax The global threshold weighting
+	 *  @param radius The radius of the circular susan
+	 */
+	public SUSANEdgeDetector( SUSANDetector s, double threshold, 
+			double nmax, double radius )
+    {
+		this.susan = s;
+		susan.threshold = threshold;
+		susan.nmax = nmax;
+		susan.radius = radius;
+    }
+	
+	/**
+	 *	{@inheritDoc}
+	 * 	@see org.openimaj.image.processor.ImageProcessor#processImage(org.openimaj.image.Image)
+	 */
+	@Override
+    public void processImage( FImage image )
+    {
+		image.internalAssign( susan.process( image ) );
+    }
+	
+	/**
+	 * 	Performs the simple SUSAN edge detection.
+	 * 	
+	 *  @param img The image to find edges in
+	 *  @param thresh The threshold
+	 *  @param nmax The global threshold weighting
+	 *  @return Edge image
+	 */
+	public static FImage simpleSusan( FImage img, double thresh, double nmax )
+	{
+	    FImage area = new FImage( img.getWidth(), img.getHeight() );
+	    
+	    double globalThresh = (3.0 * nmax) / 4.0;
+	    
+        for( int y = 1; y < img.getHeight()-1; y++ )
+	    {
+    	    for( int x = 1; x < img.getWidth()-1; x++ )
+	        {
+	            double a = 0;
+	            for( int x1 = x-1; x1 < x+2; x1++ )
+	            {
+	                for( int y1 = y-1; y1 < y+2; y1++ )
+	                {
+	                    if( Math.abs( img.getPixel(x1,y1) - img.getPixel(x,y) ) < thresh )
+	                        a++;
+	                }
+	            }
+	            
+	            if( a < globalThresh )
+	                area.setPixel( x, y, (float)(globalThresh - a) );
+	        }
+	    }
+	    
+	    return area;
+	}
+	
+	/**
+	 * 	Performs the simple SUSAN edge detection.
+	 * 	
+	 *  @param img The image to find edges in
+	 *  @param thresh The threshold
+	 *  @param nmax The global threshold weighting
+	 *  @return Edge image
+	 */
+	public static FImage smoothSusan( FImage img, double thresh, double nmax )
+	{
+	    FImage area = new FImage( img.getWidth(), img.getHeight() );
+	    
+	    double globalThresh = (3.0 * nmax) / 4.0;
+	    
+        for( int y = 1; y < img.getHeight()-1; y++ )
+	    {
+    	    for( int x = 1; x < img.getWidth()-1; x++ )
+	        {
+	            double a = 0;
+	            for( int x1 = x-1; x1 < x+2; x1++ )
+	            {
+	                for( int y1 = y-1; y1 < y+2; y1++ )
+	                {
+                        a += Math.exp( 
+                        		-Math.pow( 
+                        			Math.abs( img.getPixel(x1,y1) - 
+                        					img.getPixel(x,y) )
+                        				/thresh, 6 ) );
+	                }
+	            }
+	            
+	            if( a < globalThresh )
+	                area.setPixel( x, y, (float)(globalThresh - a) );
+	        }
+	    }
+	    
+	    return area;
+	}
+
+	/**
+	 * 	Performs the simple SUSAN edge detection.
+	 * 	
+	 *  @param img The image to find edges in
+	 *  @param thresh The threshold
+	 *  @param nmax The global threshold weighting
+	 *  @param radius The radius of the circle (try 3.4)
+	 *  @return Edge image
+	 */
+	public static FImage smoothCircularSusan( FImage img, double thresh, double nmax, double radius )
+	{
+	    FImage area   = new FImage( img.getWidth(), img.getHeight() );
+	    double globalThresh = (3.0 * nmax) / 4.0;
+	    
+	    int r = (int)Math.ceil(radius);
+        for( int y = r; y < img.getHeight()-r; y++ )
+	    {
+    	    for( int x = r; x < img.getWidth()-r; x++ )
+	        {
+    	    	float[] pixelValues = getPixelsInCircle( x, y, radius, img );
+    	    	double a = 0;
+    	    	for( float f : pixelValues )    	    		
+                        a += Math.exp( 
+                        		-Math.pow( 
+                        			Math.abs( f - 
+                        					img.getPixel(x,y) )
+                        				/thresh, 6 ) );
+	            
+	            if( a < globalThresh )
+	                area.setPixel( x, y, (float)(globalThresh - a) );
+	        }
+	    }
+	    
+	    return area;
+	}
+
+	/**
+	 * 	Returns the values of pixels within a circle centres at cx, cy in the
+	 * 	image img, with a radius r.
+	 *  @param cx The centre of the circle's x coordinate
+	 *  @param cy The centre of the circle's y coordinate
+	 *  @param r The radius of the circle
+	 *  @param img The image from which to take pixels
+	 *  @return A list of pixel values
+	 */
+	private static float[] getPixelsInCircle( int cx, int cy, double r, FImage img )
+	{
+		TFloatArrayList f = new TFloatArrayList();
+	    for( int i  = (int)Math.ceil(cx-r); i < (int)Math.ceil(cx+r); i++ )
+	    {
+	        double ri = Math.sqrt( r*r-(i-cx)*(i-cx) );
+	        for( int j = (int)Math.ceil(cy-ri); j < (int)Math.ceil(cy+ri); j++ )
+	        {
+	        	f.add( img.getPixel(i,j) );
+	        }
+	    }
+	    return f.toArray();
+	}
+}
