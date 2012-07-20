a69d285bf7ffb4bab4066f7b87353ba07c1719d2
Sina Samangooei <sinjax@gmail.com>
2012-07-20 15:38:53 +0000

    some fixes to yago 2 lucene index

--- text/nlp/src/main/java/org/openimaj/text/nlp/namedentity/YagoLuceneBuilder.java
+++ text/nlp/src/main/java/org/openimaj/text/nlp/namedentity/YagoLuceneBuilder.java
@@ -1,8 +1,10 @@
 package org.openimaj.text.nlp.namedentity;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.openimaj.io.FileUtils;
 import org.openimaj.text.nlp.namedentity.YagoCompanyIndexFactory.YagoCompanyIndex;
 
 public class YagoLuceneBuilder {
@@ -10,19 +12,42 @@ public class YagoLuceneBuilder {
 	private static String indexPath = "src/main/resources/org/openimaj/text/namedentity/yagolucene";
 
 	
-	public static void main(String[] args) {
-		File f = new File(indexPath);
-		if(f.isDirectory()){
-			for(File ff : f.listFiles()){
-				ff.delete();
+	/**
+	 * Validate the (local) ouput from an String and return the 
+	 * corresponding file.
+	 * 
+	 * @param out where the file will go
+	 * @param overwrite whether to overwrite existing files
+	 * @param contin whether an existing output should be continued (i.e. ignored if it exists)
+	 * @return the output file location, deleted if it is allowed to be deleted
+	 * @throws IOException if the file exists, but can't be deleted
+	 */
+	public static File validateLocalOutput(String out, boolean overwrite, boolean contin) throws IOException {
+		if(out == null){
+			throw new IOException("No output specified");
+		}
+		File output = new File(out);
+		if(output.exists()){
+			if(overwrite){
+				if(!FileUtils.deleteRecursive(output)) throw new IOException("Couldn't delete existing output");
 			}
-			try {
-				YagoCompanyIndex yci = YagoCompanyIndexFactory.createFromSparqlEndPoint(YagoCompanyIndexFactory.YAGO_SPARQL_ENDPOINT, indexPath);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			else if(!contin){
+				throw new IOException("Output already exists, didn't remove");
 			}
-		}		
+		}
+		return output;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		File f = validateLocalOutput(indexPath,true,false);
+		f.mkdirs();
+		try {
+			YagoCompanyIndex yci = YagoCompanyIndexFactory.createFromSparqlEndPoint(YagoCompanyIndexFactory.YAGO_SPARQL_ENDPOINT, indexPath);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
