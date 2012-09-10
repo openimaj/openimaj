34ec3a901f06374009bf1e194507b0b98488cf74
Sina Samangooei <sinjax@gmail.com>
2012-09-10 16:47:47 +0000

    fixed a strange bug in picslurper which resulted in a memory leak in twitter4j if onStatus returned slowly. For some bigger images it would do so resulting in a big back log of tweets to process. I've tentatively solved the problem with a synchronised bounded priority queue but this might not scale well. The true solution might be to process the damn stream faster, i.e., more machines, more threads

--- tools/picslurper/src/main/java/org/openimaj/picslurper/Twitter4JStreamFeeder.java
+++ tools/picslurper/src/main/java/org/openimaj/picslurper/Twitter4JStreamFeeder.java
@@ -1,5 +1,9 @@
 package org.openimaj.picslurper;
 
+import java.util.Comparator;
+
+import org.openimaj.util.queue.BoundedPriorityQueue;
+
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
@@ -9,47 +13,105 @@ import twitter4j.TwitterStreamFactory;
 import twitter4j.auth.AccessToken;
 
 /**
- * Uses the {@link TwitterStreamFactory} of twitter4j and oAuth using 
- * 
+ * Uses the {@link TwitterStreamFactory} of twitter4j and oAuth using
+ *
  * @author Jonathan Hare (jsh2@ecs.soton.ac.uk), Sina Samangooei (ss@ecs.soton.ac.uk), David Duplaw (dpd@ecs.soton.ac.uk)
  *
  */
 public class Twitter4JStreamFeeder implements StatusFeeder {
+	private final class StatusTimeComparator implements Comparator<Status>{
+
+		@Override
+		public int compare(Status o1, Status o2) {
+			if(o2 == null && o1 == null){
+				return 0;
+			}
+			if(o2 == null){
+				return -1;
+			}
+			if(o1 == null){
+				return 1;
+			}
+
+			long id1 = o1.getId() ;
+			long id2 = o2.getId() ;
+			if(id1 < id2){
+				return -1;
+			}
+			else if(id1 > id2){
+				return 1;
+			}
+
+			return 0;
+		}
+
+	}
+	private final class PriorityQueueStatusListener implements StatusListener {
+		private BoundedPriorityQueue<Status> queue;
+
+		public PriorityQueueStatusListener(final PicSlurper slurper) {
+			this.queue = new BoundedPriorityQueue<Status>(1000, new StatusTimeComparator());
+			// Start a thread which feeds the slurper from the queue
+			new Thread(new Runnable(){
+				@Override
+				public void run() {
+					while(true){
+						Status s = nextStatus();
+						if(s!=null){
+							slurper.handleStatus(s);
+						}
+					}
+				}
+
+			}).start();
+
+		}
+		@Override
+		public synchronized void onStatus(Status status) {
+			queue.add(status);
+		}
+
+		public synchronized Status nextStatus(){
+			return queue.poll();
+		}
+
+		@Override
+		public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
+
+		@Override
+		public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
+
+		@Override
+		public void onException(Exception ex) {
+		    ex.printStackTrace();
+		}
+
+		@Override
+		public void onScrubGeo(long userId, long upToStatusId) {
+			// TODO Auto-generated method stub
+
+		}
+	}
 	private AccessToken accessToken;
 	/**
-	 * @throws TwitterException 
-	 * 
+	 * @throws TwitterException
+	 *
 	 */
 	public Twitter4JStreamFeeder() throws TwitterException {
 //		Twitter twitter = new TwitterFactory().getInstance();
 //		twitter.setOAuthConsumer(
-//			System.getProperty("twitter4j.oauth.consumerKey"), 
+//			System.getProperty("twitter4j.oauth.consumerKey"),
 //			System.getProperty("twitter4j.oauth.consumerSecret")
 //		);
 //		RequestToken requestToken = twitter.getOAuthRequestToken();
 		this.accessToken = new AccessToken(
-			System.getProperty("twitter4j.oauth.accessKey"), 
+			System.getProperty("twitter4j.oauth.accessKey"),
 			System.getProperty("twitter4j.oauth.accessSecret")
 		);
 	}
 	@Override
 	public void feedStatus(final PicSlurper slurper) {
-		StatusListener listener = new StatusListener(){
-	        public void onStatus(Status status) {
-	        	slurper.handleStatus(status);
-	        }
-	        
-			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
-	        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
-	        public void onException(Exception ex) {
-	            ex.printStackTrace();
-	        }
-			@Override
-			public void onScrubGeo(long userId, long upToStatusId) {
-				// TODO Auto-generated method stub
-				
-			}
-	    };
+		StatusListener listener = new PriorityQueueStatusListener(slurper);
 		TwitterStream twitterStream = new TwitterStreamFactory().getInstance(accessToken);
 		twitterStream.addListener(listener);
 		twitterStream.sample();
