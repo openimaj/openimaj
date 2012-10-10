a1e6258aa384aab99771ef4909dc339ae0bbe866
Sina Samangooei <sinjax@gmail.com>
2012-10-10 15:54:39 +0000

    fixed the picslurper tests

--- tools/picslurper/src/test/java/org/openimaj/picslurper/TestPicSlurper.java
+++ tools/picslurper/src/test/java/org/openimaj/picslurper/TestPicSlurper.java
@@ -3,10 +3,10 @@ package org.openimaj.picslurper;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.openimaj.io.FileUtils;
@@ -17,16 +17,15 @@ import org.openimaj.picslurper.consumer.SimpleHTMLScrapingConsumer;
 import org.openimaj.picslurper.consumer.TwipleConsumer;
 import org.openimaj.picslurper.consumer.TwitPicConsumer;
 import org.openimaj.picslurper.consumer.TwitterPhotoConsumer;
-import org.openimaj.text.nlp.TweetTokeniserException;
 
 /**
  * Test the various functions of the {@link PicSlurper}
- * 
+ *
  * @author Sina Samangooei (ss@ecs.soton.ac.uk)
- * 
+ *
  */
 public class TestPicSlurper {
-
+	Logger logger = Logger.getLogger(TestPicSlurper.class);
 	/**
 	 * Turn off console login to make the tests work without stopping
 	 */
@@ -38,122 +37,144 @@ public class TestPicSlurper {
 	/**
 	 * Test whether the URLs can be translated to directories specifically
 	 * {@link StatusConsumer#urlToOutput(URL, File)}
-	 * 
-	 * @throws Exception
 	 */
 	@Test
-	public void testURLDir() throws Exception {
-		final File testOut = File.createTempFile("dir", "out");
-		testOut.delete();
-		testOut.mkdirs();
-		testOut.deleteOnExit();
+	public void testURLDir()  {
+		try{
+			final File testOut = File.createTempFile("dir", "out");
+			testOut.delete();
+			testOut.mkdirs();
+			testOut.deleteOnExit();
 
-		File out = StatusConsumer.urlToOutput(new URL("http://www.google.com"), testOut);
-		System.out.println(out);
-		out = StatusConsumer.urlToOutput(new URL("http://www.google.com/?bees"), testOut);
-		System.out.println(out);
-		out = StatusConsumer.urlToOutput(new URL("http://www.google.com/some/long/path.html?bees"), testOut);
-		System.out.println(out);
-	}
-
-	/**
-	 * Test a few URLs that seem to forward forever if not dealt with properly
-	 * 
-	 * @throws IOException
-	 */
-	@Test
-	public void testForeverForwarding() throws IOException {
-		final String[] urls = new String[] {
-				"http://www.thegatewaypundit.com/2012/08/out-of-touch/",
-				"http://i.imgur.com/Y1fMz.jpg",
-				"http://www.timlo.net/?random&random_year=2012&random_month=9&random_cat_id=2&random_cat_id=3&random_cat_id=4&random_cat_id=5&random_cat_id=6&random_cat_id=7&random_cat_id=8"
-		};
-		final StatusConsumer consumer = new StatusConsumer();
-		for (final String string : urls) {
-			consumer.add(string);
-			consumer.processAll(null);
+			File out = StatusConsumer.urlToOutput(new URL("http://www.google.com"), testOut);
+			System.out.println(out);
+			out = StatusConsumer.urlToOutput(new URL("http://www.google.com/?bees"), testOut);
+			System.out.println(out);
+			out = StatusConsumer.urlToOutput(new URL("http://www.google.com/some/long/path.html?bees"), testOut);
+			System.out.println(out);
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
 		}
 	}
 
 	/**
-	 * Test the images in /images-10.txt
-	 * 
-	 * @throws Exception
+	 * Test a few URLs that seem to forward forever if not dealt with properly
 	 */
 	@Test
-	public void testImageTweets() throws Exception {
-		final File testIn = File.createTempFile("image", ".txt");
-		final File testOut = File.createTempFile("image", "out");
-		System.out.println("output location: " + testOut);
-		testIn.delete();
-		testOut.delete();
-		FileUtils.copyStreamToFile(TestPicSlurper.class.getResourceAsStream("/images-10.txt"), testIn);
+	public void testForeverForwarding() {
+		try{
+			final String[] urls = new String[] {
+					"http://www.thegatewaypundit.com/2012/08/out-of-touch/",
+					"http://i.imgur.com/Y1fMz.jpg",
+					"http://www.timlo.net/?random&random_year=2012&random_month=9&random_cat_id=2&random_cat_id=3&random_cat_id=4&random_cat_id=5&random_cat_id=6&random_cat_id=7&random_cat_id=8"
+			};
+			final StatusConsumer consumer = new StatusConsumer();
+			for (final String string : urls) {
+				consumer.add(string);
+				consumer.processAll(null);
+			}
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
 
-		PicSlurper.main(new String[] { "-i", testIn.getAbsolutePath(), "-o", testOut.getAbsolutePath() });
+	}
+
+	/**
+	 * Test the images in /images-10.txt
+	 */
+	@Test
+	public void testImageTweets()  {
+		try{
+			final File testIn = File.createTempFile("image", ".txt");
+			final File testOut = File.createTempFile("image", "out");
+			System.out.println("output location: " + testOut);
+			testIn.delete();
+			testOut.delete();
+			FileUtils.copyStreamToFile(TestPicSlurper.class.getResourceAsStream("/images-10.txt"), testIn);
+			PicSlurper.main(new String[] { "-i", testIn.getAbsolutePath(), "-o", testOut.getAbsolutePath() });
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
+
+
 	}
 
 	/**
 	 * Check the images in /images-10.txt using storm
-	 * 
-	 * @throws Exception
 	 */
 	@Test
-	public void testImageTweetsStorm() throws Exception {
-		final File testIn = File.createTempFile("image", ".txt");
-		final File testOut = File.createTempFile("image", "out");
-		System.out.println("output location: " + testOut);
-		testIn.delete();
-		testOut.delete();
-		FileUtils.copyStreamToFile(TestPicSlurper.class.getResourceAsStream("/images-10.txt"), testIn);
+	public void testImageTweetsStorm()  {
+		try{
+			final File testIn = File.createTempFile("image", ".txt");
+			final File testOut = File.createTempFile("image", "out");
+			System.out.println("output location: " + testOut);
+			testIn.delete();
+			testOut.delete();
+			FileUtils.copyStreamToFile(TestPicSlurper.class.getResourceAsStream("/images-10.txt"), testIn);
 
-		StormPicSlurper.main(new String[] { "-i", testIn.getAbsolutePath(), "-o", testOut.getAbsolutePath() });
+			StormPicSlurper.main(new String[] { "-i", testIn.getAbsolutePath(), "-o", testOut.getAbsolutePath() });
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
+
 	}
 
 	/**
-	 * @throws Exception
 	 */
 	@Test
-	public void testImageTweetsStormStream() throws Exception {
-		final File testOut = File.createTempFile("image", "out");
-		System.out.println("output location: " + testOut);
-		testOut.delete();
-		System.setIn(TestPicSlurper.class.getResourceAsStream("/images-10.txt"));
+	public void testImageTweetsStormStream()  {
+		try{
+			final File testOut = File.createTempFile("image", "out");
+			System.out.println("output location: " + testOut);
+			testOut.delete();
+			System.setIn(TestPicSlurper.class.getResourceAsStream("/images-10.txt"));
 
-		StormPicSlurper.main(new String[] { "-o", testOut.getAbsolutePath() });
+			StormPicSlurper.main(new String[] { "-o", testOut.getAbsolutePath() });
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
+
 	}
 
 	/**
 	 * Check the {@link InstagramConsumer}
-	 * 
-	 * @throws Exception
 	 */
 	@Test
-	public void testInstagramConsumer() throws Exception {
-		final InstagramConsumer consumer = new InstagramConsumer();
-		System.out.println(consumer.consume(new URL("http://instagr.am/p/MbsBS_SkJo/")));
-		System.out.println(consumer.consume(new URL("http://instagr.am/p/PE1Or4mbNf/")));
+	public void testInstagramConsumer()  {
+		try{
+			final InstagramConsumer consumer = new InstagramConsumer();
+			System.out.println(consumer.consume(new URL("http://instagr.am/p/MbsBS_SkJo/")));
+			System.out.println(consumer.consume(new URL("http://instagr.am/p/PE1Or4mbNf/")));
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
+
+
 
 	}
 
 	/**
 	 * Check the {@link TwitterPhotoConsumer}
-	 * 
-	 * @throws Exception
 	 */
 	@Test
-	public void testTwitterPhotoConsumer() throws Exception {
-		final TwitterPhotoConsumer consumer = new TwitterPhotoConsumer();
-		System.out.println(consumer
-				.consume(new URL("http://twitter.com/sentirsevilla/status/222772198987927553/photo/1")));
+	public void testTwitterPhotoConsumer()  {
+		try{
+			final TwitterPhotoConsumer consumer = new TwitterPhotoConsumer();
+			System.out.println(consumer
+					.consume(new URL("http://twitter.com/sentirsevilla/status/222772198987927553/photo/1")));
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
+
 	}
 
 	// /**
 	// * Check {@link TmblrPhotoConsumer}
 	// *
-	// * @throws Exception
+	// * @
 	// */
 	// @Test
-	// public void testTmblrConsumer() throws Exception {
+	// public void testTmblrConsumer()  {
 	// PicSlurper.loadConfig();
 	// TmblrPhotoConsumer consumer = new TmblrPhotoConsumer();
 	// URL im2 = consumer.consume(new
@@ -165,103 +186,121 @@ public class TestPicSlurper {
 
 	/**
 	 * Check the {@link ImgurConsumer}
-	 * 
-	 * @throws Exception
 	 */
 	@Test
-	public void testImgurConsumer() throws Exception {
-		final ImgurConsumer consumer = new ImgurConsumer();
-		System.out.println(consumer.consume(new URL("http://imgur.com/a/ijrTZ")));
+	public void testImgurConsumer()  {
+		try{
+			final ImgurConsumer consumer = new ImgurConsumer();
+			System.out.println(consumer.consume(new URL("http://imgur.com/a/ijrTZ")));
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
+
 	}
 
 	/**
 	 * Check the {@link TwitPicConsumer}
-	 * 
-	 * @throws Exception
 	 */
 	@Test
-	public void testTwitPicConsumer() throws Exception {
-		final TwitPicConsumer consumer = new TwitPicConsumer();
-		System.out.println(consumer.canConsume(new URL("http://twitpic.com/au680l")));
-		System.out.println(consumer.consume(new URL("http://twitpic.com/au680l")));
-		System.out.println(consumer.consume(new URL("http://twitpic.com/a67733")));
-		System.out.println(consumer.consume(new URL("http://twitpic.com/a67dei")));
+	public void testTwitPicConsumer()  {
+		try{
+			final TwitPicConsumer consumer = new TwitPicConsumer();
+			System.out.println(consumer.canConsume(new URL("http://twitpic.com/au680l")));
+			System.out.println(consumer.consume(new URL("http://twitpic.com/au680l")));
+			System.out.println(consumer.consume(new URL("http://twitpic.com/a67733")));
+			System.out.println(consumer.consume(new URL("http://twitpic.com/a67dei")));
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
+
 	}
 
 	/**
 	 * Check the {@link TwitPicConsumer}
-	 * 
-	 * @throws Exception
 	 */
 	@Test
-	public void testTwipleConsumer() throws Exception {
-		final TwipleConsumer consumer = new TwipleConsumer();
-		final URL testURL = new URL("http://p.twipple.jp/hUaUl");
-		System.out.println(consumer.canConsume(testURL));
-		System.out.println(consumer.consume(testURL));
+	public void testTwipleConsumer()  {
+		try{
+			final TwipleConsumer consumer = new TwipleConsumer();
+			final URL testURL = new URL("http://p.twipple.jp/hUaUl");
+			System.out.println(consumer.canConsume(testURL));
+			System.out.println(consumer.consume(testURL));
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
+
+
 	}
 
 	/**
 	 * Check the {@link TwitPicConsumer}
-	 * 
-	 * @throws Exception
 	 */
 	@Test
-	public void testSimpleHTMLScraper() throws Exception {
-		SiteSpecificConsumer consumer = new SimpleHTMLScrapingConsumer("fotolog", "#flog_img_holder img");
-		URL testURL = new URL("http://www.fotolog.com/brenanatalia/233000000000016162/");
-		System.out.println(consumer.canConsume(testURL));
-		System.out.println(consumer.consume(testURL));
+	public void testSimpleHTMLScraper()  {
+		try{
+			SiteSpecificConsumer consumer = new SimpleHTMLScrapingConsumer("fotolog", "#flog_img_holder img");
+			URL testURL = new URL("http://www.fotolog.com/brenanatalia/233000000000016162/");
+			System.out.println(consumer.canConsume(testURL));
+			System.out.println(consumer.consume(testURL));
 
-		// e.g. http://photonui.com/3Hbh
-		consumer = new SimpleHTMLScrapingConsumer("photonui", "#image-box img");
-		testURL = new URL("http://photonui.com/3Hbh");
-		System.out.println(consumer.canConsume(testURL));
-		System.out.println(consumer.consume(testURL));
+			// e.g. http://photonui.com/3Hbh
+			consumer = new SimpleHTMLScrapingConsumer("photonui", "#image-box img");
+			testURL = new URL("http://photonui.com/3Hbh");
+			System.out.println(consumer.canConsume(testURL));
+			System.out.println(consumer.consume(testURL));
 
-		consumer = new SimpleHTMLScrapingConsumer("pics.lockerz", "#photo");
-		testURL = new URL("http://pics.lockerz.com/s/243838341");
-		System.out.println(consumer.canConsume(testURL));
-		System.out.println(consumer.consume(testURL));
+			consumer = new SimpleHTMLScrapingConsumer("pics.lockerz", "#photo");
+			testURL = new URL("http://pics.lockerz.com/s/243838341");
+			System.out.println(consumer.canConsume(testURL));
+			System.out.println(consumer.consume(testURL));
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
+
+
 	}
 
 	/**
 	 * Test the {@link FacebookConsumer}
-	 * 
-	 * @throws IOException
 	 */
 	@Test
-	public void testFacebookConsumer() throws IOException {
-		final FacebookConsumer consumer = new FacebookConsumer();
-		final String[] facebookImages = new String[] {
-				"https://www.facebook.com/fsdeventos/posts/251363008318613",
-				"http://www.facebook.com/dreddyclinic/posts/434840279891662",
-				"https://www.facebook.com/photo.php?pid=1005525&l=8649b56ff5&id=100001915865131",
-				"http://www.facebook.com/photo.php?pid=4097023&l=14a33b4930&id=1159082025",
-				"http://www.facebook.com/photo.php?pid=888026&l=5e0079b36f&id=131625683584436",
-				"http://www.facebook.com/photo.php?pid=1436406&l=893506478b&id=179699385416365"
+	public void testFacebookConsumer() {
+		try{
+			final FacebookConsumer consumer = new FacebookConsumer();
+			final String[] facebookImages = new String[] {
+					"https://www.facebook.com/fsdeventos/posts/251363008318613",
+					"http://www.facebook.com/dreddyclinic/posts/434840279891662",
+					"https://www.facebook.com/photo.php?pid=1005525&l=8649b56ff5&id=100001915865131",
+					"http://www.facebook.com/photo.php?pid=4097023&l=14a33b4930&id=1159082025",
+					"http://www.facebook.com/photo.php?pid=888026&l=5e0079b36f&id=131625683584436",
+					"http://www.facebook.com/photo.php?pid=1436406&l=893506478b&id=179699385416365"
 
-		};
-		for (final String string : facebookImages) {
-			final List<URL> images = consumer.consume(new URL(string));
-			assertTrue(images != null && images.size() > 0);
+			};
+			for (final String string : facebookImages) {
+				final List<URL> images = consumer.consume(new URL(string));
+				assertTrue(images != null && images.size() > 0);
+			}
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
 		}
+
 	}
 
 	/**
 	 * things
-	 * 
-	 * @throws InterruptedException
-	 * @throws TweetTokeniserException
-	 * @throws IOException
 	 */
 	@Test
-	public void testTwitter4jMode() throws IOException, TweetTokeniserException, InterruptedException {
-		final File testOut = File.createTempFile("image", "out");
-		System.out.println("output location: " + testOut);
-		testOut.delete();
-		PicSlurper.main(new String[] { "-oauth", "-o", testOut.getAbsolutePath() });
-		Thread.sleep(10000);
+	public void testTwitter4jMode(){
+		try{
+			final File testOut = File.createTempFile("image", "out");
+			System.out.println("output location: " + testOut);
+			testOut.delete();
+			PicSlurper.main(new String[] { "-oauth", "-o", testOut.getAbsolutePath() });
+			Thread.sleep(10000);
+		}catch(Exception e){
+			logger.error("Test failed: " + e.getMessage());
+		}
+
 	}
 
 }
