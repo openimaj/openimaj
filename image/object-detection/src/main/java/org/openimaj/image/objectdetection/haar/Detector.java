17e87a040ce75f557f7ac7b74e338d94994d9e41
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2012-10-15 18:39:48 +0000

    removed test code

--- image/object-detection/src/main/java/org/openimaj/image/objectdetection/haar/Detector.java
+++ image/object-detection/src/main/java/org/openimaj/image/objectdetection/haar/Detector.java
@@ -1,20 +1,12 @@
 package org.openimaj.image.objectdetection.haar;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.openimaj.image.FImage;
-import org.openimaj.image.MBFImage;
 import org.openimaj.image.analysis.algorithm.SummedSqTiltAreaTable;
-import org.openimaj.image.colour.RGBColour;
 import org.openimaj.image.objectdetection.AbstractMultiScaleObjectDetector;
-import org.openimaj.image.objectdetection.filtering.OpenCVGrouping;
 import org.openimaj.math.geometry.shape.Rectangle;
-import org.openimaj.time.Timer;
-import org.openimaj.video.VideoDisplay;
-import org.openimaj.video.VideoDisplayListener;
-import org.openimaj.video.capture.VideoCapture;
 
 public class Detector extends AbstractMultiScaleObjectDetector<FImage, Rectangle> {
 	StageTreeClassifier cascade;
@@ -98,54 +90,5 @@ public class Detector extends AbstractMultiScaleObjectDetector<FImage, Rectangle
 		}
 
 		return results;
-	}
-
-	public static void main(String[] args) throws IOException {
-		// final FImage test = ImageUtilities.readF(new
-		// File("/Users/jsh2/Desktop/face.jpg"));
-
-		final StageTreeClassifier cascade = OCVHaarLoader.read(StageTreeClassifier.class
-				.getResourceAsStream("haarcascade_frontalface_alt2.xml"));
-
-		final Detector det = new Detector(cascade);
-		det.setMinimumDetectionSize(80);
-
-		// final SqRotSummedAreaTable sat = new SqRotSummedAreaTable(test,
-		// false);
-		// cascade.matches(sat, 171, 38, 4.1772494f);
-		//
-		// final List<Rectangle> rects = det.detect(test, 80);
-		// System.out.println(rects);
-		//
-		// final MBFImage cimg = new MBFImage(test.clone(), test.clone(),
-		// test.clone());
-		// for (final Rectangle r : rects) {
-		// cimg.drawShape(r, RGBColour.RED);
-		// }
-		//
-		// DisplayUtilities.display(cimg);
-
-		final OpenCVGrouping grp = new OpenCVGrouping();
-
-		VideoDisplay.createVideoDisplay(new VideoCapture(640, 480)).addVideoListener(new VideoDisplayListener<MBFImage>()
-		{
-			@Override
-			public void beforeUpdate(MBFImage frame) {
-				final Timer t = Timer.timer();
-				List<Rectangle> rects = det.detect(frame.flatten());
-				rects = grp.apply(rects);
-				System.out.println(t.duration());
-
-				for (final Rectangle r : rects) {
-					frame.drawShape(r, RGBColour.RED);
-				}
-			}
-
-			@Override
-			public void afterUpdate(VideoDisplay<MBFImage> display) {
-				// TODO Auto-generated method stub
-
-			}
-		});
 	}
 }
