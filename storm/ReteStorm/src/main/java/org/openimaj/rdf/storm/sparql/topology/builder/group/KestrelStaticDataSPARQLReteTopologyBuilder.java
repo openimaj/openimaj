1ae6fb3041599617c7aa355d2066016822bfee75
Sina Samangooei <sinjax@gmail.com>
2012-11-21 14:37:52 +0000

    fixed some algorithmic errors in the static sparql bolt and added monitors to the retestorm tool

--- storm/ReteStorm/src/main/java/org/openimaj/rdf/storm/sparql/topology/builder/group/KestrelStaticDataSPARQLReteTopologyBuilder.java
+++ storm/ReteStorm/src/main/java/org/openimaj/rdf/storm/sparql/topology/builder/group/KestrelStaticDataSPARQLReteTopologyBuilder.java
@@ -33,6 +33,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
