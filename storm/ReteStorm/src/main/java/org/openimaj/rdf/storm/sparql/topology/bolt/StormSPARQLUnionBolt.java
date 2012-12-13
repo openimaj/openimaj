2ca99fa528a73450b8c39c02ad26018e1c7f4c2b
David Monks <david.monks@zepler.net>
2012-12-13 13:41:17 +0000

    Logging added via the storm.topology.logging.LoggerBolt class, not 'wired into' the rest of the topology in this commit" UnionBolt added in storm.sparql.topology.bolt package, not integrated into the builder (to be tested in comparison to forking)

--- storm/ReteStorm/src/main/java/org/openimaj/rdf/storm/sparql/topology/bolt/StormSPARQLUnionBolt.java
+++ storm/ReteStorm/src/main/java/org/openimaj/rdf/storm/sparql/topology/bolt/StormSPARQLUnionBolt.java
@@ -1,6 +1,7 @@
 package org.openimaj.rdf.storm.sparql.topology.bolt;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.openimaj.rdf.storm.bolt.RETEStormNode;
@@ -10,6 +11,7 @@ import backtype.storm.tuple.Values;
 
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.reasoner.rulesys.impl.RETERuleContext;
+import com.hp.hpl.jena.sparql.core.Var;
 
 public class StormSPARQLUnionBolt extends StormSPARQLReteBolt {
 
@@ -22,10 +24,20 @@ public class StormSPARQLUnionBolt extends StormSPARQLReteBolt {
 
 	public StormSPARQLUnionBolt(Query query, Map<String, Query> subQs){
 		super(query);
-		
 		this.subQueries = new HashMap<String,int[]>();
+		List<Var> superVars = query.getValuesVariables();
 		for (String boltName : subQs.keySet()){
-			
+			int[] mapping = new int[superVars.size()];
+			List<Var> subVars = subQs.get(boltName).getValuesVariables();
+			for (int i = 0; i < superVars.size(); i++){
+				mapping[i] = -1;
+				for (int x = 0; x < subVars.size(); x++)
+					if (superVars.get(i).getName().equals(subVars.get(x).getName())){
+						mapping[i] = x;
+						break;
+					}
+			}
+			this.subQueries.put(boltName, mapping);
 		}
 	}
 	
@@ -33,6 +45,16 @@ public class StormSPARQLUnionBolt extends StormSPARQLReteBolt {
 	public void execute(Tuple input) {
 		boolean isAdd = extractIsAdd(input);
 		Values output = new Values();
+		int[] mapping = this.subQueries.get(input.getSourceComponent());
+		if (mapping != null){
+			for (int i = 0; i < mapping.length; i++)
+				if (mapping[i] < 0)
+					output.add(null);
+				else 
+					output.add(input.getValue(mapping[i]));
+			for (int i = input.getValues().size() - Component.values().length; i < input.getValues().size(); i++)
+				output.add(input.getValue(i));
+		}
 		this.fire(output, isAdd);
 	}
 
