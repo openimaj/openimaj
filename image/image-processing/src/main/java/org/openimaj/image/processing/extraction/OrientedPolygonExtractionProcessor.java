4d04ea46de787fe3358919a1a1f4a2f4fbc5b248
David Duplaw <david.dupplaw@wds.co>
2013-01-02 12:15:46 +0000

    * Removed diamond operator as source is 1.6

--- image/image-processing/src/main/java/org/openimaj/image/processing/extraction/OrientedPolygonExtractionProcessor.java
+++ image/image-processing/src/main/java/org/openimaj/image/processing/extraction/OrientedPolygonExtractionProcessor.java
@@ -15,47 +15,47 @@ import Jama.Matrix;
  * The ellipse fitting the polygon is extracted.
  * The rotation of the ellipse is used to orientate the polygon.
  * The major and minor axis of the ellipse form the dimensions of the output FImage
- * The {@link ProjectionProcessor} is used to perform the extraction. Concretely, 
+ * The {@link ProjectionProcessor} is used to perform the extraction. Concretely,
  * the projection processor is set up with a transform matrix combining the rotation of
  * the ellipse and a translation using the COG of the ellipse.
- * 
+ *
  * @author Sina Samangooei (ss@ecs.soton.ac.uk)
  *
  */
 public class OrientedPolygonExtractionProcessor implements ImageProcessor<FImage>{
-	
-	private float background;
-	private Ellipse polygonEllipse;
+
+	private final float background;
+	private final Ellipse polygonEllipse;
 
 	/**
 	 * @param polygon
 	 * @param background
 	 */
-	public OrientedPolygonExtractionProcessor(Polygon polygon, float background) {
+	public OrientedPolygonExtractionProcessor(final Polygon polygon, final float background) {
 		this.polygonEllipse = polygon.toEllipse();
 		this.background = background;
 	}
-	
+
 	@Override
-	public void processImage(FImage image) {
-		image.internalAssign(orientedBoundingBoxProjection(image));
+	public void processImage(final FImage image) {
+		image.internalAssign(this.orientedBoundingBoxProjection(image));
 	}
-	
-	private FImage orientedBoundingBoxProjection(FImage image) {
-		ProjectionProcessor<Float, FImage> pp = new ProjectionProcessor<>();
+
+	private FImage orientedBoundingBoxProjection(final FImage image) {
+		final ProjectionProcessor<Float, FImage> pp = new ProjectionProcessor<Float,FImage>();
 		Matrix trans = Matrix.identity(3, 3);
-		trans = trans.times(TransformUtilities.rotationMatrix(-polygonEllipse.getRotation()));
+		trans = trans.times(TransformUtilities.rotationMatrix(-this.polygonEllipse.getRotation()));
 		trans = trans.times(
 			TransformUtilities.translateToPointMatrix(
-					polygonEllipse.getCOG(), 
+					this.polygonEllipse.getCOG(),
 					new Point2dImpl(0,0))
 		);
 		pp.setMatrix(trans);
 		pp.accumulate(image);
 		return pp.performProjection(
-				(int)-polygonEllipse.getMajor(),(int)polygonEllipse.getMajor(),
-				(int)-polygonEllipse.getMinor(),(int)polygonEllipse.getMinor(),
-				background
+				(int)-this.polygonEllipse.getMajor(),(int)this.polygonEllipse.getMajor(),
+				(int)-this.polygonEllipse.getMinor(),(int)this.polygonEllipse.getMinor(),
+				this.background
 		);
 	}
 
