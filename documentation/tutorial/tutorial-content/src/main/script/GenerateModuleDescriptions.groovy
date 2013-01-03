c007853f2bd6158c41ac0030bb6fffa3687aeaac
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-01-03 12:47:48 +0000

    fixed pdf fonts

---
+++ documentation/tutorial/tutorial-content/src/main/script/GenerateModuleDescriptions.groovy
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) 2011, The University of Southampton and the individual contributors.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *   * 	Redistributions of source code must retain the above copyright notice,
+ * 	this list of conditions and the following disclaimer.
+ *
+ *   *	Redistributions in binary form must reproduce the above copyright notice,
+ * 	this list of conditions and the following disclaimer in the documentation
+ * 	and/or other materials provided with the distribution.
+ *
+ *   *	Neither the name of the University of Southampton nor the names of its
+ * 	contributors may be used to endorse or promote products derived from this
+ * 	software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * This script generates the module descriptions list by recursing through all the 
+ * modules from the root. The output file is 
+ * ${project.build.outputDirectory}/module-descriptions.xml
+ */
+import org.apache.maven.model.Model;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+
+import java.io.FileReader;
+
+import groovy.xml.MarkupBuilder
+
+MavenProject getRootProject() {
+	MavenProject p = project;
+	
+	while (p.parent != null) {
+		p = p.parent;
+	}
+	
+	return p;
+}
+
+MavenProject loadProject(File pomFile) {
+    Model model = null;
+    FileReader reader = null;
+    MavenXpp3Reader mavenreader = new MavenXpp3Reader();
+    try {
+        reader = new FileReader(pomFile);
+        model = mavenreader.read(reader);
+        model.setPomFile(pomFile);
+    } catch(Exception ex) {
+        fail("Unable to read pom " + pomFile);
+    }
+
+    return new MavenProject(model);
+}
+
+void process(MavenProject p, int level, File baseDir, MarkupBuilder builder) {
+	builder.varlistentry() {
+		term() {
+			filename( baseDir.name == "." ? "OpenIMAJ" : baseDir.name );
+		}
+		listitem() {
+		    if (p.getDescription() != null) {
+				para( p.getDescription().replaceAll("\\s+", " ") )
+			}
+
+		    def modules = p.getModules();
+		    if (modules != null) {
+				variablelist() {
+			        modules.each { module ->
+						File baseDir2 = new File(baseDir, module)
+						MavenProject mp = loadProject(new File(baseDir2, "/pom.xml"));
+			            process(mp, level+1, baseDir2, builder);
+			        }
+				}
+		    }
+		}
+	}
+}
+
+MavenProject rootProject = getRootProject();
+File outdir = new File(project.build.outputDirectory);
+outdir.mkdirs();
+def writer = new File(outdir, "module-descriptions.xml").newWriter();
+def xml = new MarkupBuilder(writer)
+
+writer << '<?xml version="1.0" encoding="UTF-8"?>\n';
+writer <<  '<!DOCTYPE chapter PUBLIC "-//OASIS//DTD DocBook XML V4.5//EN" "http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd">\n';
+xml.variablelist() {
+	process(rootProject, 1, rootProject.basedir, xml);
+}
+
+writer.close();
