50140ac28d270787505bba1bbf10bbc64b7fde73
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-02-20 19:17:16 +0000

    work-around compiler generics bug

--- demos/sandbox/src/main/java/org/openimaj/demos/sandbox/image/PersonMatcher.java
+++ demos/sandbox/src/main/java/org/openimaj/demos/sandbox/image/PersonMatcher.java
@@ -44,6 +44,7 @@ import net.billylieurance.azuresearch.AzureSearchImageQuery;
 import net.billylieurance.azuresearch.AzureSearchImageResult;
 import net.billylieurance.azuresearch.AzureSearchResultSet;
 
+import org.openimaj.feature.FeatureExtractor;
 import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
@@ -60,17 +61,17 @@ import org.openimaj.tools.faces.recognition.options.RecognitionStrategy;
 import org.openimaj.util.pair.IndependentPair;
 
 /**
- *	Class for providing verification of unseen people in images. It does this
- *	by limiting the search space to be a verification problem rather than a
- *	recognition problem - that is, the possible number of people that could
- *	possibly be in an image is limited.  The tool does a web search
- *	(currently using Bing) to retrieve images of the people in question and
- *	trains a face recognition engine using these. It then looks in the query
- *	image for faces and attempts to classify the found faces.
- *
- *	@author David Dupplaw (dpd@ecs.soton.ac.uk)
- *  @created 5 Feb 2013
- *	@version $Author$, $Revision$, $Date$
+ * Class for providing verification of unseen people in images. It does this by
+ * limiting the search space to be a verification problem rather than a
+ * recognition problem - that is, the possible number of people that could
+ * possibly be in an image is limited. The tool does a web search (currently
+ * using Bing) to retrieve images of the people in question and trains a face
+ * recognition engine using these. It then looks in the query image for faces
+ * and attempts to classify the found faces.
+ * 
+ * @author David Dupplaw (dpd@ecs.soton.ac.uk)
+ * @created 5 Feb 2013
+ * @version $Author$, $Revision$, $Date$
  */
 public class PersonMatcher
 {
@@ -93,135 +94,161 @@ public class PersonMatcher
 	private final boolean cacheImages = true;
 
 	/**
-	 *	Create a person matcher
-	 * 	@throws Exception
+	 * Create a person matcher
+	 * 
+	 * @throws Exception
 	 */
 	public PersonMatcher() throws Exception
 	{
-		this( null );
+		this(null);
 	}
 
 	/**
-	 * 	Create a person matcher with the given file
-	 * 	@param recogniserFile The recogniser file to load
-	 * 	@throws Exception
+	 * Create a person matcher with the given file
+	 * 
+	 * @param recogniserFile
+	 *            The recogniser file to load
+	 * @throws Exception
 	 */
-	public PersonMatcher( final File recogniserFile ) throws Exception
+	public PersonMatcher(final File recogniserFile) throws Exception
 	{
 		// Setup a new face recognition engine
-		this.faceRecogniser = this.getFaceRecogniserEngine( recogniserFile );
+		this.faceRecogniser = this.getFaceRecogniserEngine(recogniserFile);
 
-		if( this.faceRecogniser == null )
-			throw new Exception( "Face recogniser not initialised" );
+		if (this.faceRecogniser == null)
+			throw new Exception("Face recogniser not initialised");
 
 		// Get the face detector for this strategy
 		this.faceDetector = this.faceRecogniser.getDetector();
 	}
 
 	/**
-	 * 	Create a recogniser for the given person into the given file.
-	 *
-	 *	@param person The person to create a recogniser for
-	 *	@param recogniserFile The recogniser file to save
-	 *	@throws Exception If the face recognition engine could not be initialised
+	 * Create a recogniser for the given person into the given file.
+	 * 
+	 * @param person
+	 *            The person to create a recogniser for
+	 * @param recogniserFile
+	 *            The recogniser file to save
+	 * @throws Exception
+	 *             If the face recognition engine could not be initialised
 	 */
-	public PersonMatcher( final String person, final File recogniserFile ) throws Exception
+	public PersonMatcher(final String person, final File recogniserFile) throws Exception
 	{
-		this( new String[] { person }, recogniserFile, true );
+		this(new String[] { person }, recogniserFile, true);
 	}
 
 	/**
-	 * 	Constructor that takes a query string.
-	 *	@param queries The query strings to use
-	 * 	@param recogniserFile The file to save the recogniser into
-	 * 	@param addCounterExamples Whether to add counter examples
-	 * 	@throws Exception If the face recognition engine could not be initialised
+	 * Constructor that takes a query string.
+	 * 
+	 * @param queries
+	 *            The query strings to use
+	 * @param recogniserFile
+	 *            The file to save the recogniser into
+	 * @param addCounterExamples
+	 *            Whether to add counter examples
+	 * @throws Exception
+	 *             If the face recognition engine could not be initialised
 	 */
-	public PersonMatcher( final List<String> queries, final File recogniserFile, final boolean addCounterExamples ) throws Exception
-    {
-		this( queries.toArray( new String[0] ), recogniserFile, addCounterExamples );
-    }
-
-	/**
-	 * 	Constructor that takes a set of queries to search for
-	 *	@param queries The query strings to use
-	 * 	@param recogniserFile The file to save the recogniser into
-	 * 	@param addCounterExamples Whether to add counter examples
-	 * 	@throws Exception If the face recognition engine could not be initialised
-	 */
-	public PersonMatcher( final String[] queries, final File recogniserFile, final boolean addCounterExamples )
+	public PersonMatcher(final List<String> queries, final File recogniserFile, final boolean addCounterExamples)
 			throws Exception
 	{
-		this( recogniserFile );
+		this(queries.toArray(new String[0]), recogniserFile, addCounterExamples);
+	}
+
+	/**
+	 * Constructor that takes a set of queries to search for
+	 * 
+	 * @param queries
+	 *            The query strings to use
+	 * @param recogniserFile
+	 *            The file to save the recogniser into
+	 * @param addCounterExamples
+	 *            Whether to add counter examples
+	 * @throws Exception
+	 *             If the face recognition engine could not be initialised
+	 */
+	public PersonMatcher(final String[] queries, final File recogniserFile, final boolean addCounterExamples)
+			throws Exception
+	{
+		this(recogniserFile);
 
 		// Train using the given queries
-		this.train( queries );
+		this.train(queries);
 
 		// Add a set of images that are not of the query person.
-		if( addCounterExamples )
+		if (addCounterExamples)
 			this.addCounterExamples();
 
 		// Save the recogniser for later
-		this.saveRecogniser( recogniserFile );
+		this.saveRecogniser(recogniserFile);
 	}
 
 	/**
-	 * 	After training, you might want to save the recogniser
-	 * 	@param recogniserFile The recogniser file to save to
+	 * After training, you might want to save the recogniser
+	 * 
+	 * @param recogniserFile
+	 *            The recogniser file to save to
 	 * @throws IOException
 	 */
-	public void saveRecogniser( final File recogniserFile ) throws IOException
+	public void saveRecogniser(final File recogniserFile) throws IOException
 	{
-		System.out.println( "Saving recogniser to "+recogniserFile );
+		System.out.println("Saving recogniser to " + recogniserFile);
 
 		// Save the recogniser
-		this.faceRecogniser.save( recogniserFile );
-    }
-
-	/**
-	 * 	Train the recogniser with examples retrieved from searching
-	 * 	with the given queries.
-	 *	@param queries The query strings
-	 */
-	public void train( final String[] queries )
-	{
-		// Now go and retrieve the images for the query
-		for( final String query: queries )
-			this.searchForExamples( query, query, false );
+		this.faceRecogniser.save(recogniserFile);
 	}
 
 	/**
-	 * 	@param fi The image to find the query person within
-	 * 	@return The matching results
+	 * Train the recogniser with examples retrieved from searching with the
+	 * given queries.
+	 * 
+	 * @param queries
+	 *            The query strings
 	 */
-	public List<? extends IndependentPair<? extends DetectedFace, ScoredAnnotation<String>>>
-		query( final FImage fi )
+	public void train(final String[] queries)
 	{
-		System.out.println( "Querying with image" );
-
-		// Recognise the unknown faces in the image.
-		final List<? extends IndependentPair<? extends DetectedFace,ScoredAnnotation<String>>>
-			recognisedFaces	= this.faceRecogniser.recogniseBest( fi );
-
-		System.out.println( "Recognised "+recognisedFaces.size()+" faces." );
-		System.out.println( recognisedFaces );
-
-		return recognisedFaces;
-    }
+		// Now go and retrieve the images for the query
+		for (final String query : queries)
+			this.searchForExamples(query, query, false);
+	}
 
 	/**
-	 * 	Returns a face recogniser by using the FaceRecogniserTools.
-	 * 	@param recogniserFile
-	 *	@return The face recogniser engine
-	 * 	@throws IOException
+	 * @param fi
+	 *            The image to find the query person within
+	 * @return The matching results
 	 */
-	private FaceRecognitionEngine<? extends DetectedFace, ?, String> getFaceRecogniserEngine( final File recogniserFile )
+	public List<? extends IndependentPair<? extends DetectedFace, ScoredAnnotation<String>>>
+			query(final FImage fi)
+	{
+		System.out.println("Querying with image");
+
+		// Recognise the unknown faces in the image.
+		final List<? extends IndependentPair<? extends DetectedFace, ScoredAnnotation<String>>> recognisedFaces = this.faceRecogniser
+				.recogniseBest(fi);
+
+		System.out.println("Recognised " + recognisedFaces.size() + " faces.");
+		System.out.println(recognisedFaces);
+
+		return recognisedFaces;
+	}
+
+	/**
+	 * Returns a face recogniser by using the FaceRecogniserTools.
+	 * 
+	 * @param recogniserFile
+	 * @return The face recogniser engine
+	 * @throws IOException
+	 */
+	private FaceRecognitionEngine<? extends DetectedFace, ?, String> getFaceRecogniserEngine(final File recogniserFile)
 			throws IOException
 	{
-		if( recogniserFile.exists() )
+		if (recogniserFile.exists())
 		{
-			System.out.println( "Loading existing recogniser from "+recogniserFile+" to update...");
-			return FaceRecognitionEngine.load( recogniserFile );
+			System.out.println("Loading existing recogniser from " + recogniserFile + " to update...");
+
+			final FaceRecognitionEngine<DetectedFace, FeatureExtractor<?, DetectedFace>, String> fre = FaceRecognitionEngine
+					.load(recogniserFile);
+			return fre;
 		}
 
 		final RecognitionEngineProvider<? extends DetectedFace> o = RecognitionStrategy.CLMFeature_KNN.getOptions();
@@ -229,33 +256,32 @@ public class PersonMatcher
 	}
 
 	/**
-	 * 	Adds a set of counter examples to the recogniser by searching the web for the
-	 * 	generic string "face" and adding them as an unknown person.
+	 * Adds a set of counter examples to the recogniser by searching the web for
+	 * the generic string "face" and adding them as an unknown person.
 	 */
 	public void addCounterExamples()
 	{
-		this.searchForExamples( "face", "unknown", true );
+		this.searchForExamples("face", "unknown", true);
 	}
 
 	/**
-	 * 	Retrieves a set of images from Bing that match the query then for each
-	 * 	one calls the face recogniser to train it.
+	 * Retrieves a set of images from Bing that match the query then for each
+	 * one calls the face recogniser to train it.
 	 */
-	private void searchForExamples( final String query, final String label, final boolean facesOnly )
-    {
-		System.out.println( "Searching for '"+query+"'" );
+	private void searchForExamples(final String query, final String label, final boolean facesOnly)
+	{
+		System.out.println("Searching for '" + query + "'");
 
 		File f = null;
-		if( this.cacheImages && (f = new File( PersonMatcher.CACHE_DIR+"/"+label+"/" )).exists() )
+		if (this.cacheImages && (f = new File(PersonMatcher.CACHE_DIR + "/" + label + "/")).exists())
 		{
-			System.out.println( "Using cached images: ");
-			for( final File cachedImage : f.listFiles() )
+			System.out.println("Using cached images: ");
+			for (final File cachedImage : f.listFiles())
 			{
 				try
 				{
-					this.processImageURL( cachedImage.toURI().toURL().toString(), label );
-				}
-				catch( final MalformedURLException m )
+					this.processImageURL(cachedImage.toURI().toURL().toString(), label);
+				} catch (final MalformedURLException m)
 				{
 					m.printStackTrace();
 				}
@@ -266,43 +292,45 @@ public class PersonMatcher
 		// Do an image search with the given query. Note that the format
 		// must be XML - the library has no parser for JSON
 		final AzureSearchImageQuery aq = new AzureSearchImageQuery();
-		aq.setAppid( PersonMatcher.APPID );
-		aq.setFormat( AZURESEARCH_FORMAT.XML );
-		aq.setMarket( "en-us" );
-		aq.setQuery( query );
+		aq.setAppid(PersonMatcher.APPID);
+		aq.setFormat(AZURESEARCH_FORMAT.XML);
+		aq.setMarket("en-us");
+		aq.setQuery(query);
 		aq.doQuery();
 
 		// Get the results
 		final AzureSearchResultSet<AzureSearchImageResult> results =
 				aq.getQueryResult();
 
-		System.out.println( "    - Got "+results.getImageTotal()+" results");
+		System.out.println("    - Got " + results.getImageTotal() + " results");
 
 		// Loop over all the results and process each one
-		for( final AzureSearchImageResult result : results )
-			this.processImageURL( result.getMediaUrl(), label );
+		for (final AzureSearchImageResult result : results)
+			this.processImageURL(result.getMediaUrl(), label);
 	}
 
 	/**
-	 * 	For each URL (that is an image representation of the query),
-	 * 	load it in and train the face recogniser.
-	 *	@param url The URL of an image that is a representation of the query
-	 *	@param label The classification of the URL
+	 * For each URL (that is an image representation of the query), load it in
+	 * and train the face recogniser.
+	 * 
+	 * @param url
+	 *            The URL of an image that is a representation of the query
+	 * @param label
+	 *            The classification of the URL
 	 */
-	private void processImageURL( final String url, final String label )
+	private void processImageURL(final String url, final String label)
 	{
-		if( this.cacheImages )
+		if (this.cacheImages)
 		{
 			try
 			{
-				final MBFImage i = ImageUtilities.readMBF( new URL(url) );
-				final UUID uuid = UUID.nameUUIDFromBytes( i.toByteImage() );
-				final File f = new File( PersonMatcher.CACHE_DIR+"/"+label+"/"+uuid+".png" );
+				final MBFImage i = ImageUtilities.readMBF(new URL(url));
+				final UUID uuid = UUID.nameUUIDFromBytes(i.toByteImage());
+				final File f = new File(PersonMatcher.CACHE_DIR + "/" + label + "/" + uuid + ".png");
 				f.getParentFile().mkdirs();
-				if( !f.exists() )
-					ImageUtilities.write( i, f );
-			}
-			catch( final IOException e )
+				if (!f.exists())
+					ImageUtilities.write(i, f);
+			} catch (final IOException e)
 			{
 				e.printStackTrace();
 			}
@@ -310,48 +338,47 @@ public class PersonMatcher
 
 		try
 		{
-			System.out.println( "Reading "+url );
+			System.out.println("Reading " + url);
 
 			// Read in the result image
 			final FImage img = ImageUtilities.readF(
-					new URL( url ) );
+					new URL(url));
 
 			// Get the detected faces from the given image
 			final List<? extends DetectedFace> detectedFaces =
-					this.faceDetector.detectFaces( img );
+					this.faceDetector.detectFaces(img);
 
-			System.out.println( "    - Found "+detectedFaces.size()+" faces ");
+			System.out.println("    - Found " + detectedFaces.size() + " faces ");
 
 			// If there is more than one person in the image (or none),
 			// then we can't sensibly say which one was the query.. so
 			// we must ignore. If there is only one detected face, we
 			// assume that it's the face of the person in the query
-			if( detectedFaces.size() == 1 )
-					this.faceRecogniser.train( label, img );
-			else	System.out.println( "    - Ignoring this image.");
-		}
-		catch( final IOException e )
+			if (detectedFaces.size() == 1)
+				this.faceRecogniser.train(label, img);
+			else
+				System.out.println("    - Ignoring this image.");
+		} catch (final IOException e)
 		{
-			System.out.println( "    - Could not read image "+url );
+			System.out.println("    - Could not read image " + url);
 		}
 	}
 
 	/**
-	 *	@param args
-	 * 	@throws IOException
+	 * @param args
+	 * @throws IOException
 	 */
-	public static void main( final String[] args ) throws IOException
-    {
+	public static void main(final String[] args) throws IOException
+	{
 		// Use the constructor that takes the queries and automatically creates
 		// a recogniser for the given person and then saves it
 		try
 		{
-			System.out.println( "----------- TRAINING ---------- " );
+			System.out.println("----------- TRAINING ---------- ");
 			new PersonMatcher(
-					new String[]{"Barack Obama", "Arnold Schwarzenegger"},
-					new File( PersonMatcher.RECOGNISER_FILE ), false );
-		}
-		catch( final Exception e )
+					new String[] { "Barack Obama", "Arnold Schwarzenegger" },
+					new File(PersonMatcher.RECOGNISER_FILE), false);
+		} catch (final Exception e)
 		{
 			e.printStackTrace();
 		}
@@ -359,27 +386,25 @@ public class PersonMatcher
 		// Load back in the recogniser and try querying using the given image
 		try
 		{
-			System.out.println( "----------- QUERYING ----------- ");
-			final FImage fi = ImageUtilities.readF( PersonMatcher.class.getResource(
-					"/org/openimaj/demos/sandbox/BarackObama1.jpg" ) );
-			final PersonMatcher pm = new PersonMatcher( new File( PersonMatcher.RECOGNISER_FILE ) );
-			final List<? extends IndependentPair<? extends DetectedFace, ScoredAnnotation<String>>> l
-					= pm.query( fi );
+			System.out.println("----------- QUERYING ----------- ");
+			final FImage fi = ImageUtilities.readF(PersonMatcher.class.getResource(
+					"/org/openimaj/demos/sandbox/BarackObama1.jpg"));
+			final PersonMatcher pm = new PersonMatcher(new File(PersonMatcher.RECOGNISER_FILE));
+			final List<? extends IndependentPair<? extends DetectedFace, ScoredAnnotation<String>>> l = pm.query(fi);
 
-			final MBFImage m = new MBFImage( fi, fi, fi );
-			for( final IndependentPair<? extends DetectedFace, ScoredAnnotation<String>> i : l )
+			final MBFImage m = new MBFImage(fi, fi, fi);
+			for (final IndependentPair<? extends DetectedFace, ScoredAnnotation<String>> i : l)
 			{
 				final Rectangle b = i.firstObject().getBounds();
-				m.drawShape( b, RGBColour.RED );
-				m.drawText( i.secondObject().annotation, (int)b.x, (int)b.y,
-						HersheyFont.TIMES_MEDIUM, 12, RGBColour.GREEN );
+				m.drawShape(b, RGBColour.RED);
+				m.drawText(i.secondObject().annotation, (int) b.x, (int) b.y,
+						HersheyFont.TIMES_MEDIUM, 12, RGBColour.GREEN);
 			}
-			DisplayUtilities.display( m );
-			ImageUtilities.write( m, new File("output.png") );
-		}
-		catch( final Exception e )
+			DisplayUtilities.display(m);
+			ImageUtilities.write(m, new File("output.png"));
+		} catch (final Exception e)
 		{
 			e.printStackTrace();
 		}
-    }
+	}
 }
