92daac85a52283c7e620b51f7565155971e2e1fa
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-02-25 14:07:58 +0000

    LDALearner now compiles

--- machine-learning/machine-learning/src/main/java/org/openimaj/pgm/vb/lda/mle/LDALearner.java
+++ machine-learning/machine-learning/src/main/java/org/openimaj/pgm/vb/lda/mle/LDALearner.java
@@ -74,22 +74,22 @@ public class LDALearner {
 				return new LDABetaInitStrategy.RandomBetaInit();
 			}
 
-		}, 
-		EM_CONVERGED{
-
-			@Override
-			public Double defaultValue() {
-				return 1e-5;
-			}
-			
 		},
-		VAR_EM_CONVERGED{
+		EM_CONVERGED {
 
 			@Override
 			public Double defaultValue() {
 				return 1e-5;
 			}
-			
+
+		},
+		VAR_EM_CONVERGED {
+
+			@Override
+			public Double defaultValue() {
+				return 1e-5;
+			}
+
 		};
 		public abstract Object defaultValue();
 	}
@@ -107,7 +107,7 @@ public class LDALearner {
 	 */
 	@SuppressWarnings("unchecked")
 	public <T> T getConfig(LDAConfig key) {
-		T val = (T) this.config.get(key);
+		final T val = (T) this.config.get(key);
 		if (val == null)
 			return (T) key.defaultValue();
 		return val;
@@ -123,62 +123,64 @@ public class LDALearner {
 	}
 
 	private void performEM(Corpus corpus) {
-		// some variables 
-		double initialAlpha = (double)this.getConfig(LDAConfig.ALPHA);
-		LDABetaInitStrategy initStrat = this.getConfig(LDAConfig.INIT_STRATEGY);
-		
+		// some variables
+		final double initialAlpha = (Double) this.getConfig(LDAConfig.ALPHA);
+		final LDABetaInitStrategy initStrat = this.getConfig(LDAConfig.INIT_STRATEGY);
+
 		// initialise the first state
-		LDAModel state = new LDAModel(this.ntopics);
+		final LDAModel state = new LDAModel(this.ntopics);
 		state.prepare(corpus);
 		state.setAlpha(initialAlpha);
 		initStrat.initModel(state, corpus);
-		
-		LDAVariationlState vstate = new LDAVariationlState(state);
+
+		final LDAVariationlState vstate = new LDAVariationlState(state);
 		while (modelConverged(vstate.state)) {
-			LDAModel nextState = vstate.state.newInstance();
+			final LDAModel nextState = vstate.state.newInstance();
 			nextState.setAlpha(initialAlpha);
-			for (Document doc : corpus.getDocuments()) {
+			for (final Document doc : corpus.getDocuments()) {
 				vstate.prepare(doc);
-				performE(doc, vstate); // updates the variation parameters given the current Beta
-				performM(doc, vstate,nextState); // updates the nextState given the variational parameters
+				performE(doc, vstate); // updates the variation parameters given
+										// the current Beta
+				performM(doc, vstate, nextState); // updates the nextState given
+													// the variational
+													// parameters
 				nextState.likelihood += vstate.likelihood;
 			}
 			nextState.iteration++;
 			vstate.state = nextState;
 		}
 	}
-	
+
 	private LDAVariationlState performE(Document doc, LDAVariationlState vstate) {
 		vstate.prepare(doc);
 		while (!variationalStateConverged(vstate)) {
 			int docWordIndex = 0;
-			for (Entry wordCount : doc.getVector().entries()) {
+			for (final Entry wordCount : doc.getVector().entries()) {
 				double phiSum = 0;
-				int word = wordCount.index;
-				int count = wordCount.value;
+				final int word = wordCount.index;
+				final int count = wordCount.value;
 				for (int topicIndex = 0; topicIndex < vstate.phi.length; topicIndex++) {
 					vstate.oldphi[topicIndex] = vstate.phi[docWordIndex][topicIndex];
 					// If this word has been seen in this class before
 					if (vstate.state.topicWord[topicIndex][docWordIndex] > 0) {
 						// Update phi
 						// Remember this phi is actually the same value for
-						// every instance of thisparticular word. 
+						// every instance of thisparticular word.
 						// Whenever phi is actually used there is likely to be a
 						// multiplication by the number of times this particular
 						// word appears in this document
 						// From eqn 16 in blei 2003
 						// The sum gamma cancels when the exact phi for a given
 						// word is calculated
-						double logBeta = 
-								Math.log(vstate.state.topicWord[topicIndex][word]) - 
-								Math.log(vstate.state.topicTotal[topicIndex])
-						;
-						vstate.phi[docWordIndex][topicIndex] = 
-								logBeta + 
-								Gamma.digamma(vstate.varGamma[topicIndex])
-						;
+						final double logBeta =
+								Math.log(vstate.state.topicWord[topicIndex][word]) -
+										Math.log(vstate.state.topicTotal[topicIndex]);
+						vstate.phi[docWordIndex][topicIndex] =
+								logBeta +
+										Gamma.digamma(vstate.varGamma[topicIndex]);
 					} else {
-						// if not, \Beta_wi = ETA (very small) so log \Beta_wi ~= -100 (ETA = 10-34)
+						// if not, \Beta_wi = ETA (very small) so log \Beta_wi
+						// ~= -100 (ETA = 10-34)
 						vstate.phi[docWordIndex][topicIndex] = Gamma.digamma(vstate.varGamma[topicIndex]) - 100;
 					}
 					if (topicIndex == 0) {
@@ -198,13 +200,14 @@ public class LDALearner {
 					// normalise a given word's phi summing over all i in eqn 16
 					vstate.phi[docWordIndex][topicIndex] = Math.exp(
 							vstate.phi[docWordIndex][topicIndex] - phiSum
-					);
+							);
 					// update gamma incrementally (eqn 17 blei 2003)
 					// - take away the old phi,
 					// - add the new phi,
 					// - do this N times for the number of times this particular
 					// word appears in this document
-					vstate.varGamma[topicIndex] += count * (vstate.phi[docWordIndex][topicIndex] - vstate.oldphi[topicIndex]);
+					vstate.varGamma[topicIndex] += count
+							* (vstate.phi[docWordIndex][topicIndex] - vstate.oldphi[topicIndex]);
 				}
 				docWordIndex++;
 			}
@@ -216,30 +219,26 @@ public class LDALearner {
 	}
 
 	private boolean modelConverged(LDAModel model) {
-		double EM_CONVERGED = this.getConfig(LDAConfig.EM_CONVERGED);
-		int MAX_ITER = this.getConfig(LDAConfig.MAX_ITERATIONS);
+		final double EM_CONVERGED = this.getConfig(LDAConfig.EM_CONVERGED);
+		final int MAX_ITER = this.getConfig(LDAConfig.MAX_ITERATIONS);
 		// if likelihood ~= oldLikelihood then this value will approach 0.
-		double converged = (model.likelihood - model.oldLikelihood)/model.oldLikelihood;
-		boolean liklihoodSettled = (( converged < EM_CONVERGED) || (model.iteration <= 2));
-		boolean maxIterExceeded = model.iteration > MAX_ITER;
-        
+		final double converged = (model.likelihood - model.oldLikelihood) / model.oldLikelihood;
+		final boolean liklihoodSettled = ((converged < EM_CONVERGED) || (model.iteration <= 2));
+		final boolean maxIterExceeded = model.iteration > MAX_ITER;
+
 		return liklihoodSettled || maxIterExceeded;
 	}
-	
+
 	private boolean variationalStateConverged(LDAVariationlState vstate) {
-		double EM_CONVERGED = this.getConfig(LDAConfig.VAR_EM_CONVERGED);
-		int MAX_ITER = this.getConfig(LDAConfig.VAR_MAX_ITERATIONS);
+		final double EM_CONVERGED = this.getConfig(LDAConfig.VAR_EM_CONVERGED);
+		final int MAX_ITER = this.getConfig(LDAConfig.VAR_MAX_ITERATIONS);
 		// if likelihood ~= oldLikelihood then this value will approach 0.
-		double converged = (vstate.likelihood - vstate.oldLikelihood)/vstate.oldLikelihood;
-		boolean liklihoodSettled = (( converged < EM_CONVERGED) || (vstate.iteration <= 2));
-		boolean maxIterExceeded = vstate.iteration > MAX_ITER;
-        
+		final double converged = (vstate.likelihood - vstate.oldLikelihood) / vstate.oldLikelihood;
+		final boolean liklihoodSettled = ((converged < EM_CONVERGED) || (vstate.iteration <= 2));
+		final boolean maxIterExceeded = vstate.iteration > MAX_ITER;
+
 		return liklihoodSettled || maxIterExceeded;
 	}
-
-	
-
-	
 
 	/**
 	 * Given the current state of the variational parameters, update the maximum
@@ -247,14 +246,14 @@ public class LDALearner {
 	 * 
 	 * @param d
 	 * @param vstate
-	 * @param nextState 
+	 * @param nextState
 	 */
 	private void performM(Document d, LDAVariationlState vstate, LDAModel nextState) {
 
-		for (Entry entry : d.values.entries()) {
+		for (final Entry entry : d.values.entries()) {
 			for (int topicIndex = 0; topicIndex < ntopics; topicIndex++) {
-				int wordIndex = entry.index;
-				int count = entry.value;
+				final int wordIndex = entry.index;
+				final int count = entry.value;
 				nextState.incTopicWord(topicIndex, wordIndex, count * vstate.phi[wordIndex][topicIndex]);
 				nextState.incTopicTotal(topicIndex, count);
 			}
@@ -297,31 +296,33 @@ public class LDALearner {
 		for (int topicIndex = 0; topicIndex < ntopics; topicIndex++) {
 			// Now add the things that just need an interation over k
 			// eqn (15) line 4
-			double topicGammaDiff = vstate.digamma[topicIndex] - sumDiGamma;
-			likelihood += Gamma.logGamma(vstate.varGamma[topicIndex]) - (vstate.varGamma[topicIndex] - 1) * topicGammaDiff;
+			final double topicGammaDiff = vstate.digamma[topicIndex] - sumDiGamma;
+			likelihood += Gamma.logGamma(vstate.varGamma[topicIndex]) - (vstate.varGamma[topicIndex] - 1)
+					* topicGammaDiff;
 			int wordIndex = 0;
-			for (Entry wordCount : doc.getVector().entries()) {
-				int word = wordCount.index;
-				int count = wordCount.value;
-				double logBeta = Math.log(
-					vstate.state.topicWord[topicIndex][word]) - 
-					Math.log(vstate.state.topicTotal[topicIndex]
-				);
+			for (final Entry wordCount : doc.getVector().entries()) {
+				final int word = wordCount.index;
+				final int count = wordCount.value;
+				final double logBeta = Math.log(
+						vstate.state.topicWord[topicIndex][word]) -
+						Math.log(vstate.state.topicTotal[topicIndex]
+								);
 				likelihood +=
-					// Count because these sums are over N and
-					// the sum of the counts of each unique word is == N
-					count * (
-					// Each of these lines happens to multiply by the current word's phi
-						vstate.phi[wordIndex][topicIndex] * (
-							// eqn (15) line 2
-							topicGammaDiff +
-							// eqn (15) line 3
-							count * logBeta -
-							// eqn (15) line 5
-							Math.log(vstate.phi[wordIndex][topicIndex]
-						)
-					)
-				);
+						// Count because these sums are over N and
+						// the sum of the counts of each unique word is == N
+						count * (
+								// Each of these lines happens to multiply by
+								// the current word's phi
+								vstate.phi[wordIndex][topicIndex] * (
+								// eqn (15) line 2
+								topicGammaDiff +
+										// eqn (15) line 3
+										count * logBeta -
+								// eqn (15) line 5
+								Math.log(vstate.phi[wordIndex][topicIndex]
+											)
+								)
+								);
 				wordIndex++;
 			}
 		}
