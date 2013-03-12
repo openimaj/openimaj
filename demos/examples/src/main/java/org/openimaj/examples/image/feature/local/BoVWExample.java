d6c28c89da170f0934128f5d5089f00cf659a9fa
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-03-12 22:37:14 +0000

    working on bovw example

---
+++ demos/examples/src/main/java/org/openimaj/examples/image/feature/local/BoVWExample.java
@@ -0,0 +1,58 @@
+package org.openimaj.examples.image.feature.local;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openimaj.feature.local.list.LocalFeatureList;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
+import org.openimaj.image.feature.local.engine.DoGSIFTEngine;
+import org.openimaj.image.feature.local.keypoints.Keypoint;
+import org.openimaj.ml.clustering.ByteCentroidsResult;
+import org.openimaj.ml.clustering.assignment.HardAssigner;
+import org.openimaj.ml.clustering.kmeans.ByteKMeans;
+
+/**
+ * 
+ * 
+ * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
+ * 
+ */
+public class BoVWExample {
+	public static void main(String[] args) throws MalformedURLException, IOException {
+		final MBFImage image = ImageUtilities.readMBF(new URL(
+				"http://users.ecs.soton.ac.uk/dpd/projects/openimaj/tutorial/hist1.jpg"));
+
+		final DoGSIFTEngine engine = new DoGSIFTEngine();
+
+		final LocalFeatureList<Keypoint> imageKeypoints = engine.findFeatures(image.flatten());
+
+		final byte[][] AllFeaturesDescriptors = new byte[imageKeypoints.size()][];
+
+		for (int i = 0; i < imageKeypoints.size(); i++) {
+			AllFeaturesDescriptors[i] = imageKeypoints.get(i).ivec;
+		}
+
+		/**
+		 * clustering
+		 */
+
+		final ByteKMeans byteKmeans = ByteKMeans.createKDTreeEnsemble(128, 300);
+
+		final ByteCentroidsResult result = byteKmeans.cluster(AllFeaturesDescriptors);
+
+		/**
+		 * hard assignement
+		 */
+		final HardAssigner<byte[], ?, ?> assigner = result.defaultHardAssigner();
+
+		/**
+		 * BagOfVisualWords construction!
+		 */
+		final BagOfVisualWords<byte[]> bagOfVisualWords = new BagOfVisualWords<byte[]>(assigner);
+
+		final Object quantizationResult = BagOfVisualWords.computeQuantisedFeatures(assigner, imageKeypoints);
+	}
+}
