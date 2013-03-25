78fe7664275f6b2b20e312c7ed591c6164f741db
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-03-25 12:27:56 +0000

    finishing the multilabel mode of the liblinear annotator

--- machine-learning/machine-learning/src/main/java/org/openimaj/ml/annotation/linear/LiblinearAnnotator.java
+++ machine-learning/machine-learning/src/main/java/org/openimaj/ml/annotation/linear/LiblinearAnnotator.java
@@ -22,6 +22,19 @@ import de.bwaldvogel.liblinear.Parameter;
 import de.bwaldvogel.liblinear.Problem;
 import de.bwaldvogel.liblinear.SolverType;
 
+/**
+ * Annotator based on linear classifiers learned using Liblinear (see
+ * {@link Linear}). Two modes of operation are available depending on whether
+ * the problem is multiclass or multilabel. Binary classification can be
+ * achieved with either mode, although multiclass mode is more efficient in this
+ * case.
+ * 
+ * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
+ * 
+ * @param <OBJECT>
+ * @param <ANNOTATION>
+ * @param <EXTRACTOR>
+ */
 public class LiblinearAnnotator<OBJECT, ANNOTATION, EXTRACTOR extends FeatureExtractor<? extends FeatureVector, OBJECT>>
 		extends
 		BatchAnnotator<OBJECT, ANNOTATION, EXTRACTOR>
@@ -96,9 +109,18 @@ public class LiblinearAnnotator<OBJECT, ANNOTATION, EXTRACTOR extends FeatureExt
 		}
 	}
 
+	/**
+	 * Multi-label classifier built from multiple binary classifiers.
+	 * 
+	 * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
+	 * 
+	 */
 	class Multilabel implements InternalModel<OBJECT, ANNOTATION> {
 		private Parameter parameter;
 		private Model[] models;
+
+		private static final int NEGATIVE_CLASS = 1;
+		private static final int POSTIVE_CLASS = 2;
 
 		public Multilabel(SolverType solver, double C, double eps) {
 			parameter = new Parameter(solver, C, eps);
@@ -128,31 +150,42 @@ public class LiblinearAnnotator<OBJECT, ANNOTATION, EXTRACTOR extends FeatureExt
 				problem.y = new double[problem.l];
 
 				for (int j = 0; j < negative.size(); j++) {
-
+					problem.x[j] = LiblinearHelper.convert(negative.get(j));
+					problem.y[j] = NEGATIVE_CLASS;
 				}
 
-				// for (int i = 0; i < nItems; i++) {
-				// final Annotated<OBJECT, ANNOTATION> object = data.get(i);
-				//
-				// if (object.getAnnotations().size() != 1)
-				// throw new IllegalArgumentException(
-				// "A multiclass problem cannot have more than one class per instance");
-				//
-				// final ANNOTATION annotation =
-				// object.getAnnotations().iterator().next();
-				//
-				// problem.y[i] = annotationsList.indexOf(annotation) + 1;
-				// problem.x[i] = computeFeature(object.getObject());
-				// }
-				//
-				// model = Linear.train(problem, parameter);
+				for (int j = negative.size(), k = 0; k < positive.size(); j++, k++) {
+					problem.x[j] = LiblinearHelper.convert(positive.get(k));
+					problem.y[j] = POSTIVE_CLASS;
+				}
+
+				models[i] = Linear.train(problem, parameter);
 			}
 		}
 
 		@Override
 		public List<ScoredAnnotation<ANNOTATION>> annotate(OBJECT object) {
-			// TODO Auto-generated method stub
-			return null;
+			final Feature[] feature = computeFeature(object);
+			final List<ScoredAnnotation<ANNOTATION>> result = new ArrayList<ScoredAnnotation<ANNOTATION>>();
+
+			for (int i = 0; i < annotationsList.size(); i++) {
+				final double clz;
+				final double prob;
+				if (parameter.getSolverType().isLogisticRegressionSolver()) {
+					final double[] probs = new double[annotations.size()];
+					clz = Linear.predictProbability(models[i], feature, probs);
+					prob = probs[(int) clz - 1];
+				} else {
+					clz = Linear.predict(models[i], feature);
+					prob = 1;
+				}
+
+				if (clz == POSTIVE_CLASS) {
+					result.add(new ScoredAnnotation<ANNOTATION>(annotationsList.get(i), (float) prob));
+				}
+			}
+
+			return result;
 		}
 	}
 
@@ -160,11 +193,19 @@ public class LiblinearAnnotator<OBJECT, ANNOTATION, EXTRACTOR extends FeatureExt
 	private Set<ANNOTATION> annotations;
 	private ArrayList<ANNOTATION> annotationsList;
 
-	public LiblinearAnnotator(EXTRACTOR extractor) {
+	public LiblinearAnnotator(EXTRACTOR extractor, Mode mode, SolverType solver, double C, double eps) {
 		super(extractor);
 
-		// FIXME
-		this.internal = new Multiclass(SolverType.L2R_LR, 1.0, 0.01);
+		switch (mode) {
+		case MULTICLASS:
+			this.internal = new Multiclass(solver, C, eps);
+			break;
+		case MULTILABEL:
+			this.internal = new Multilabel(solver, C, eps);
+			break;
+		default:
+			throw new RuntimeException("Unhandled mode");
+		}
 	}
 
 	private Feature[] computeFeature(OBJECT object) {
@@ -190,13 +231,13 @@ public class LiblinearAnnotator<OBJECT, ANNOTATION, EXTRACTOR extends FeatureExt
 
 	public static void main(String[] args) {
 		final LiblinearAnnotator<DoubleFV, String, IdentityFeatureExtractor<DoubleFV>> ann = new LiblinearAnnotator<DoubleFV, String, IdentityFeatureExtractor<DoubleFV>>(
-				new IdentityFeatureExtractor<DoubleFV>());
+				new IdentityFeatureExtractor<DoubleFV>(), Mode.MULTICLASS, SolverType.L2R_LR, 1.0, 0.01);
 
 		final List<Annotated<DoubleFV, String>> trainingData = new ArrayList<Annotated<DoubleFV, String>>();
 		trainingData.add(AnnotatedObject.create(new DoubleFV(new double[] { -1.0 }), "neg"));
 		trainingData.add(AnnotatedObject.create(new DoubleFV(new double[] { 1.0 }), "pos"));
 		ann.train(trainingData);
 
-		System.out.println(ann.annotate(new DoubleFV(new double[] { -10.0 })));
+		System.out.println(ann.annotate(new DoubleFV(new double[] { 10.0 })));
 	}
 }
