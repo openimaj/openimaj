2e8621276b8c37c1c9452cb48160647683fa6f89
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-04-04 18:27:55 +0000

    code for tutorial on image datasets

---
+++ documentation/tutorial/tutorial-code/src/main/java/org/openimaj/docs/tutorial/images/datasets/App.java
@@ -0,0 +1,62 @@
+package org.openimaj.docs.tutorial.images.datasets;
+
+import java.util.Map.Entry;
+
+import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.dataset.BingImageDataset;
+import org.openimaj.image.dataset.FlickrImageDataset;
+import org.openimaj.util.api.auth.DefaultTokenFactory;
+import org.openimaj.util.api.auth.common.BingAPIToken;
+import org.openimaj.util.api.auth.common.FlickrAPIToken;
+
+/**
+ * OpenIMAJ Hello world!
+ * 
+ */
+public class App {
+	/**
+	 * Main method
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		/**
+		 * Build a dataset from images in a zip file
+		 */
+		final VFSListDataset<FImage> faces = new VFSListDataset<FImage>("zip:http://datasets.openimaj.org/att_faces.zip",
+				ImageUtilities.FIMAGE_READER);
+		System.out.println(faces.size());
+		DisplayUtilities.display("ATT faces", faces);
+
+		/**
+		 * Build a grouped dataset from images in directories in a zip file
+		 */
+		final VFSGroupDataset<FImage> groupedFaces = new VFSGroupDataset<FImage>(
+				"zip:http://datasets.openimaj.org/att_faces.zip", ImageUtilities.FIMAGE_READER);
+		for (final Entry<String, VFSListDataset<FImage>> entry : groupedFaces.entrySet()) {
+			DisplayUtilities.display(entry.getKey(), entry.getValue());
+		}
+
+		/**
+		 * Search for cats on Flickr
+		 */
+		final FlickrAPIToken flickrToken = DefaultTokenFactory.get(FlickrAPIToken.class);
+		final FlickrImageDataset<FImage> cats = FlickrImageDataset.create(ImageUtilities.FIMAGE_READER, flickrToken,
+				"cat", 10);
+		DisplayUtilities.display("Cats", cats);
+
+		/**
+		 * Search for pics of Arnie on Bing
+		 */
+		final BingAPIToken bingToken = DefaultTokenFactory.get(BingAPIToken.class);
+		final BingImageDataset<MBFImage> arnie = BingImageDataset.create(ImageUtilities.MBFIMAGE_READER, bingToken,
+				"Arnold Schwarzenegger", 10);
+		DisplayUtilities.display("Arnie", arnie);
+	}
+}
