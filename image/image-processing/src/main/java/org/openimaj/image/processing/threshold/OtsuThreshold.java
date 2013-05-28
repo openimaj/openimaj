a0c88013d103d4d9168bfa3d95d6e85704fbe2b1
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-05-28 22:39:13 +0000

    allow number of bins to be set

--- image/image-processing/src/main/java/org/openimaj/image/processing/threshold/OtsuThreshold.java
+++ image/image-processing/src/main/java/org/openimaj/image/processing/threshold/OtsuThreshold.java
@@ -29,26 +29,63 @@
  */
 package org.openimaj.image.processing.threshold;
 
+import org.openimaj.citation.annotation.Reference;
+import org.openimaj.citation.annotation.ReferenceType;
 import org.openimaj.image.FImage;
 import org.openimaj.image.processor.ImageProcessor;
+import org.openimaj.util.array.ArrayUtils;
 
 /**
- * Otsu's adaptive thresholding algorithm. 
+ * Otsu's adaptive thresholding algorithm.
  * 
- * @see <a href="http://en.wikipedia.org/wiki/Otsu's_method">http://en.wikipedia.org/wiki/Otsu&apos;s_method</a>
+ * @see <a
+ *      href="http://en.wikipedia.org/wiki/Otsu's_method">http://en.wikipedia.org/wiki/Otsu&apos;s_method</a>
  * 
  * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
  */
+@Reference(
+		type = ReferenceType.Article,
+		author = { "Nobuyuki Otsu" },
+		title = "A Threshold Selection Method from Gray-Level Histograms",
+		year = "1979",
+		journal = "Systems, Man and Cybernetics, IEEE Transactions on",
+		pages = { "62", "66" },
+		number = "1",
+		volume = "9",
+		customData = {
+				"keywords",
+				"Displays;Gaussian distribution;Histograms;Least squares approximation;Marine vehicles;Q measurement;Radar tracking;Sea measurements;Surveillance;Target tracking",
+				"doi", "10.1109/TSMC.1979.4310076",
+				"ISSN", "0018-9472"
+		})
 public class OtsuThreshold implements ImageProcessor<FImage> {
-	private static final int NUM_BINS = 256;
+	private static final int DEFAULT_NUM_BINS = 256;
+	int numBins = DEFAULT_NUM_BINS;
 
-	protected int[] makeHistogram(FImage fimg) {
-		int [] histData = new int[NUM_BINS];
+	/**
+	 * Default constructor
+	 */
+	public OtsuThreshold() {
+
+	}
+
+	/**
+	 * Construct with the given number of histogram bins
+	 * 
+	 * @param numBins
+	 *            the number of histogram bins
+	 */
+	public OtsuThreshold(int numBins) {
+		this.numBins = numBins;
+	}
+
+	protected static int[] makeHistogram(FImage fimg, int numBins) {
+		final int[] histData = new int[numBins];
 
 		// Calculate histogram
-		for (int r=0; r<fimg.height; r++) {
-			for (int c=0; c<fimg.width; c++) {
-				int h = (int) (fimg.pixels[r][c] * (NUM_BINS - 1));
+		for (int r = 0; r < fimg.height; r++) {
+			for (int c = 0; c < fimg.width; c++) {
+				final int h = (int) (fimg.pixels[r][c] * (numBins - 1));
 				histData[h]++;
 			}
 		}
@@ -56,19 +93,73 @@ public class OtsuThreshold implements ImageProcessor<FImage> {
 		return histData;
 	}
 
+	protected static int[] makeHistogram(float[] data, int numBins, float min, float max) {
+		final int[] histData = new int[numBins];
+
+		// Calculate histogram
+		for (int c = 0; c < data.length; c++) {
+			final float d = (data[c] - min) / (max - min);
+			final int h = (int) (d * (numBins - 1));
+			histData[h]++;
+		}
+
+		return histData;
+	}
+
 	/**
 	 * Estimate the threshold for the given image.
-	 * @param img the image
+	 * 
+	 * @param img
+	 *            the image
+	 * @param numBins
+	 *            the number of histogram bins
 	 * @return the estimated threshold
 	 */
-	public float calculateThreshold(FImage img) {
-		int [] histData = makeHistogram(img);
+	public static float calculateThreshold(FImage img, int numBins) {
+		final int[] histData = makeHistogram(img, numBins);
 
 		// Total number of pixels
-		int total = img.getWidth() * img.getHeight();
+		final int total = img.getWidth() * img.getHeight();
 
+		return computeThresholdFromHistogram(histData, total);
+	}
+
+	/**
+	 * Estimate the threshold for the given data.
+	 * <p>
+	 * Internally, the data will be min-max normalised before the histogram is
+	 * built, and the specified number of bins will cover the entire
+	 * <code>max-min</code> range. The returned threshold will have
+	 * <code>min</code> added to it to return it to the original range.
+	 * 
+	 * @param data
+	 *            the data
+	 * @param numBins
+	 *            the number of histogram bins
+	 * @return the estimated threshold
+	 */
+	public static float calculateThreshold(float[] data, int numBins) {
+		final float min = ArrayUtils.minValue(data);
+		final float max = ArrayUtils.maxValue(data);
+		final int[] histData = makeHistogram(data, numBins, min, max);
+
+		return computeThresholdFromHistogram(histData, data.length) + min;
+	}
+
+	/**
+	 * Estimate the threshold for the given histogram.
+	 * 
+	 * @param histData
+	 *            the histogram
+	 * @param total
+	 *            the total number of items in the histogram
+	 * @return the estimated threshold
+	 */
+	public static float computeThresholdFromHistogram(int[] histData, int total) {
+		final int numBins = histData.length;
 		float sum = 0;
-		for (int t=0; t<NUM_BINS; t++) sum += t * histData[t];
+		for (int t = 0; t < numBins; t++)
+			sum += t * histData[t];
 
 		float sumB = 0;
 		int wB = 0;
@@ -77,20 +168,22 @@ public class OtsuThreshold implements ImageProcessor<FImage> {
 		float varMax = 0;
 		float threshold = 0;
 
-		for (int t=0; t<NUM_BINS; t++) {
-			wB += histData[t];               // Weight Background
-			if (wB == 0) continue;
+		for (int t = 0; t < numBins; t++) {
+			wB += histData[t]; // Weight Background
+			if (wB == 0)
+				continue;
 
-			wF = total - wB;                 // Weight Foreground
-			if (wF == 0) break;
+			wF = total - wB; // Weight Foreground
+			if (wF == 0)
+				break;
 
 			sumB += (t * histData[t]);
 
-			float mB = sumB / wB;            // Mean Background
-			float mF = (sum - sumB) / wF;    // Mean Foreground
+			final float mB = sumB / wB; // Mean Background
+			final float mF = (sum - sumB) / wF; // Mean Foreground
 
 			// Calculate Between Class Variance
-			float varBetween = (float)wB * (float)wF * (mB - mF) * (mB - mF);
+			final float varBetween = (float) wB * (float) wF * (mB - mF) * (mB - mF);
 
 			// Check if new maximum found
 			if (varBetween > varMax) {
@@ -99,12 +192,12 @@ public class OtsuThreshold implements ImageProcessor<FImage> {
 			}
 		}
 
-		return threshold / (NUM_BINS - 1);
+		return threshold / (numBins - 1);
 	}
 
 	@Override
 	public void processImage(FImage image) {
-		float threshold = calculateThreshold(image);
+		final float threshold = calculateThreshold(image, numBins);
 
 		image.threshold(threshold);
 	}
