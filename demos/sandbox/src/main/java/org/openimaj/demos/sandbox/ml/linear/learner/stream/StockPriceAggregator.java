689fdaf159d51607dc9f6f1d2fc4bf10e308b83c
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-06-01 14:48:14 +0000

    work around compiler typing issue

--- demos/sandbox/src/main/java/org/openimaj/demos/sandbox/ml/linear/learner/stream/StockPriceAggregator.java
+++ demos/sandbox/src/main/java/org/openimaj/demos/sandbox/ml/linear/learner/stream/StockPriceAggregator.java
@@ -16,10 +16,12 @@ import org.openimaj.util.stream.Stream;
 import org.openimaj.util.stream.window.SequentialStreamAggregator;
 
 /**
- * Given a paired stream of user-word-counts and ticker-price, implement a {@link SequentialStreamAggregator}
- * with a comparator which tests stock prices. Identical stock prices (i.e. the same for each ticker) are combined
+ * Given a paired stream of user-word-counts and ticker-price, implement a
+ * {@link SequentialStreamAggregator} with a comparator which tests stock
+ * prices. Identical stock prices (i.e. the same for each ticker) are combined
+ * 
  * @author Sina Samangooei (ss@ecs.soton.ac.uk)
- *
+ * 
  */
 public final class StockPriceAggregator
 		extends
@@ -36,24 +38,27 @@ public final class StockPriceAggregator
 		}
 
 		@Override
-		public int compare(Context o1,Context o2)
+		public int compare(Context o1, Context o2)
 		{
-			Map<String, Double> a = o1.getTyped("averageticks");
-			Map<String, Double> b = o2.getTyped("averageticks");
+			final Map<String, Double> a = o1.getTyped("averageticks");
+			final Map<String, Double> b = o2.getTyped("averageticks");
 
-			Set<String> sharedKeys = new HashSet<String>(a.keySet());
+			final Set<String> sharedKeys = new HashSet<String>(a.keySet());
 			sharedKeys.addAll(b.keySet());
 			double diff = 0;
-			for (String ticker : sharedKeys) {
+			for (final String ticker : sharedKeys) {
 				diff += a.get(ticker) - b.get(ticker);
 			}
-			if(Math.abs(diff) < this.thresh) return 0;
-			else if(diff<0)return -1;
-			else if(diff>0)return 1;
-			else return 0;
+			if (Math.abs(diff) < this.thresh)
+				return 0;
+			else if (diff < 0)
+				return -1;
+			else if (diff > 0)
+				return 1;
+			else
+				return 0;
 		}
 	}
-
 
 	public StockPriceAggregator(double thresh)
 	{
@@ -63,108 +68,125 @@ public final class StockPriceAggregator
 	@Override
 	public Context combine(List<Context> window)
 	{
-		Map<String, Map<String, Double>> combinedUserWords = new HashMap<String, Map<String,Double>>();
-		int nItems = window.size();
+		final Map<String, Map<String, Double>> combinedUserWords = new HashMap<String, Map<String, Double>>();
+		final int nItems = window.size();
 		Map<String, Double> stocks = null;
-		long timestamp = window.get(0).getTyped("timestamp");
-		for (Context itemAggr : window) {
-			Map<String, Map<String, Double>> bagofwords = itemAggr.getTyped("bagofwords");
-			stocks = itemAggr.getTyped("averageticks"); // They should all be the same
-			for (Entry<String, Map<String, Double>> es : bagofwords.entrySet()) {
-				addUsers(combinedUserWords,es,nItems);
+		final long timestamp = (Long) window.get(0).getTyped("timestamp");
+		for (final Context itemAggr : window) {
+			final Map<String, Map<String, Double>> bagofwords = itemAggr.getTyped("bagofwords");
+			stocks = itemAggr.getTyped("averageticks"); // They should all be
+														// the same
+			for (final Entry<String, Map<String, Double>> es : bagofwords.entrySet()) {
+				addUsers(combinedUserWords, es, nItems);
 			}
 		}
-		Context newC = window.get(0).clone();
+		final Context newC = window.get(0).clone();
 		newC.put("timestamp", timestamp);
 		newC.put("bagofwords", combinedUserWords);
 		newC.put("averageticks", stocks);
 		return newC;
 	}
 
-	private void addUsers(Map<String, Map<String, Double>> combinedUserWords, Entry<String, Map<String, Double>> es, int nItems) {
+	private void addUsers(Map<String, Map<String, Double>> combinedUserWords, Entry<String, Map<String, Double>> es,
+			int nItems)
+	{
 		Map<String, Double> userWords = combinedUserWords.get(es.getKey());
-		if(userWords == null) combinedUserWords.put(es.getKey(), userWords = new HashMap<String, Double>());
-		for (Entry<String, Double> wordTotals : es.getValue().entrySet()) {
-			addWord(userWords,wordTotals,nItems);
+		if (userWords == null)
+			combinedUserWords.put(es.getKey(), userWords = new HashMap<String, Double>());
+		for (final Entry<String, Double> wordTotals : es.getValue().entrySet()) {
+			addWord(userWords, wordTotals, nItems);
 		}
 	}
 
-	private void addWord(Map<String, Double> userWords,Entry<String, Double> wordTotals, int nItems) {
-		String word = wordTotals.getKey();
+	private void addWord(Map<String, Double> userWords, Entry<String, Double> wordTotals, int nItems) {
+		final String word = wordTotals.getKey();
 		Double wordTotal = userWords.get(word);
-		if(wordTotal == null) wordTotal = 0d;
-		userWords.put(word, wordTotal + (wordTotals.getValue()/nItems));
+		if (wordTotal == null)
+			wordTotal = 0d;
+		userWords.put(word, wordTotal + (wordTotals.getValue() / nItems));
 	}
 
 	/**
 	 * tests
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		List<Context> coll = new ArrayList<Context>();
+		final List<Context> coll = new ArrayList<Context>();
 
-		Map<String,Map<String,Double>> t1 = sparcify("u1: this is cheese","u2: cheese is good","u3: i like cheese");
-		Map<String,Map<String,Double>> t2 = sparcify("u1: it is still cheese","u3: cheese is good","u4: i like cheese");
-		Map<String,Map<String,Double>> t3 = sparcify("u2: this is cheese");
-		Map<String,Map<String,Double>> t4 = sparcify("u1: this is cheese","u2: cheese is good","u3: i like cheese");
-		Map<String,Double> price1 = sparcifyPrice("p1: 100","p2: 200");
-		Map<String,Double> price2 = sparcifyPrice("p1: 100","p2: 201");
-
+		final Map<String, Map<String, Double>> t1 = sparcify("u1: this is cheese", "u2: cheese is good",
+				"u3: i like cheese");
+		final Map<String, Map<String, Double>> t2 = sparcify("u1: it is still cheese", "u3: cheese is good",
+				"u4: i like cheese");
+		final Map<String, Map<String, Double>> t3 = sparcify("u2: this is cheese");
+		final Map<String, Map<String, Double>> t4 = sparcify("u1: this is cheese", "u2: cheese is good",
+				"u3: i like cheese");
+		final Map<String, Double> price1 = sparcifyPrice("p1: 100", "p2: 200");
+		final Map<String, Double> price2 = sparcifyPrice("p1: 100", "p2: 201");
 
 		Context c = new Context();
-		c.put("timestamp", 0l); c.put("bagofwords", t1); c.put("averageticks", price1);
+		c.put("timestamp", 0l);
+		c.put("bagofwords", t1);
+		c.put("averageticks", price1);
 		coll.add(c);
 		c = new Context();
-		c.put("timestamp", 1l); c.put("bagofwords", t2); c.put("averageticks", price1);
+		c.put("timestamp", 1l);
+		c.put("bagofwords", t2);
+		c.put("averageticks", price1);
 		coll.add(c);
 		c = new Context();
-		c.put("timestamp", 2l); c.put("bagofwords", t3); c.put("averageticks", price2);
+		c.put("timestamp", 2l);
+		c.put("bagofwords", t3);
+		c.put("averageticks", price2);
 		coll.add(c);
 		c = new Context();
-		c.put("timestamp", 3l); c.put("bagofwords", t4); c.put("averageticks", price1);
+		c.put("timestamp", 3l);
+		c.put("bagofwords", t4);
+		c.put("averageticks", price1);
 		coll.add(c);
-		Stream<Context> stream = new CollectionStream<Context>(coll);
+		final Stream<Context> stream = new CollectionStream<Context>(coll);
 		stream.transform(new StockPriceAggregator(0.0001)).forEach(
-			new Operation<Context>() {
+				new Operation<Context>() {
 
-			@Override
-			public void perform(Context object) {
-				System.out.println(object.getTyped("timestamp"));
-				System.out.println(object.getTyped("bagofwords"));
-				System.out.println(object.getTyped("averageticks"));
-			}
-		});
+					@Override
+					public void perform(Context object) {
+						System.out.println(object.getTyped("timestamp"));
+						System.out.println(object.getTyped("bagofwords"));
+						System.out.println(object.getTyped("averageticks"));
+					}
+				});
 
 	}
 
-	private static Map<String, Double> sparcifyPrice(String ...strings ) {
+	private static Map<String, Double> sparcifyPrice(String... strings) {
 
-		Map<String, Double> ret = new HashMap<String, Double>();
-		for (String string : strings) {
-			String[] tickPrice = string.split(":");
+		final Map<String, Double> ret = new HashMap<String, Double>();
+		for (final String string : strings) {
+			final String[] tickPrice = string.split(":");
 			ret.put(tickPrice[0].trim(), Double.parseDouble(tickPrice[1].trim()));
-		}
-		return ret ;
-	}
-
-	private static Map<String, Map<String, Double>> sparcify(String ...strings) {
-		Map<String, Map<String, Double>> ret = new HashMap<String, Map<String,Double>>();
-		for (String string : strings) {
-			String[] userWords = string.split(":");
-			String user = userWords[0].trim();
-			String[] words = userWords[1].trim().split(" ");
-
-			Map<String,Double> wordCounts = ret.get(user);
-			if(wordCounts == null) ret.put(user, wordCounts = new HashMap<String, Double>());
-			ret.put(user, wordCounts);
-			for (String word : words) {
-				Double count = wordCounts.get(word);
-				if(count == null) count = 0d;
-				wordCounts.put(word, count+1);
-			}
 		}
 		return ret;
 	}
 
+	private static Map<String, Map<String, Double>> sparcify(String... strings) {
+		final Map<String, Map<String, Double>> ret = new HashMap<String, Map<String, Double>>();
+		for (final String string : strings) {
+			final String[] userWords = string.split(":");
+			final String user = userWords[0].trim();
+			final String[] words = userWords[1].trim().split(" ");
+
+			Map<String, Double> wordCounts = ret.get(user);
+			if (wordCounts == null)
+				ret.put(user, wordCounts = new HashMap<String, Double>());
+			ret.put(user, wordCounts);
+			for (final String word : words) {
+				Double count = wordCounts.get(word);
+				if (count == null)
+					count = 0d;
+				wordCounts.put(word, count + 1);
+			}
+		}
+		return ret;
+	}
 
 }
--- demos/sandbox/src/main/java/org/openimaj/demos/sandbox/ml/linear/learner/stream/recorder/GeoFinancialSearchAPIRecorder.java
+++ demos/sandbox/src/main/java/org/openimaj/demos/sandbox/ml/linear/learner/stream/recorder/GeoFinancialSearchAPIRecorder.java
@@ -42,10 +42,11 @@ import com.mongodb.util.JSON;
 
 /**
  * @author Sina Samangooei (ss@ecs.soton.ac.uk)
- *
+ * 
  */
 public class GeoFinancialSearchAPIRecorder {
 	static Logger logger = Logger.getLogger(GeoFinancialSearchAPIRecorder.class);
+
 	/**
 	 * @param args
 	 * @throws MalformedURLException
@@ -54,13 +55,14 @@ public class GeoFinancialSearchAPIRecorder {
 	public static void main(String[] args) throws MalformedURLException, IOException {
 
 		// The financial stream
-		String[] tickers = new String[]{
-			"apple","google","virgin","oracle","sony","microsoft"
+		final String[] tickers = new String[] {
+				"apple", "google", "virgin", "oracle", "sony", "microsoft"
 		};
-		ContextRealTimeWindowFunction<Map<String, Double>> yahooWindow = new ContextRealTimeWindowFunction<Map<String,Double>>(5000);
-		Stream<Context> yahooAveragePriceStream = new YahooFinanceStream(true,tickers).transform(yahooWindow);
+		final ContextRealTimeWindowFunction<Map<String, Double>> yahooWindow = new ContextRealTimeWindowFunction<Map<String, Double>>(
+				5000);
+		final Stream<Context> yahooAveragePriceStream = new YahooFinanceStream(true, tickers).transform(yahooWindow);
 
-		List<Map<String,String>> geoLocs = loadGeoLocs("/org/openimaj/demos/sandbox/ml/linear/learner/stream/locations_input_srv_II.txt");
+		final List<Map<String, String>> geoLocs = loadGeoLocs("/org/openimaj/demos/sandbox/ml/linear/learner/stream/locations_input_srv_II.txt");
 
 		// The Twitter Stream
 		final ArrayBlockingDroppingQueue<Context> buffer = new ArrayBlockingDroppingQueue<Context>(1000);
@@ -69,115 +71,121 @@ public class GeoFinancialSearchAPIRecorder {
 		final TokeniseMode tokeniseMode = new TokeniseMode();
 		final CountryCodeMode ccm = new CountryCodeMode();
 
-		Stream<Context> twitterUserWordCountStream =
-			new ContextRoundRobinTwitterSearchAPIDataset(
-				geoLocQueries(geoLocs),
-				DefaultTokenFactory.get(TwitterAPIToken.class),
-				buffer
-			)
-			.transform(new ContextRealTimeWindowFunction<Context>(10000))
-			.map(
-				new ContextListFunction<Context, Context>("item",
-					new ContextTwitterStatusAsUSMFStatus()
+		final Stream<Context> twitterUserWordCountStream =
+				new ContextRoundRobinTwitterSearchAPIDataset(
+						geoLocQueries(geoLocs),
+						DefaultTokenFactory.get(TwitterAPIToken.class),
+						buffer
 				)
-			)
-			.map(
-				new ContextListFunction<Context, Context>("item",
-					new ContextFunction<USMFStatus,USMFStatus>("usmfstatus",
-						new TwitterPreprocessingFunction(languageDetectionMode,tokeniseMode,stopwordMode)
-					)
-				)
-		);
-//		twitterUserWordCountStream.forEach(new Operation<Context>() {
-//
-//			@Override
-//			public void perform(Context objectTime) {
-//				List<Context> object = objectTime.getTyped("item");
-//				for (Context statusContext : object) {
-//					USMFStatus usmfStatus = statusContext.getTyped("usmfstatus");
-//					System.out.format("@%s (location: %s): (geo: %s) (place: %s)\n",usmfStatus.user.name,usmfStatus.user.location,usmfStatus.geo,usmfStatus.location);
-//				}
-//			}
-//
-//		});
-		List<ServerAddress> serverList = Arrays.asList(
-			new ServerAddress("rumi",27017),
-			new ServerAddress("hafez",27017)
-		);
-		StreamCombiner.combine(twitterUserWordCountStream,yahooAveragePriceStream)
-		.forEach(
-			new MongoDBOutputOp<
-				IndependentPair<
-					Context,
-					Context
-			>>
-			(serverList) {
+						.transform(new ContextRealTimeWindowFunction<Context>(10000))
+						.map(
+								new ContextListFunction<Context, Context>("item",
+										new ContextTwitterStatusAsUSMFStatus()
+								)
+						)
+						.map(
+								new ContextListFunction<Context, Context>("item",
+										new ContextFunction<USMFStatus, USMFStatus>("usmfstatus",
+												new TwitterPreprocessingFunction(languageDetectionMode, tokeniseMode,
+														stopwordMode)
+										)
+								)
+						);
+		// twitterUserWordCountStream.forEach(new Operation<Context>() {
+		//
+		// @Override
+		// public void perform(Context objectTime) {
+		// List<Context> object = objectTime.getTyped("item");
+		// for (Context statusContext : object) {
+		// USMFStatus usmfStatus = statusContext.getTyped("usmfstatus");
+		// System.out.format("@%s (location: %s): (geo: %s) (place: %s)\n",usmfStatus.user.name,usmfStatus.user.location,usmfStatus.geo,usmfStatus.location);
+		// }
+		// }
+		//
+		// });
+		final List<ServerAddress> serverList = Arrays.asList(
+				new ServerAddress("rumi", 27017),
+				new ServerAddress("hafez", 27017)
+				);
+		StreamCombiner.combine(twitterUserWordCountStream, yahooAveragePriceStream)
+				.forEach(
+						new MongoDBOutputOp<
+						IndependentPair<
+						Context,
+						Context
+						>>
+						(serverList)
+						{
 
-				@Override
-				public String getCollectionName() {
-					return "searchapi_yahoo_billgeo";
-				}
+							@Override
+							public String getCollectionName() {
+								return "searchapi_yahoo_billgeo";
+							}
 
-				@Override
-				public DBObject asDBObject(IndependentPair<Context,Context> obj) {
-					BasicDBObject dbobj = new BasicDBObject();
-					List<Context> tweets = obj.firstObject().getTyped("item");
-					List<Object> dbtweets = new ArrayList<Object>();
-					List<Object> normaltweets = new ArrayList<Object>();
-					String actualQuery = null;
-					HashMap<String,List<Integer>> queries = new HashMap<String,List<Integer>>();
-					int item = 0;
-					for (Context tweetContext : tweets) {
-						USMFStatus usmfStatus = tweetContext.getTyped("usmfstatus");
-						dbtweets.add(JSON.parse(usmfStatus.toJson()));
-						normaltweets.add(JSON.parse((String)tweetContext.getTyped("status_json")));
-						actualQuery = ((Query)tweetContext.getTyped("query")).getGeocode();
-						List<Integer> itemMap = queries.get(actualQuery);
-						if(itemMap == null) queries.put(actualQuery, itemMap  = new ArrayList<Integer>());
-						itemMap.add(item++);
-					}
-					if(actualQuery!=null){
-						dbobj.append("twitter_query", prepareQueries(queries));
-					}
-					dbobj.append("tweets", dbtweets);
-					dbobj.append("tweets_raw", normaltweets);
-					dbobj.append("search", "bill area code");
-					List<?> stockTicks = obj.getSecondObject().getTyped("item");
-					dbobj.append("tickers", stockTicks);
-					long timestamp = obj.getSecondObject().getTyped("windowstart");
-					dbobj.append("timestamp", timestamp);
-					logger.debug(String.format("Dumping %d tweets and %d stock-ticks at %d with %d queries",dbtweets.size(),stockTicks.size(),timestamp,queries.size()));
-					return dbobj;
-				}
+							@Override
+							public DBObject asDBObject(IndependentPair<Context, Context> obj) {
+								final BasicDBObject dbobj = new BasicDBObject();
+								final List<Context> tweets = obj.firstObject().getTyped("item");
+								final List<Object> dbtweets = new ArrayList<Object>();
+								final List<Object> normaltweets = new ArrayList<Object>();
+								String actualQuery = null;
+								final HashMap<String, List<Integer>> queries = new HashMap<String, List<Integer>>();
+								int item = 0;
+								for (final Context tweetContext : tweets) {
+									final USMFStatus usmfStatus = tweetContext.getTyped("usmfstatus");
+									dbtweets.add(JSON.parse(usmfStatus.toJson()));
+									normaltweets.add(JSON.parse((String) tweetContext.getTyped("status_json")));
+									actualQuery = ((Query) tweetContext.getTyped("query")).getGeocode();
+									List<Integer> itemMap = queries.get(actualQuery);
+									if (itemMap == null)
+										queries.put(actualQuery, itemMap = new ArrayList<Integer>());
+									itemMap.add(item++);
+								}
+								if (actualQuery != null) {
+									dbobj.append("twitter_query", prepareQueries(queries));
+								}
+								dbobj.append("tweets", dbtweets);
+								dbobj.append("tweets_raw", normaltweets);
+								dbobj.append("search", "bill area code");
+								final List<?> stockTicks = obj.getSecondObject().getTyped("item");
+								dbobj.append("tickers", stockTicks);
+								final long timestamp = (Long) obj.getSecondObject().getTyped("windowstart");
+								dbobj.append("timestamp", timestamp);
+								logger.debug(String.format("Dumping %d tweets and %d stock-ticks at %d with %d queries",
+										dbtweets.size(), stockTicks.size(), timestamp, queries.size()));
+								return dbobj;
+							}
 
-				private List<Map<String,Object>> prepareQueries(HashMap<String, List<Integer>> queries) {
-					List<Map<String, Object>> ret = new ArrayList<Map<String,Object>>();
-					for (Entry<String, List<Integer>> ql: queries.entrySet()) {
-						Map<String, Object> map = new HashMap<String, Object>();
-						map.put("geoquery", ql.getKey());
-						map.put("applies_to", ql.getValue());
-						ret.add(map );
-					}
-					return ret ;
-				}
+							private List<Map<String, Object>> prepareQueries(HashMap<String, List<Integer>> queries) {
+								final List<Map<String, Object>> ret = new ArrayList<Map<String, Object>>();
+								for (final Entry<String, List<Integer>> ql : queries.entrySet()) {
+									final Map<String, Object> map = new HashMap<String, Object>();
+									map.put("geoquery", ql.getKey());
+									map.put("applies_to", ql.getValue());
+									ret.add(map);
+								}
+								return ret;
+							}
 
-				@Override
-				public String getDBName() {
-					return "twitterticker";
-				}
-			}
-		);
+							@Override
+							public String getDBName() {
+								return "twitterticker";
+							}
+						}
+				);
 	}
+
 	private static List<Map<String, String>> loadGeoLocs(String resource) {
 		try {
-			String rawLocations = IOUtils.toString(GeoFinancialSearchAPIRecorder.class.getResourceAsStream(resource));
-			String[] lines = rawLocations.split("\n");
-			List<Map<String, String>> ret = new ArrayList<Map<String,String>>();
-			for (String line : lines) {
-				Map<String, String> location = new HashMap<String, String>();
-				String[] fileLoc = line.split("/");
-				String infoPart = fileLoc[fileLoc.length-1];
-				String[] infoParts = infoPart.split(",");
+			final String rawLocations = IOUtils.toString(GeoFinancialSearchAPIRecorder.class
+					.getResourceAsStream(resource));
+			final String[] lines = rawLocations.split("\n");
+			final List<Map<String, String>> ret = new ArrayList<Map<String, String>>();
+			for (final String line : lines) {
+				final Map<String, String> location = new HashMap<String, String>();
+				final String[] fileLoc = line.split("/");
+				final String infoPart = fileLoc[fileLoc.length - 1];
+				final String[] infoParts = infoPart.split(",");
 				location.put("country", infoParts[0]);
 				location.put("city", infoParts[1]);
 				location.put("lat", infoParts[2]);
@@ -188,25 +196,25 @@ public class GeoFinancialSearchAPIRecorder {
 				ret.add(location);
 			}
 			return ret;
-		} catch (Throwable e) {
+		} catch (final Throwable e) {
 			return null;
 		}
 	}
 
-	private static List<Query> geoLocQueries(List<Map<String,String>> geolocs){
-		List<Query> ret = new ArrayList<Query>();
-		for (Map<String, String> geoloc : geolocs) {
-			Query q = new Query();
+	private static List<Query> geoLocQueries(List<Map<String, String>> geolocs) {
+		final List<Query> ret = new ArrayList<Query>();
+		for (final Map<String, String> geoloc : geolocs) {
+			final Query q = new Query();
 			q.geoCode(
-				new GeoLocation(
-					Double.parseDouble(geoloc.get("lat")),
-					Double.parseDouble(geoloc.get("lon"))
-				),
-				Double.parseDouble(geoloc.get("rad")),
-				Query.KILOMETERS
-			);
+					new GeoLocation(
+							Double.parseDouble(geoloc.get("lat")),
+							Double.parseDouble(geoloc.get("lon"))
+					),
+					Double.parseDouble(geoloc.get("rad")),
+					Query.KILOMETERS
+					);
 			ret.add(q);
 		}
-		return ret ;
+		return ret;
 	}
 }
