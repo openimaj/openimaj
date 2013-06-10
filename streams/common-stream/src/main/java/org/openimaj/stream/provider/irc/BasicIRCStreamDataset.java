15c8f52b0b4a81194fc77e330c09441eef31d4d8
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-06-10 13:57:19 +0000

    make basic irc stream use a context object

--- streams/common-stream/src/main/java/org/openimaj/stream/provider/irc/BasicIRCStreamDataset.java
+++ streams/common-stream/src/main/java/org/openimaj/stream/provider/irc/BasicIRCStreamDataset.java
@@ -2,119 +2,38 @@ package org.openimaj.stream.provider.irc;
 
 import java.io.IOException;
 
-import org.openimaj.stream.provider.irc.BasicIRCStreamDataset.IRCMessage;
 import org.openimaj.util.concurrent.ArrayBlockingDroppingQueue;
 import org.openimaj.util.concurrent.BlockingDroppingQueue;
-import org.openimaj.util.function.Function;
+import org.openimaj.util.data.Context;
 
 /**
- * Basic streaming dataset from IRC messages. Message objects can have single
- * fields extracted by applying the {@link IRCMessageExtractionFunction}.
+ * Basic streaming dataset from IRC messages. Messages are provided as
+ * {@link Context} objects.
  * 
  * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
  * 
  */
-public class BasicIRCStreamDataset extends AbstractIRCStreamDataset<IRCMessage> {
+public class BasicIRCStreamDataset extends AbstractIRCStreamDataset<Context> {
 	/**
-	 * Representation of an IRC Message
-	 * 
-	 * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
+	 * The IRC channel name key
 	 */
-	public static class IRCMessage {
-		/**
-		 * The IRC channel name
-		 */
-		public String channel;
-		/**
-		 * The sender
-		 */
-		public String sender;
-		/**
-		 * The login
-		 */
-		public String login;
-		/**
-		 * The hostname
-		 */
-		public String hostname;
-		/**
-		 * The message text
-		 */
-		public String message;
-
-		IRCMessage(String channel, String sender, String login, String hostname, String message) {
-			this.channel = channel;
-			this.sender = sender;
-			this.login = login;
-			this.hostname = hostname;
-			this.message = message;
-		}
-	}
-
+	public String CHANNEL_KEY = "channel";
 	/**
-	 * Simple function to extract a single field from an {@link IRCMessage} as a
-	 * {@link String}.
-	 * 
-	 * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
-	 * 
+	 * The sender key
 	 */
-	public static class IRCMessageExtractionFunction implements Function<IRCMessage, String> {
-		/**
-		 * The data to output from the function
-		 * 
-		 * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
-		 * 
-		 */
-		public enum OutputMode {
-			/**
-			 * The IRC channel name
-			 */
-			Channel,
-			/**
-			 * The sender
-			 */
-			Sender,
-			/**
-			 * The login
-			 */
-			Login,
-			/**
-			 * The hostname
-			 */
-			Hostname,
-			/**
-			 * The message text
-			 */
-			Message
-		}
-
-		OutputMode output;
-
-		/**
-		 * 
-		 * @param output
-		 */
-		public IRCMessageExtractionFunction(OutputMode output) {
-			this.output = output;
-		}
-
-		@Override
-		public String apply(IRCMessage in) {
-			switch (output) {
-			case Channel:
-				return in.channel;
-			case Sender:
-				return in.sender;
-			case Login:
-				return in.login;
-			case Hostname:
-				return in.hostname;
-			case Message:
-				return in.message;
-			}
-			return null;
-		}
-	}
+	public String SENDER_KEY = "sender";
+	/**
+	 * The login key
+	 */
+	public String LOGIN_KEY = "login";
+	/**
+	 * The host name key
+	 */
+	public String HOSTNAME_KEY = "hostname";
+	/**
+	 * The message text key
+	 */
+	public String MESSAGE_KEY = "message";
 
 	/**
 	 * Connect to the given host and channel. Internally a
@@ -130,7 +49,7 @@ public class BasicIRCStreamDataset extends AbstractIRCStreamDataset<IRCMessage> 
 	public BasicIRCStreamDataset(String hostname, String channel)
 			throws IOException
 	{
-		this(new ArrayBlockingDroppingQueue<IRCMessage>(1), hostname, channel);
+		this(new ArrayBlockingDroppingQueue<Context>(1), hostname, channel);
 	}
 
 	/**
@@ -145,15 +64,22 @@ public class BasicIRCStreamDataset extends AbstractIRCStreamDataset<IRCMessage> 
 	 *            the channel
 	 * @throws IOException
 	 */
-	public BasicIRCStreamDataset(BlockingDroppingQueue<IRCMessage> buffer, String hostname, String channel)
+	public BasicIRCStreamDataset(BlockingDroppingQueue<Context> buffer, String hostname, String channel)
 			throws IOException
 	{
 		super(buffer, hostname, channel);
 	}
 
 	@Override
-	public IRCMessage construct(String channel, String sender, String login, String hostname, String message) {
-		return new IRCMessage(channel, sender, login, hostname, message);
-	}
+	public Context construct(String channel, String sender, String login, String hostname, String message) {
+		final Context ctx = new Context();
 
+		ctx.put(CHANNEL_KEY, channel);
+		ctx.put(SENDER_KEY, sender);
+		ctx.put(LOGIN_KEY, login);
+		ctx.put(HOSTNAME_KEY, hostname);
+		ctx.put(MESSAGE_KEY, message);
+
+		return ctx;
+	}
 }
