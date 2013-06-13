d80c9bd969a306dd76ed4c3edc157256507cf413
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-06-13 13:23:13 +0000

    temporarily disable test

--- text/nlp/src/test/java/org/openimaj/text/geocode/GeonamesIndexTest.java
+++ text/nlp/src/test/java/org/openimaj/text/geocode/GeonamesIndexTest.java
@@ -1,50 +1,42 @@
 package org.openimaj.text.geocode;
 
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocument;
-import org.apache.solr.common.SolrDocumentList;
-import org.junit.Before;
 import org.junit.Test;
-import org.openimaj.text.nlp.geocode.GeonamesIndex;
 
-/**
- * @author Sina Samangooei (ss@ecs.soton.ac.uk), Jonathan Hare (jsh2@ecs.soton.ac.uk), David Duplaw (dpd@ecs.soton.ac.uk)
- *
- */
 public class GeonamesIndexTest {
-	private GeonamesIndex geonames;
-
-	/**
-	 * 
-	 */
-	@Before
-	public void setup(){
-		System.setProperty("geonames.solr.home", "/Users/ss/Experiments/geonames/solr.short");
-		this.geonames = GeonamesIndex.instance();
-	}
-	
-	/**
-	 * @throws SolrServerException 
-	 * 
-	 */
+	// private GeonamesIndex geonames;
+	//
+	// /**
+	// *
+	// */
+	// @Before
+	// public void setup(){
+	// System.setProperty("geonames.solr.home",
+	// "/Users/ss/Experiments/geonames/solr.short");
+	// this.geonames = GeonamesIndex.instance();
+	// }
+	//
+	// /**
+	// * @throws SolrServerException
+	// *
+	// */
 	@Test
-	public void testIndex() throws SolrServerException{
-		
-		QueryResponse resp = this.geonames.query(
-			"{!type=dismax qf=\"country_code basic_name timezone alternames\" mm=1}" +
-			"visanceny",
-			100
-		);
-		SolrDocumentList results = resp.getResults();
-		for (SolrDocument solrDocument : results) {
-//			System.out.println(solrDocument.keySet());
-			System.out.println(
-				solrDocument.get("basic_name") + ", " + 
-				solrDocument.get("country_code")+ ", " +
-				solrDocument.get("timezone")
-			);
-		}
+	public void testIndex() throws SolrServerException {
+		//
+		// QueryResponse resp = this.geonames.query(
+		// "{!type=dismax qf=\"country_code basic_name timezone alternames\" mm=1}"
+		// +
+		// "visanceny",
+		// 100
+		// );
+		// SolrDocumentList results = resp.getResults();
+		// for (SolrDocument solrDocument : results) {
+		// // System.out.println(solrDocument.keySet());
+		// System.out.println(
+		// solrDocument.get("basic_name") + ", " +
+		// solrDocument.get("country_code")+ ", " +
+		// solrDocument.get("timezone")
+		// );
+		// }
 	}
-	
 }
