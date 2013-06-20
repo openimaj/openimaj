5d44d70cd98ee7521e5c288ed81dc14cfa3b284c
David Duplaw <david.dupplaw@wds.co>
2013-06-20 09:22:25 +0000

    Update to ACMMM after change to JavaSoundGrabber constructor

--- demos/ACMMM-Presentation/src/main/java/org/openimaj/demos/acmmm11/presentation/slides/AudioOutroSlide.java
+++ demos/ACMMM-Presentation/src/main/java/org/openimaj/demos/acmmm11/presentation/slides/AudioOutroSlide.java
@@ -47,79 +47,78 @@ import org.openimaj.util.pair.Pair;
 
 /**
  * Slide showing a picture overlayed with a live spectrogram.
- * 
+ *
  * @author David Dupplaw (dpd@ecs.soton.ac.uk)
  * @author Sina Samangooei (ss@ecs.soton.ac.uk)
  *
  */
 public class AudioOutroSlide extends PictureSlide implements Listener {
 
-	private int sampleChunkSize = 512;
+	private final int sampleChunkSize = 512;
 	private FImage spectra;
 	private BufferedImage buf = null;
 	private JavaSoundAudioGrabber xa;
 	private FrequencyAudioSource source;
-	
-	
+
+
 	/**
 	 * Default constructor
 	 * @param picture
 	 * @throws IOException
 	 */
-	public AudioOutroSlide(URL picture) throws IOException {
+	public AudioOutroSlide(final URL picture) throws IOException {
 		super(picture);
 	}
-	
+
 	@Override
-	public Component getComponent(int width, int height) throws IOException{
-		Component comp = super.getComponent(width, height);
-		xa = new JavaSoundAudioGrabber();
-		xa.setFormat( new AudioFormat( 16, 96.1, 1 ) );
-		xa.setMaxBufferSize( sampleChunkSize );
-		new Thread( xa ).start();
-		source = new FrequencyAudioSource(xa);
-		source.addFrequencyListener(this,new Pair<Integer>(30,3400));
-		spectra = null;
+	public Component getComponent(final int width, final int height) throws IOException{
+		final Component comp = super.getComponent(width, height);
+		this.xa = new JavaSoundAudioGrabber(new AudioFormat( 16, 96.1, 1 ));
+		this.xa.setMaxBufferSize( this.sampleChunkSize );
+		new Thread( this.xa ).start();
+		this.source = new FrequencyAudioSource(this.xa);
+		this.source.addFrequencyListener(this,new Pair<Integer>(30,3400));
+		this.spectra = null;
 		return comp;
 	}
-	
+
 	@Override
 	public void close(){
 		super.close();
-		if(xa!=null){
-			
-			xa.stop();
-			xa = null;
+		if(this.xa!=null){
+
+			this.xa.stop();
+			this.xa = null;
 		}
 	}
 
 	@Override
-	public void consumeFrequency(float[] fftReal, float[] fftImag,int low,int high) {
-		
-		int blockWidth = 10;
-		int blockHeight = 5;
-		
-		if( spectra == null || spectra.getHeight() != (high-low) * blockHeight )
+	public void consumeFrequency(final float[] fftReal, final float[] fftImag,final int low,final int high) {
+
+		final int blockWidth = 10;
+		final int blockHeight = 5;
+
+		if( this.spectra == null || this.spectra.getHeight() != (high-low) * blockHeight )
 		{
-			spectra = new FImage( mbfImage.getWidth(), (high-low)*blockHeight);
+			this.spectra = new FImage( this.mbfImage.getWidth(), (high-low)*blockHeight);
 		}
-		
-		spectra.shiftLeftInplace(blockWidth);
+
+		this.spectra.shiftLeftInplace(blockWidth);
 		// Draw the spectra
 		for( int i = low; i < high; i++ )
 		{
-			float re = fftReal[i];
-			float im = fftImag[i];
+			final float re = fftReal[i];
+			final float im = fftImag[i];
 			float mag = (float)Math.log(Math.sqrt( re*re + im*im )+1)/5;
 			if( mag > 1 ) mag = 1;
-			spectra.drawShapeFilled(new Rectangle(spectra.getWidth()-blockWidth, spectra.getHeight()-(i * blockHeight), blockWidth,blockHeight), mag );
+			this.spectra.drawShapeFilled(new Rectangle(this.spectra.getWidth()-blockWidth, this.spectra.getHeight()-(i * blockHeight), blockWidth,blockHeight), mag );
 		}
-		
-		MBFImage toDraw = mbfImage.clone();
-		toDraw.drawImage(new MBFImage(spectra,spectra,spectra), (mbfImage.getWidth() - spectra.width)/2, mbfImage.getHeight() - spectra.height);
-		this.ic.setImage(buf = ImageUtilities.createBufferedImageForDisplay( toDraw, buf ));
+
+		final MBFImage toDraw = this.mbfImage.clone();
+		toDraw.drawImage(new MBFImage(this.spectra,this.spectra,this.spectra), (this.mbfImage.getWidth() - this.spectra.width)/2, this.mbfImage.getHeight() - this.spectra.height);
+		this.ic.setImage(this.buf = ImageUtilities.createBufferedImageForDisplay( toDraw, this.buf ));
 	}
 
-	
-	
+
+
 }
