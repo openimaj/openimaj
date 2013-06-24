235db00b32ab950ae6ee08a99a5cf95e582b03f9
David Duplaw <david.dupplaw@wds.co>
2013-06-24 09:26:36 +0000

    Updates to audio tutorial code

--- documentation/tutorial/tutorial-code/src/main/java/org/openimaj/docs/tutorial/audio/MFCCsLive.java
+++ documentation/tutorial/tutorial-code/src/main/java/org/openimaj/docs/tutorial/audio/MFCCsLive.java
@@ -4,21 +4,22 @@
 package org.openimaj.docs.tutorial.audio;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
+import org.openimaj.audio.AudioFormat;
+import org.openimaj.audio.JavaSoundAudioGrabber;
 import org.openimaj.audio.features.MFCC;
-import org.openimaj.video.xuggle.XuggleAudio;
+import org.openimaj.audio.processor.FixedSizeSampleAudioProcessor;
 import org.openimaj.vis.general.BarVisualisation;
 
 /**
- *	Example that shows the extraction of MFCC features from an audio stream
+ *	Example that shows the extraction of MFCC features from a live audio stream
  *	and displaying the results in a visualisation.
  *
  *	@author David Dupplaw (dpd@ecs.soton.ac.uk)
  *  @created 18 Jun 2013
  *	@version $Author$, $Revision$, $Date$
  */
-public class MFCCs
+public class MFCCsLive
 {
 	/**
 	 * 	Main method
@@ -29,15 +30,19 @@ public class MFCCs
 	{
 		// Open a URL to the sine wave sweep. If you have downloaded
 		// this file you should use a new File(<filename>) here.
-		final XuggleAudio xa = new XuggleAudio(
-			new URL( "http://www.audiocheck.net/download.php?" +
-					"filename=Audio/audiocheck.net_sweep20-20klin.wav" ) );
+		final JavaSoundAudioGrabber jsag = new JavaSoundAudioGrabber(
+				new AudioFormat( 16, 44.1, 1 ) );
+		new Thread( jsag ).start();
+
+		// Let's create 30ms windows with 10ms overlap
+		final FixedSizeSampleAudioProcessor fssap = new FixedSizeSampleAudioProcessor( jsag, 441*3, 441 );
 
 		// Create the Fourier transform processor chained to the audio decoder
-		final MFCC mfcc = new MFCC( xa );
+		final MFCC mfcc = new MFCC( fssap );
 
 		// Create a visualisation to show our FFT and open the window now
 		final BarVisualisation bv = new BarVisualisation( 400, 200 );
+		bv.setAxisLocation( 100 );
 		bv.showWindow( "MFCCs" );
 
 		// Loop through the sample chunks from the audio capture thread
--- documentation/tutorial/tutorial-code/src/main/java/org/openimaj/docs/tutorial/audio/Spectrogram.java
+++ documentation/tutorial/tutorial-code/src/main/java/org/openimaj/docs/tutorial/audio/Spectrogram.java
@@ -3,15 +3,15 @@
  */
 package org.openimaj.docs.tutorial.audio;
 
-import java.io.File;
-
+import org.openimaj.audio.AudioFormat;
+import org.openimaj.audio.JavaSoundAudioGrabber;
 import org.openimaj.audio.SampleChunk;
-import org.openimaj.audio.filters.HanningAudioProcessor;
-import org.openimaj.video.xuggle.XuggleAudio;
 import org.openimaj.vis.audio.AudioSpectrogram;
 
 /**
- *
+ *	This is the code for exercise 1 in the basic audio tutorial.
+ *	When you talk or sing into the computer can you see the pitches in your voice?
+ *	How does speech compare to other sounds?
  *
  *	@author David Dupplaw (dpd@ecs.soton.ac.uk)
  *  @created 19 Jun 2013
@@ -22,22 +22,20 @@ public class Spectrogram
 	/**
 	 * 	Main method
 	 *	@param args command-line args (not used)
-	 *	@throws InterruptedException probably never thrown
+	 * 	@throws InterruptedException
 	 */
 	public static void main( final String[] args ) throws InterruptedException
 	{
 		// Construct a new audio waveform visualisation
 		final AudioSpectrogram aw = new AudioSpectrogram( 440,600 );
-		aw.showWindow( "Waveform" );
+		aw.showWindow( "Spectrogram" );
 
 		// Start a sound grabber that will grab from your default microphone
-//		final JavaSoundAudioGrabber jsag = new JavaSoundAudioGrabber( new AudioFormat( 16, 44.1, 1 ) );
-//		new Thread( jsag ).start();
-		final XuggleAudio xa = new XuggleAudio( new File("/home/dd/Downloads/audiocheck.net_sweep20-20klin.wav"));
-		final HanningAudioProcessor jsag = new HanningAudioProcessor( xa, 1024 );
+		final JavaSoundAudioGrabber jsag = new JavaSoundAudioGrabber( new AudioFormat( 16, 44.1, 1 ) );
+		new Thread( jsag ).start();
 
 		// Wait until the grabber has started (sometimes it takes a while)
-//		while( jsag.isStopped() ) Thread.sleep( 50 );
+		while( jsag.isStopped() ) Thread.sleep( 50 );
 
 		// Then send each of the frames to the visualisation
 		SampleChunk sc = null;
