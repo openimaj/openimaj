f5191434857689b057098c44ed101f96cd6fa9b7
David Duplaw <david.dupplaw@wds.co>
2013-06-24 10:27:29 +0000

    Javadoc updates Also renamed an inner class which had a typo in its name!

--- content/visualisations/src/main/java/org/openimaj/vis/audio/AudioSpectrogram.java
+++ content/visualisations/src/main/java/org/openimaj/vis/audio/AudioSpectrogram.java
@@ -60,13 +60,13 @@ import org.openimaj.vis.Visualisation;
 public class AudioSpectrogram extends Visualisation<float[]>
 {
 	/**
-	 * A listener for when the spectragram has completed processing.
+	 * A listener for when the spectrogram has completed processing.
 	 *
 	 * @author David Dupplaw (dpd@ecs.soton.ac.uk)
 	 * @created 19 Jul 2012
 	 * @version $Author$, $Revision$, $Date$
 	 */
-	public static interface SpectragramCompleteListener
+	public static interface SpectrogramCompleteListener
 	{
 		/**
 		 * Called when the spectragram is complete
@@ -83,10 +83,10 @@ public class AudioSpectrogram extends Visualisation<float[]>
 	private final FourierTransform fftp = new FourierTransform();
 
 	/** Whether to draw the frequency bands, or not */
-	private final boolean drawFreqBands = true;
+	private boolean drawFreqBands = true;
 
 	/** The frequency bands to mark on the spectragram */
-	private final double[] Hz =
+	private double[] frequencyBands =
 	{ 100, 500, 1000, 5000, 10000, 20000, 40000 };
 
 	/** Is the processing complete */
@@ -96,20 +96,23 @@ public class AudioSpectrogram extends Visualisation<float[]>
 	private double binSize = 0;
 
 	/** The listeners */
-	private final List<SpectragramCompleteListener> listeners = new ArrayList<AudioSpectrogram.SpectragramCompleteListener>();
+	private final List<SpectrogramCompleteListener> listeners =
+			new ArrayList<AudioSpectrogram.SpectrogramCompleteListener>();
 
 	/** The format of the audio being processed */
 	private AudioFormat audioFormat = null;
 
 	/** Whether to draw the line at the current position of drawing */
-	private final boolean drawCurrentPositionLine = true;
+	private boolean drawCurrentPositionLine = true;
 
 	/** Colour of the line delineating the end of the current spectrogram */
-	private final Float[] currentPositionLineColour = new Float[]
-	{ 0.5f, 0.5f, 0.5f, 1f };
+	private Float[] currentPositionLineColour = new Float[]
+			{ 0.5f, 0.5f, 0.5f, 1f };
 
+	/** The total number of frames we've drawn */
 	private int nFrames = 0;
 
+	/** The current position we're drawing at */
 	private int currentDrawPosition;
 
 	/** The last spectrogram image */
@@ -142,15 +145,24 @@ public class AudioSpectrogram extends Visualisation<float[]>
 	 *
 	 * @param l The listener
 	 */
-	public void addListener( final SpectragramCompleteListener l )
+	public void addListener( final SpectrogramCompleteListener l )
 	{
 		this.listeners.add( l );
 	}
 
 	/**
+	 * 	Remove the given listener
+	 *	@param l The listener to remove
+	 */
+	public void removeListener( final SpectrogramCompleteListener l )
+	{
+		this.listeners.remove( l );
+	}
+
+	/**
 	 * Process the entire stream (or as much data will fit into the
-	 * visualisation window) and will store all the processed
-	 * {@link SampleChunk} into the data member of the visualisation.
+	 * visualisation window). The last transform to be processed will
+	 * be available in the <code>data</code> field of this class.
 	 *
 	 * @param as The stream to process
 	 */
@@ -162,10 +174,11 @@ public class AudioSpectrogram extends Visualisation<float[]>
 			@Override
 			public void run()
 			{
-				/** The hanning processor */
+				// We'll process the incoming stream with a hanning processor
 				final HanningAudioProcessor hanningProcessor = new HanningAudioProcessor(
 						as, AudioSpectrogram.this.visImage.getHeight()*8 );
 
+				// Loop through all the sample chunks and process them.
 				SampleChunk s = null;
 				AudioSpectrogram.this.currentDrawPosition = 0;
 				while( (s = hanningProcessor.nextSampleChunk()) != null
@@ -174,42 +187,65 @@ public class AudioSpectrogram extends Visualisation<float[]>
 					AudioSpectrogram.this.process( s );
 				}
 
+				// We're done.
 				AudioSpectrogram.this.isComplete = true;
 
-				for( final SpectragramCompleteListener l : AudioSpectrogram.this.listeners )
+				// So, fire the complete listener.
+				for( final SpectrogramCompleteListener l : AudioSpectrogram.this.listeners )
 					l.spectragramComplete( AudioSpectrogram.this );
 			}
 		} ).start();
 	}
 
 	/**
-	 * @param s The sample chunk
+	 * 	Processes a single sample chunk: calculates the FFT, gets the magnitudes,
+	 * 	copies the format (if it's the first chunk), and then goes on to update the image.
+	 *
+	 * 	@param s The sample chunk to process
 	 */
 	public void process( final SampleChunk s )
 	{
 		// Process the FFT
 		this.fftp.process( s.getSampleBuffer() );
+
+		// Get the magnitudes to show in the spectrogram
 		final float[] f = this.fftp.getNormalisedMagnitudes( 1f/Integer.MAX_VALUE )[0];
 
+		// Store the format of this sample chunk if we don't have one yet.
+		// This allows us to continue to draw the frequency bands on the image
+		// (if it's configured to do that).
 		if( this.audioFormat == null )
 			this.audioFormat = s.getFormat().clone();
 
-		// Store this FFT into the data
+		// Store this FFT into the data member. Note this calls a method in this class.
 		this.setData( f );
 	}
 
+	/**
+	 *	{@inheritDoc}
+	 * 	@see org.openimaj.vis.Visualisation#setData(java.lang.Object)
+	 */
 	@Override
 	public void setData( final float[] data )
 	{
+		// Set the data into the data field
 		super.setData( data );
+
+		// We count the number of frames so we can stop
+		// if we get to the edge of the window
 		this.nFrames++;
 
+		// Shift the data along (if the window's too small)
 		this.shiftData();
+
+		// Repaint the visualisation
 		this.updateVis();
 	}
 
 	/**
-	 * 	Add the given sample chunk into the spectrogram.
+	 * 	Add the given sample chunk into the spectrogram. This is a
+	 * 	handy method for processing an audio stream outside of this class.
+	 *
 	 *	@param sc The sample chunk to add.
 	 */
 	public void setData( final SampleChunk sc )
@@ -246,12 +282,24 @@ public class AudioSpectrogram extends Visualisation<float[]>
 		return this.isComplete;
 	}
 
+	/**
+	 * 	Checks whether the visualisation needs to be shifted to the left.
+	 * 	If not it draws the spectra from the left of the image. If it
+	 * 	reaches the right of the image, the image will be scrolled to the
+	 * 	left and the new spectra drawn at the right of the image.
+	 */
 	private void shiftData()
 	{
+		// Check if we should be drawing outside of the image. If so,
+		// shift the image to the left and draw at the right hand edge.
+		// (our draw position is given in currentDrawPosition and it's
+		// not updated if we enter this if clause).
 		if( this.nFrames > this.visImage.getWidth() )
 			this.previousSpecImage = this.previousSpecImage.shiftLeft();
 		else
 		{
+			// Blat the previous spectrogram and update where we're going to draw
+			// the newest spectra.
 			if( this.nFrames > 0 )
 			{
 				final FImage t = new FImage( this.nFrames, this.visImage.getHeight() );
@@ -261,6 +309,9 @@ public class AudioSpectrogram extends Visualisation<float[]>
 				this.currentDrawPosition++;
 			}
 		}
+
+		// Draw the newest spectra. Note that we draw onto the "previousSpecImage"
+		// memory image and we blat this out in the update() method.
 		synchronized( this.data )
 		{
 			// Draw spectra onto image
@@ -268,6 +319,10 @@ public class AudioSpectrogram extends Visualisation<float[]>
 		}
 	}
 
+	/**
+	 *	{@inheritDoc}
+	 * 	@see org.openimaj.vis.Visualisation#update()
+	 */
 	@Override
 	public void update()
 	{
@@ -275,19 +330,21 @@ public class AudioSpectrogram extends Visualisation<float[]>
 		{
 			synchronized( this.visImage )
 			{
+				// Draw the spectra image if we have one.
 				if( this.previousSpecImage != null )
 				{
 					this.visImage.drawImage( MBFImage.createRGB(this.previousSpecImage), 0,
 						this.visImage.getHeight()-this.previousSpecImage.getHeight() );
 				}
 
+				// Draw the frequency bands onto the image.
 				if( this.drawFreqBands && this.audioFormat != null )
 				{
 					// Work out where to plot the next spectra
 					this.binSize = (this.audioFormat.getSampleRateKHz() * 500) / this.data.length;
 
 					// Draw the frequency bands
-					for( final double freq : this.Hz )
+					for( final double freq : this.frequencyBands )
 					{
 						final Float[] fbc = new Float[] { 0.2f, 0.2f, 0.2f };
 						final Float[] fbtc = fbc;
@@ -299,10 +356,93 @@ public class AudioSpectrogram extends Visualisation<float[]>
 					}
 				}
 
+				// Draw the bar showing where we're drawing at.
 				if( this.drawCurrentPositionLine )
-					this.visImage.drawLine( this.currentDrawPosition + 1, 0, this.currentDrawPosition + 1, this.visImage.getHeight(),
+					this.visImage.drawLine( this.currentDrawPosition + 1, 0,
+							this.currentDrawPosition + 1, this.visImage.getHeight(),
 							this.currentPositionLineColour );
 			}
 		}
+	}
+
+	/**
+	 *	Returns whether the frequency bands are being drawn.
+	 *	@return TRUE if the bands are being drawn; FALSE otherwise
+	 */
+	public boolean isDrawFreqBands()
+	{
+		return this.drawFreqBands;
+	}
+
+	/**
+	 * 	Set whether to overlay the frequency bands onto the image.
+	 *	@param drawFreqBands TRUE to draw the frequency bands; FALSE otherwise
+	 */
+	public void setDrawFreqBands( final boolean drawFreqBands )
+	{
+		this.drawFreqBands = drawFreqBands;
+	}
+
+	/**
+	 * 	Get the frequency bands which are being drawn in Hz.
+	 *	@return The frequency bands which are being drawn.
+	 */
+	public double[] getFrequencyBands()
+	{
+		return this.frequencyBands;
+	}
+
+	/**
+	 * 	Set the frequency bands to overlay on the image in Hz.
+	 *	@param frequencyBands the frequency bands to draw
+	 */
+	public void setFrequencyBands( final double[] frequencyBands )
+	{
+		this.frequencyBands = frequencyBands;
+	}
+
+	/**
+	 * 	Returns whether the current position line is being drawn
+	 *	@return Whether the current position line is being drawn
+	 */
+	public boolean isDrawCurrentPositionLine()
+	{
+		return this.drawCurrentPositionLine;
+	}
+
+	/**
+	 * 	Set whether to draw the position at which the current spectra will be drawn
+	 *	@param drawCurrentPositionLine TRUE to draw the current position line
+	 */
+	public void setDrawCurrentPositionLine( final boolean drawCurrentPositionLine )
+	{
+		this.drawCurrentPositionLine = drawCurrentPositionLine;
+	}
+
+	/**
+	 * 	Get the colour of the current position line
+	 *	@return The current position line colour
+	 */
+	public Float[] getCurrentPositionLineColour()
+	{
+		return this.currentPositionLineColour;
+	}
+
+	/**
+	 * 	Set the colour of the line which is showning the current draw position.
+	 *	@param currentPositionLineColour The colour of the current draw position.
+	 */
+	public void setCurrentPositionLineColour( final Float[] currentPositionLineColour )
+	{
+		this.currentPositionLineColour = currentPositionLineColour;
+	}
+
+	/**
+	 *	Get the position at which the next spectrum will be drawn.
+	 *	@return the position at which the next spectrum will be drawn.
+	 */
+	public int getCurrentDrawPosition()
+	{
+		return this.currentDrawPosition;
 	}
 }
