77c121da8f819ef76dae439997c9b5b8109cf42b
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-07-19 13:39:42 +0000

    javadocs

--- core/core-feature/src/main/java/org/openimaj/feature/DatasetExtractors.java
+++ core/core-feature/src/main/java/org/openimaj/feature/DatasetExtractors.java
@@ -11,7 +11,32 @@ import org.openimaj.data.dataset.ListBackedDataset;
 import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.MapBackedDataset;
 
+/**
+ * Static methods to generate datasets of features from datasets of objects
+ * using a feature extractor.
+ * 
+ * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
+ */
 public class DatasetExtractors {
+	private DatasetExtractors() {
+	}
+
+	/**
+	 * Create a {@link ListDataset} of features from the given
+	 * {@link ListDataset} of objects by extracting the features from the
+	 * objects with the given feature extractor.
+	 * <p>
+	 * This function is produces a lazy {@link ListDataset} instance that
+	 * extracts features on demand. No caching is performed, so if you
+	 * <code>get</code> from the resultant list for the same element many times,
+	 * you'll invoke the feature extractor each time.
+	 * 
+	 * @param input
+	 *            the {@link ListDataset} of input objects
+	 * @param extractor
+	 *            the feature extractor
+	 * @return a {@link ListDataset} of features
+	 */
 	public static <FEATURE, OBJECT> ListDataset<FEATURE> createLazyFeatureDataset(final ListDataset<OBJECT> input,
 			final FeatureExtractor<FEATURE, OBJECT> extractor)
 	{
@@ -28,6 +53,23 @@ public class DatasetExtractors {
 		});
 	}
 
+	/**
+	 * Create a {@link GroupedDataset} of keys to {@link ListDataset} of
+	 * features from the given {@link GroupedDataset} of keys to
+	 * {@link ListDataset}s of objects by extracting the features from the
+	 * objects with the given feature extractor.
+	 * <p>
+	 * This function is produces a lazy {@link GroupedDataset} instance that
+	 * extracts features on demand. No caching is performed, so if you
+	 * <code>get</code> from the resultant list for the same element many times,
+	 * you'll invoke the feature extractor each time.
+	 * 
+	 * @param input
+	 *            the {@link GroupedDataset} of input objects
+	 * @param extractor
+	 *            the feature extractor
+	 * @return a {@link GroupedDataset} of features
+	 */
 	public static <FEATURE, OBJECT, KEY> GroupedDataset<KEY, ListDataset<FEATURE>, FEATURE> createLazyFeatureDataset(
 			final GroupedDataset<KEY, ? extends ListDataset<OBJECT>, OBJECT> input,
 			final FeatureExtractor<FEATURE, OBJECT> extractor)
