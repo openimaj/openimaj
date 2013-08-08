547ff16eda569fcf07ef0eae07c2f4e8ad17aa58
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-08-08 14:41:20 +0000

    typing

--- machine-learning/clustering/src/main/java/org/openimaj/ml/clustering/spectral/DoubleMultiviewSpectralClustering.java
+++ machine-learning/clustering/src/main/java/org/openimaj/ml/clustering/spectral/DoubleMultiviewSpectralClustering.java
@@ -1,7 +1,5 @@
 package org.openimaj.ml.clustering.spectral;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +8,6 @@ import org.openimaj.citation.annotation.ReferenceType;
 import org.openimaj.math.matrix.MatlibMatrixUtils;
 import org.openimaj.ml.clustering.IndexClusters;
 import org.openimaj.ml.clustering.MultiviewSimilarityClusterer;
-import org.openimaj.util.array.ArrayUtils;
 import org.openimaj.util.pair.IndependentPair;
 
 import ch.akuhn.matrix.DenseMatrix;
@@ -29,15 +26,14 @@ import ch.akuhn.matrix.SparseMatrix;
 		year = "2011",
 		booktitle = "Advances in Neural Information Processing Systems 24",
 		pages = { "1413", "", "1421" },
-		editor = { "J. Shawe-Taylor", "R.S. Zemel", "P. Bartlett", "F.C.N. Pereira", "K.Q. Weinberger" }
-	)
-public class DoubleMultiviewSpectralClustering implements MultiviewSimilarityClusterer<IndexClusters>{
+		editor = { "J. Shawe-Taylor", "R.S. Zemel", "P. Bartlett", "F.C.N. Pereira", "K.Q. Weinberger" })
+public class DoubleMultiviewSpectralClustering implements MultiviewSimilarityClusterer<IndexClusters> {
 
 	private MultiviewSpectralClusteringConf<double[]> conf;
 
 	/**
 	 * @param conf
-	 * cluster the eigen vectors
+	 *            cluster the eigen vectors
 	 */
 	public DoubleMultiviewSpectralClustering(MultiviewSpectralClusteringConf<double[]> conf) {
 		this.conf = conf;
@@ -45,42 +41,47 @@ public class DoubleMultiviewSpectralClustering implements MultiviewSimilarityClu
 
 	@Override
 	public IndexClusters cluster(List<SparseMatrix> data) {
-		DoubleSpectralClustering dsp = new DoubleSpectralClustering(conf);
-		
-		if(data.size() == 1){
+		final DoubleSpectralClustering dsp = new DoubleSpectralClustering(conf);
+
+		if (data.size() == 1) {
 			return dsp.cluster(data.get(0));
-		}		
-		
+		}
+
 		// Solve the spectral clustering for each view
-		ArrayList<IndependentPair<double[], double[][]>> answers = new ArrayList<>(data.size());
+		final ArrayList<IndependentPair<double[], double[][]>> answers = new ArrayList<IndependentPair<double[], double[][]>>(
+				data.size());
 		for (int i = 0; i < data.size(); i++) {
 			answers.add(dsp.spectralCluster(data.get(i)));
 		}
-		while(!conf.stop.stop(answers)){
+		while (!conf.stop.stop(answers)) {
 			for (int i = 0; i < answers.size(); i++) {
 				// L
-				SparseMatrix laplacian = dsp.laplacian(data.get(i));
+				final SparseMatrix laplacian = dsp.laplacian(data.get(i));
 				// lambda * (Sum_w!=v u_w . u_w^T)
 				SparseMatrix ujujSum = null;
 				for (int j = 0; j < answers.size(); j++) {
-					if(i == j) continue;
-					Matrix uj = new DenseMatrix(answers.get(j).secondObject());
-					SparseMatrix ujuj = MatlibMatrixUtils.dotProductTranspose(uj,uj,new SparseMatrix(uj.rowCount(),uj.rowCount()));
-					if(ujujSum == null){
+					if (i == j)
+						continue;
+					final Matrix uj = new DenseMatrix(answers.get(j).secondObject());
+					final SparseMatrix ujuj = MatlibMatrixUtils.dotProductTranspose(uj, uj,
+							new SparseMatrix(uj.rowCount(), uj.rowCount()));
+					if (ujujSum == null) {
 						ujujSum = ujuj;
 					}
-					else{
-						MatlibMatrixUtils.plusInplace(ujujSum,ujuj);
+					else {
+						MatlibMatrixUtils.plusInplace(ujujSum, ujuj);
 					}
 				}
 				// L + lambda * (Sum_w!=v u_w . u_w^T)
-				MatlibMatrixUtils.plusInplace(laplacian,MatlibMatrixUtils.scaleInplace(ujujSum,conf.lambda));
+				MatlibMatrixUtils.plusInplace(laplacian, MatlibMatrixUtils.scaleInplace(ujujSum, conf.lambda));
 				// eig
 				answers.add(i, dsp.bestCols(laplacian));
 			}
 		}
 		// Concatenate the eigen spaces and cluster using the conf clusterer
-//		return dsp.eigenspaceCluster(ArrayUtils.concatenate(answers.toArray(new double[answers.size()][][])));
+		// return
+		// dsp.eigenspaceCluster(ArrayUtils.concatenate(answers.toArray(new
+		// double[answers.size()][][])));
 		return null;
 	}
 
