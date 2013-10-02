866ae61f0488b3a927fcbd8cf79b8a465acf06a2
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-10-02 11:19:45 +0000

    Tidy up

--- core/core-feature/src/main/java/org/openimaj/feature/CachingFeatureExtractor.java
+++ core/core-feature/src/main/java/org/openimaj/feature/CachingFeatureExtractor.java
@@ -2,16 +2,15 @@ package org.openimaj.feature;
 
 import java.util.HashMap;
 
-import org.apache.log4j.Logger;
 import org.openimaj.data.identity.Identifiable;
 
 /**
- * A simple wrapper for a feature extractor that caches the extracted feature to a {@link HashMap}.
- * If a feature has already been generated for a given object, it will be
- * re-read from the {@link HashMap}
- *
+ * A simple wrapper for a feature extractor that caches the extracted feature to
+ * a {@link HashMap}. If a feature has already been generated for a given
+ * object, it will be re-read from the {@link HashMap}
+ * 
  * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
- *
+ * 
  * @param <FEATURE>
  *            Type of feature
  * @param <OBJECT>
@@ -21,17 +20,15 @@ public class CachingFeatureExtractor<FEATURE, OBJECT extends Identifiable>
 		implements
 		FeatureExtractor<FEATURE, OBJECT>
 {
-	private static Logger logger = Logger.getLogger(CachingFeatureExtractor.class);
-
 	private FeatureExtractor<FEATURE, OBJECT> extractor;
 	private boolean force;
 
 	private HashMap<String, FEATURE> cache;
 
 	/**
-	 * Construct the cache {@link HashMap}. The given extractor will be used to generate the
-	 * features.
-	 *
+	 * Construct the cache {@link HashMap}. The given extractor will be used to
+	 * generate the features.
+	 * 
 	 * @param extractor
 	 *            the feature extractor
 	 */
@@ -40,9 +37,9 @@ public class CachingFeatureExtractor<FEATURE, OBJECT extends Identifiable>
 	}
 
 	/**
-	 * Construct the cache {@link HashMap} The given extractor will be used to generate the
-	 * features. Optionally, all features can be regenerated.
-	 *
+	 * Construct the cache {@link HashMap} The given extractor will be used to
+	 * generate the features. Optionally, all features can be regenerated.
+	 * 
 	 * @param extractor
 	 *            the feature extractor
 	 * @param force
@@ -50,18 +47,17 @@ public class CachingFeatureExtractor<FEATURE, OBJECT extends Identifiable>
 	 *            rather than being loaded.
 	 */
 	public CachingFeatureExtractor(FeatureExtractor<FEATURE, OBJECT> extractor, boolean force) {
-		this.cache = new HashMap<String,FEATURE>();
+		this.cache = new HashMap<String, FEATURE>();
 		this.extractor = extractor;
 		this.force = force;
 	}
 
 	@Override
 	public FEATURE extractFeature(OBJECT object) {
-		FEATURE cachedFeature = this.cache.get(object.getID());
-
+		final FEATURE cachedFeature = this.cache.get(object.getID());
 
 		FEATURE feature = null;
-		if (!force && cachedFeature!=null) {
+		if (!force && cachedFeature != null) {
 			feature = cachedFeature;
 
 			if (feature != null)
