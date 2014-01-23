4990657c2b4b9a80f4077ddfcc096c8c0d05af71
Sina Samangooei <sinjax@gmail.com>
2014-01-23 22:39:24 +0000

    an aestheticode plate and a sandbox demo to read it added

---
+++ demos/sandbox/src/main/java/org/openimaj/image/contour/ContourAestheticode.java
@@ -0,0 +1,34 @@
+package org.openimaj.image.contour;
+
+import java.io.IOException;
+
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.contour.SuzukiContourProcessor.Border;
+import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.image.processing.threshold.OtsuThreshold;
+import org.openimaj.image.processor.KernelProcessor;
+
+/**
+ * @author Sina Samangooei (ss@ecs.soton.ac.uk)
+ *
+ */
+public class ContourAestheticode {
+	public static void main(String[] args) throws IOException {
+		String code = "/org/openimaj/image/contour/aestheticode/aestheticode.jpg";
+		MBFImage img = ImageUtilities.readMBF(ContourAestheticode.class.getResource(code));
+		
+		final OtsuThreshold thresh = new OtsuThreshold();
+		
+		ResizeProcessor resize = new ResizeProcessor(640, 480);
+		FImage threshImg = img.flatten().process(resize ).process(thresh);
+		Border root = SuzukiContourProcessor.findContours(threshImg);
+		MBFImage contourImage = MBFImage.createRGB(threshImg);
+		ContourRenderer.drawContours(contourImage, root);
+		DisplayUtilities.display(threshImg);
+		DisplayUtilities.display(contourImage);
+		DisplayUtilities.display(img.process(resize));
+	}
+}
