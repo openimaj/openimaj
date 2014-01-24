13dff60092880d904f3b420a6c42eb758e889556
Sina Samangooei <sinjax@gmail.com>
2014-01-24 17:51:42 +0000

    fixed the suzuki border follower, added a sandbox for aestheticode detection

--- demos/sandbox/src/main/java/org/openimaj/image/contour/ContourVideo.java
+++ demos/sandbox/src/main/java/org/openimaj/image/contour/ContourVideo.java
@@ -7,7 +7,11 @@ import org.openimaj.image.FImage;
 import org.openimaj.image.MBFImage;
 import org.openimaj.image.contour.ContourAestheticode.Aestheticode;
 import org.openimaj.image.contour.SuzukiContourProcessor.Border;
+import org.openimaj.image.processing.convolution.FGaussianConvolve;
+import org.openimaj.image.processing.threshold.AdaptiveLocalThresholdMean;
 import org.openimaj.image.processing.threshold.OtsuThreshold;
+import org.openimaj.image.processor.ImageProcessor;
+import org.openimaj.image.processor.KernelProcessor;
 import org.openimaj.video.VideoDisplay;
 import org.openimaj.video.VideoDisplayListener;
 import org.openimaj.video.capture.VideoCapture;
@@ -22,25 +26,27 @@ public class ContourVideo {
 	public static void main(String[] args) throws VideoCaptureException {
 		final VideoCapture cap = new VideoCapture(640, 480);
 		final VideoDisplay<MBFImage> disp = VideoDisplay.createVideoDisplay(cap);
-		final OtsuThreshold thresh = new OtsuThreshold();
+		final ImageProcessor<FImage> thresh = new OtsuThreshold();
+//		final KernelProcessor<Float,FImage> thresh = new AdaptiveLocalThresholdMean(64);
 		disp.addVideoListener(new VideoDisplayListener<MBFImage>() {
 
 			@Override
 			public void beforeUpdate(MBFImage frame) {
 				final FImage img = frame.flatten();
-//				DisplayUtilities.displayName(img, "Grey");
-				img.processInplace(thresh);
-//				DisplayUtilities.displayName(img, "Threshold");
-				final MBFImage contourFrame = new MBFImage(img.clone(), img.clone(), img.clone());
-				final MBFImage aestheticodeFrame = new MBFImage(img.clone(), img.clone(), img.clone());
-				final Border root = SuzukiContourProcessor.findContours(img);
-				ContourRenderer.drawContours(contourFrame, root);
-//				DisplayUtilities.displayName(contourFrame, "Countours");
-				List<Aestheticode> codes = new FindAestheticode().apply(root);
-				for (Aestheticode ac : codes) {
-					ContourRenderer.drawContours(aestheticodeFrame, ac.root);
-				}
-				DisplayUtilities.displayName(aestheticodeFrame, "Aestheticodes");
+				img.processInplace(new FGaussianConvolve(20));
+				DisplayUtilities.displayName(img, "Grey");
+//				img.processInplace(thresh);
+////				DisplayUtilities.displayName(img, "Threshold");
+//				final MBFImage contourFrame = new MBFImage(img.clone(), img.clone(), img.clone());
+//				final MBFImage aestheticodeFrame = new MBFImage(img.clone(), img.clone(), img.clone());
+//				final Border root = SuzukiContourProcessor.findContours(img);
+//				ContourRenderer.drawContours(contourFrame, root);
+////				DisplayUtilities.displayName(contourFrame, "Countours");
+//				List<Aestheticode> codes = new FindAestheticode().apply(root);
+//				for (Aestheticode ac : codes) {
+//					ContourRenderer.drawContours(aestheticodeFrame, ac.root);
+//				}
+//				DisplayUtilities.displayName(aestheticodeFrame, "Aestheticodes");
 			}
 
 			@Override
