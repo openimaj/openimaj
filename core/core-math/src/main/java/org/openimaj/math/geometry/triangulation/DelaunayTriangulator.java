1bc383168ea7cd87d16da0d4a66307c8b57975fe
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-02-05 13:17:00 +0000

    Adding voronoi computation

--- core/core-math/src/main/java/org/openimaj/math/geometry/triangulation/DelaunayTriangulator.java
+++ core/core-math/src/main/java/org/openimaj/math/geometry/triangulation/DelaunayTriangulator.java
@@ -42,31 +42,26 @@ import org.openimaj.math.geometry.shape.Circle;
 import org.openimaj.math.geometry.shape.Triangle;
 
 /**
- *	ported from p bourke's triangulate.c
- *	http://astronomy.swin.edu.au/~pbourke/modelling/triangulate/
- *
- *	fjenett, 20th february 2005, offenbach-germany.
- *	contact: http://www.florianjenett.de/
- *
- * 	Modified to be more java-like and work with openimaj geometry classes 
+ * The Delaunay Triangulation algorithm. Produces a triangulation of a set of
+ * points.
+ * <p>
+ * Originally ported from <a
+ * href="http://paulbourke.net/papers/triangulate/"Paul Bourke's
+ * triangulate.c</a>.
+ * <p>
+ * This OpenIMAJ version is based off <a
+ * href="http://www.florianjenett.de/">Florian Jenett's</a> Java port: fjenett,
+ * 20th february 2005, offenbach-germany. contact: http://www.florianjenett.de/
  */
 public class DelaunayTriangulator {
 	/*
-    From P Bourke's C prototype - 
-
-    qsort(p,nv,sizeof(XYZ),XYZCompare);
-
-    int XYZCompare(void *v1,void *v2) {
-      XYZ *p1,*p2;
-      p1 = v1;
-      p2 = v2;
-      if (p1->x < p2->x)
-        return(-1);
-      else if (p1->x > p2->x)
-        return(1);
-      else
-        return(0);
-    }
+	 * From P Bourke's C prototype -
+	 * 
+	 * qsort(p,nv,sizeof(XYZ),XYZCompare);
+	 * 
+	 * int XYZCompare(void *v1,void *v2) { XYZ *p1,*p2; p1 = v1; p2 = v2; if
+	 * (p1->x < p2->x) return(-1); else if (p1->x > p2->x) return(1); else
+	 * return(0); }
 	 */
 	private static class XComparator implements Comparator<Point2d> {
 
@@ -87,131 +82,140 @@ public class DelaunayTriangulator {
 	static final float EPSILON = 0.00000001f;
 
 	/*
-    Return TRUE if a point (xp,yp) is inside the circumcircle made up
-    of the points (x1,y1), (x2,y2), (x3,y3)
-    The circumcircle centre is returned in (xc,yc) and the radius r
-    NOTE: A point on the edge is inside the circumcircle
+	 * Return TRUE if a point (xp,yp) is inside the circumcircle made up of the
+	 * points (x1,y1), (x2,y2), (x3,y3) The circumcircle centre is returned in
+	 * (xc,yc) and the radius r NOTE: A point on the edge is inside the
+	 * circumcircle
 	 */
 	private static boolean circumCircle(Point2d p, Triangle t, Circle circle) {
-		float m1,m2,mx1,mx2,my1,my2;
-		float dx,dy,rsqr,drsqr;
+		float m1, m2, mx1, mx2, my1, my2;
+		float dx, dy, rsqr, drsqr;
 
 		/* Check for coincident points */
-		if ( Math.abs(t.firstVertex().getY()-t.secondVertex().getY()) < EPSILON && Math.abs(t.secondVertex().getY()-t.thirdVertex().getY()) < EPSILON ) {
+		if (Math.abs(t.firstVertex().getY() - t.secondVertex().getY()) < EPSILON
+				&& Math.abs(t.secondVertex().getY() - t.thirdVertex().getY()) < EPSILON)
+		{
 			System.err.println("CircumCircle: Points are coincident.");
 			return false;
 		}
 
-		if ( Math.abs(t.secondVertex().getY()-t.firstVertex().getY()) < EPSILON ) {
-			m2 = - (t.thirdVertex().getX()-t.secondVertex().getX()) / (t.thirdVertex().getY()-t.secondVertex().getY());
+		if (Math.abs(t.secondVertex().getY() - t.firstVertex().getY()) < EPSILON) {
+			m2 = -(t.thirdVertex().getX() - t.secondVertex().getX()) / (t.thirdVertex().getY() - t.secondVertex().getY());
 			mx2 = (t.secondVertex().getX() + t.thirdVertex().getX()) / 2.0f;
 			my2 = (t.secondVertex().getY() + t.thirdVertex().getY()) / 2.0f;
-			circle.setX( (t.secondVertex().getX() + t.firstVertex().getX()) / 2.0f );
-			circle.setY( m2 * (circle.getX() - mx2) + my2 );
+			circle.setX((t.secondVertex().getX() + t.firstVertex().getX()) / 2.0f);
+			circle.setY(m2 * (circle.getX() - mx2) + my2);
 		}
-		else if ( Math.abs(t.thirdVertex().getY()-t.secondVertex().getY()) < EPSILON ) {
-			m1 = - (t.secondVertex().getX()-t.firstVertex().getX()) / (t.secondVertex().getY()-t.firstVertex().getY());
+		else if (Math.abs(t.thirdVertex().getY() - t.secondVertex().getY()) < EPSILON) {
+			m1 = -(t.secondVertex().getX() - t.firstVertex().getX()) / (t.secondVertex().getY() - t.firstVertex().getY());
 			mx1 = (t.firstVertex().getX() + t.secondVertex().getX()) / 2.0f;
 			my1 = (t.firstVertex().getY() + t.secondVertex().getY()) / 2.0f;
-			circle.setX( (t.thirdVertex().getX() + t.secondVertex().getX()) / 2.0f );
-			circle.setY( m1 * (circle.getX() - mx1) + my1 );	
+			circle.setX((t.thirdVertex().getX() + t.secondVertex().getX()) / 2.0f);
+			circle.setY(m1 * (circle.getX() - mx1) + my1);
 		}
 		else {
-			m1 = - (t.secondVertex().getX()-t.firstVertex().getX()) / (t.secondVertex().getY()-t.firstVertex().getY());
-			m2 = - (t.thirdVertex().getX()-t.secondVertex().getX()) / (t.thirdVertex().getY()-t.secondVertex().getY());
+			m1 = -(t.secondVertex().getX() - t.firstVertex().getX()) / (t.secondVertex().getY() - t.firstVertex().getY());
+			m2 = -(t.thirdVertex().getX() - t.secondVertex().getX()) / (t.thirdVertex().getY() - t.secondVertex().getY());
 			mx1 = (t.firstVertex().getX() + t.secondVertex().getX()) / 2.0f;
 			mx2 = (t.secondVertex().getX() + t.thirdVertex().getX()) / 2.0f;
 			my1 = (t.firstVertex().getY() + t.secondVertex().getY()) / 2.0f;
 			my2 = (t.secondVertex().getY() + t.thirdVertex().getY()) / 2.0f;
-			circle.setX( (m1 * mx1 - m2 * mx2 + my2 - my1) / (m1 - m2) );
-			circle.setY( m1 * (circle.getX() - mx1) + my1 );
+			circle.setX((m1 * mx1 - m2 * mx2 + my2 - my1) / (m1 - m2));
+			circle.setY(m1 * (circle.getX() - mx1) + my1);
 		}
 
 		dx = t.secondVertex().getX() - circle.getX();
 		dy = t.secondVertex().getY() - circle.getY();
-		rsqr = dx*dx + dy*dy;
-		circle.setRadius( (float) Math.sqrt(rsqr) );
+		rsqr = dx * dx + dy * dy;
+		circle.setRadius((float) Math.sqrt(rsqr));
 
 		dx = p.getX() - circle.getX();
 		dy = p.getY() - circle.getY();
-		drsqr = dx*dx + dy*dy;
+		drsqr = dx * dx + dy * dy;
 
 		return drsqr <= rsqr;
 	}
 
 	/**
-	 * Trianglate a set of vertices. 
+	 * Trianglate a set of vertices.
 	 * 
-	 * @param pxyz vertices to triangulate.
+	 * @param pxyz
+	 *            vertices to triangulate.
 	 * @return list of triangles arranged in a consistent clockwise order.
 	 */
-	public static List<Triangle> triangulate( List<? extends Point2d> pxyz ) {
+	public static List<Triangle> triangulate(List<? extends Point2d> pxyz) {
 
 		// sort vertex array in increasing x values
 		Collections.sort(pxyz, new XComparator());
 
 		/*
-      Find the maximum and minimum vertex bounds.
-      This is to allow calculation of the bounding triangle
+		 * Find the maximum and minimum vertex bounds. This is to allow
+		 * calculation of the bounding triangle
 		 */
-		float xmin = ((Point2d)pxyz.get(0)).getX();
-		float ymin = ((Point2d)pxyz.get(0)).getY();
+		float xmin = ((Point2d) pxyz.get(0)).getX();
+		float ymin = ((Point2d) pxyz.get(0)).getY();
 		float xmax = xmin;
 		float ymax = ymin;
 
-		for (Point2d p : pxyz) {
-			if (p.getX() < xmin) xmin = p.getX();
-			if (p.getX() > xmax) xmax = p.getX();
-			if (p.getY() < ymin) ymin = p.getY();
-			if (p.getY() > ymax) ymax = p.getY();
+		for (final Point2d p : pxyz) {
+			if (p.getX() < xmin)
+				xmin = p.getX();
+			if (p.getX() > xmax)
+				xmax = p.getX();
+			if (p.getY() < ymin)
+				ymin = p.getY();
+			if (p.getY() > ymax)
+				ymax = p.getY();
 		}
 
-		float dx = xmax - xmin;
-		float dy = ymax - ymin;
-		float dmax = (dx > dy) ? dx : dy;
-		float xmid = (xmax + xmin) / 2.0f;
-		float ymid = (ymax + ymin) / 2.0f;
+		final float dx = xmax - xmin;
+		final float dy = ymax - ymin;
+		final float dmax = (dx > dy) ? dx : dy;
+		final float xmid = (xmax + xmin) / 2.0f;
+		final float ymid = (ymax + ymin) / 2.0f;
 
-		ArrayList<Triangle> triangles = new ArrayList<Triangle>(); // for the Triangles
-		HashSet<Triangle> complete = new HashSet<Triangle>(); // for complete Triangles
+		final ArrayList<Triangle> triangles = new ArrayList<Triangle>(); // for
+																			// the
+																			// Triangles
+		final HashSet<Triangle> complete = new HashSet<Triangle>(); // for
+																	// complete
+																	// Triangles
 
 		/*
-      Set up the supertriangle
-      This is a triangle which encompasses all the sample points.
-      The supertriangle coordinates are added to the end of the
-      vertex list. The supertriangle is the first triangle in
-      the triangle list.
+		 * Set up the supertriangle This is a triangle which encompasses all the
+		 * sample points. The supertriangle coordinates are added to the end of
+		 * the vertex list. The supertriangle is the first triangle in the
+		 * triangle list.
 		 */
-		Triangle superTriangle = new Triangle(
-				new Point2dImpl( xmid - 2.0f * dmax, ymid - dmax ),
-				new Point2dImpl( xmid, ymid + 2.0f * dmax ),
-				new Point2dImpl( xmid + 2.0f * dmax, ymid - dmax )
-		);
+		final Triangle superTriangle = new Triangle(
+				new Point2dImpl(xmid - 2.0f * dmax, ymid - dmax),
+				new Point2dImpl(xmid, ymid + 2.0f * dmax),
+				new Point2dImpl(xmid + 2.0f * dmax, ymid - dmax)
+				);
 		triangles.add(superTriangle);
 
 		/*
-      Include each point one at a time into the existing mesh
+		 * Include each point one at a time into the existing mesh
 		 */
-		ArrayList<Line2d> edges = new ArrayList<Line2d>();
-		for (Point2d p : pxyz) {
+		final ArrayList<Line2d> edges = new ArrayList<Line2d>();
+		for (final Point2d p : pxyz) {
 			edges.clear();
 
 			/*
-        Set up the edge buffer.
-        If the point (xp,yp) lies inside the circumcircle then the
-        three edges of that triangle are added to the edge buffer
-        and that triangle is removed.
+			 * Set up the edge buffer. If the point (xp,yp) lies inside the
+			 * circumcircle then the three edges of that triangle are added to
+			 * the edge buffer and that triangle is removed.
 			 */
-			Circle circle = new Circle(0,0,0);
+			final Circle circle = new Circle(0, 0, 0);
 
-			for (int j = triangles.size()-1; j >= 0; j--) {
+			for (int j = triangles.size() - 1; j >= 0; j--) {
 
-				Triangle t = (Triangle)triangles.get(j);
+				final Triangle t = triangles.get(j);
 				if (complete.contains(t)) {
 					continue;
 				}
 
-				boolean inside = circumCircle( p, t, circle );
+				final boolean inside = circumCircle(p, t, circle);
 
 				if (circle.getX() + circle.getRadius() < p.getX()) {
 					complete.add(t);
@@ -226,14 +230,14 @@ public class DelaunayTriangulator {
 			}
 
 			/*
-        Tag multiple edges
-        Note: if all triangles are specified anticlockwise then all
-        interior edges are opposite pointing in direction.
+			 * Tag multiple edges Note: if all triangles are specified
+			 * anticlockwise then all interior edges are opposite pointing in
+			 * direction.
 			 */
-			for (int j=0; j<edges.size()-1; j++) {
-				Line2d e1 = edges.get(j);
-				for (int k=j+1; k<edges.size(); k++) {
-					Line2d e2 = edges.get(k);
+			for (int j = 0; j < edges.size() - 1; j++) {
+				final Line2d e1 = edges.get(j);
+				for (int k = j + 1; k < edges.size(); k++) {
+					final Line2d e2 = edges.get(k);
 					if (e1.begin == e2.end && e1.end == e2.begin) {
 						e1.begin = null;
 						e1.end = null;
@@ -251,12 +255,11 @@ public class DelaunayTriangulator {
 			}
 
 			/*
-        Form new triangles for the current point
-        Skipping over any tagged edges.
-        All edges are arranged in clockwise order.
+			 * Form new triangles for the current point Skipping over any tagged
+			 * edges. All edges are arranged in clockwise order.
 			 */
-			for (int j=0; j < edges.size(); j++) {
-				Line2d e = edges.get(j);
+			for (int j = 0; j < edges.size(); j++) {
+				final Line2d e = edges.get(j);
 				if (e.begin == null || e.end == null) {
 					continue;
 				}
@@ -266,10 +269,10 @@ public class DelaunayTriangulator {
 		}
 
 		/*
-      Remove triangles with supertriangle vertices
+		 * Remove triangles with supertriangle vertices
 		 */
-		for (int i = triangles.size()-1; i >= 0; i--) {
-			Triangle t = (Triangle)triangles.get(i);
+		for (int i = triangles.size() - 1; i >= 0; i--) {
+			final Triangle t = triangles.get(i);
 			if (t.sharesVertex(superTriangle)) {
 				triangles.remove(i);
 			}
---
+++ core/core-math/src/main/java/org/openimaj/math/geometry/triangulation/Voronoi.java
@@ -0,0 +1,492 @@
+package org.openimaj.math.geometry.triangulation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+import org.openimaj.math.geometry.line.Line2d;
+import org.openimaj.math.geometry.point.Point2d;
+import org.openimaj.math.geometry.point.Point2dImpl;
+
+/**
+ * Static methods for the computation of a Voronoi diagram (aka Dirichlet
+ * tessellation) from a set of points. Internally, these use Fortune's algorithm
+ * to do the work.
+ * 
+ * @see "http://en.wikipedia.org/wiki/Fortune%27s_algorithm"
+ * 
+ * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
+ * 
+ */
+public class Voronoi {
+	private Voronoi() {
+	}
+
+	/**
+	 * Compute the Voronoi diagram as a graph of its vertices
+	 * 
+	 * @param points
+	 *            the vertices
+	 * @return the graph
+	 */
+	public static Graph<Point2d, DefaultEdge> computeVoronoiGraph(List<? extends Point2d> points) {
+		final double[] wh = computeWidthHeight(points);
+		return computeVoronoiGraph(points, wh[0], wh[1]);
+	}
+
+	/**
+	 * Compute the Voronoi diagram as a graph of its vertices
+	 * 
+	 * @param points
+	 *            the vertices
+	 * @param width
+	 *            the width of the diagram
+	 * @param height
+	 *            the height of the diagram
+	 * @return the graph
+	 */
+	public static Graph<Point2d, DefaultEdge> computeVoronoiGraph(List<? extends Point2d> points, double width,
+			double height)
+	{
+		final FortunesAlgorithm f = new FortunesAlgorithm(width, height);
+		final List<Line2d> edges = f.runFortune(points);
+
+		final Graph<Point2d, DefaultEdge> graph = new SimpleGraph<Point2d, DefaultEdge>(DefaultEdge.class);
+		for (final Line2d l : edges) {
+			graph.addEdge(l.begin, l.end);
+		}
+
+		return graph;
+	}
+
+	/**
+	 * Compute the Voronoi diagram as a list of its edges
+	 * 
+	 * @param points
+	 *            the vertices
+	 * @return the graph
+	 */
+	public static List<Line2d> computeVoronoiEdges(List<? extends Point2d> points) {
+		final double[] wh = computeWidthHeight(points);
+
+		return computeVoronoiEdges(points, wh[0], wh[1]);
+	}
+
+	/**
+	 * Compute the Voronoi diagram as a list of its edges
+	 * 
+	 * @param points
+	 *            the vertices
+	 * @param width
+	 *            the width of the diagram
+	 * @param height
+	 *            the height of the diagram
+	 * @return the graph
+	 */
+	public static List<Line2d> computeVoronoiEdges(List<? extends Point2d> points, double width,
+			double height)
+	{
+		final FortunesAlgorithm f = new FortunesAlgorithm(width, height);
+		return f.runFortune(points);
+	}
+
+	private static double[] computeWidthHeight(List<? extends Point2d> points) {
+		float maxx = -Float.MAX_VALUE;
+		float minx = Float.MAX_VALUE;
+		float maxy = -Float.MAX_VALUE;
+		float miny = Float.MAX_VALUE;
+
+		for (final Point2d pt : points) {
+			final float x = pt.getX();
+			final float y = pt.getY();
+
+			if (maxx < x)
+				maxx = x;
+			if (minx > x)
+				minx = x;
+			if (maxy < y)
+				maxy = y;
+			if (miny > y)
+				miny = y;
+		}
+
+		final float w = maxx - minx;
+		final float h = maxy - miny;
+		return new double[] { maxx + 0.1 * w, maxy + 0.1 * h };
+	}
+
+	/**
+	 * Implementation of fortune's algorithm.
+	 * 
+	 * @see "http://stackoverflow.com/questions/2346148/fastest-way-to-get-the-set-of-convex-polygons-formed-by-voronoi-line-segments"
+	 */
+	private static class FortunesAlgorithm {
+		// A list of line segments that defines where the cells are divided
+		private List<Line2d> output = new ArrayList<Line2d>();
+		// The sites that have not yet been processed, in acending order of X
+		// coordinate
+		private PriorityQueue<Point2d> sites = new PriorityQueue<Point2d>();
+		// Possible upcoming cirlce events in acending order of X coordinate
+		private PriorityQueue<CircleEvent> events = new PriorityQueue<CircleEvent>();
+		// The root of the binary search tree of the parabolic wave front
+		private Arc root;
+		private double height;
+		private double width;
+
+		FortunesAlgorithm(double width, double height) {
+			this.width = width;
+			this.height = height;
+		}
+
+		List<Line2d> runFortune(List<? extends Point2d> points) {
+			for (final Point2d pt : points)
+				sites.add(new ComparablePoint(pt));
+
+			// Process the queues; select the top element with smaller x
+			// coordinate.
+			while (sites.size() > 0) {
+				if ((events.size() > 0) && ((events.peek().xpos) <= (((ComparablePoint) sites.peek()).x))) {
+					processCircleEvent(events.poll());
+				} else {
+					// process a site event by adding a curve to the parabolic
+					// front
+					frontInsert((ComparablePoint) sites.poll());
+				}
+			}
+
+			// After all points are processed, do the remaining circle events.
+			while (events.size() > 0) {
+				processCircleEvent(events.poll());
+			}
+
+			// Clean up dangling edges.
+			finishEdges();
+
+			return output;
+		}
+
+		private void processCircleEvent(CircleEvent event) {
+			if (event.valid) {
+				// start a new edge
+				final Edge edgy = new Edge(event.p);
+
+				// Remove the associated arc from the front.
+				final Arc parc = event.a;
+				if (parc.prev != null) {
+					parc.prev.next = parc.next;
+					parc.prev.edge1 = edgy;
+				}
+				if (parc.next != null) {
+					parc.next.prev = parc.prev;
+					parc.next.edge0 = edgy;
+				}
+
+				// Finish the edges before and after this arc.
+				if (parc.edge0 != null) {
+					parc.edge0.finish(event.p);
+				}
+				if (parc.edge1 != null) {
+					parc.edge1.finish(event.p);
+				}
+
+				// Recheck circle events on either side of p:
+				if (parc.prev != null) {
+					checkCircleEvent(parc.prev, event.xpos);
+				}
+				if (parc.next != null) {
+					checkCircleEvent(parc.next, event.xpos);
+				}
+
+			}
+		}
+
+		void frontInsert(ComparablePoint focus) {
+			if (root == null) {
+				root = new Arc(focus);
+				return;
+			}
+
+			Arc parc = root;
+			while (parc != null) {
+				final CircleResultPack rez = intersect(focus, parc);
+				if (rez.valid) {
+					// New parabola intersects parc. If necessary, duplicate
+					// parc.
+
+					if (parc.next != null) {
+						final CircleResultPack rezz = intersect(focus, parc.next);
+						if (!rezz.valid) {
+							final Arc bla = new Arc(parc.focus);
+							bla.prev = parc;
+							bla.next = parc.next;
+							parc.next.prev = bla;
+							parc.next = bla;
+						}
+					} else {
+						parc.next = new Arc(parc.focus);
+						parc.next.prev = parc;
+					}
+					parc.next.edge1 = parc.edge1;
+
+					// Add new arc between parc and parc.next.
+					final Arc bla = new Arc(focus);
+					bla.prev = parc;
+					bla.next = parc.next;
+					parc.next.prev = bla;
+					parc.next = bla;
+
+					parc = parc.next; // Now parc points to the new arc.
+
+					// Add new half-edges connected to parc's endpoints.
+					parc.edge0 = new Edge(rez.center);
+					parc.prev.edge1 = parc.edge0;
+					parc.edge1 = new Edge(rez.center);
+					parc.next.edge0 = parc.edge1;
+
+					// Check for new circle events around the new arc:
+					checkCircleEvent(parc, focus.x);
+					checkCircleEvent(parc.prev, focus.x);
+					checkCircleEvent(parc.next, focus.x);
+
+					return;
+				}
+
+				// proceed to next arc
+				parc = parc.next;
+			}
+
+			// Special case: If p never intersects an arc, append it to the
+			// list.
+			parc = root;
+			while (parc.next != null) {
+				parc = parc.next; // Find the last node.
+			}
+			parc.next = new Arc(focus);
+			parc.next.prev = parc;
+			final ComparablePoint start = new ComparablePoint(0, (parc.next.focus.y + parc.focus.y) / 2);
+			parc.next.edge0 = new Edge(start);
+			parc.edge1 = parc.next.edge0;
+
+		}
+
+		void checkCircleEvent(Arc parc, double xpos) {
+			// Invalidate any old event.
+			if ((parc.event != null) && (parc.event.xpos != xpos)) {
+				parc.event.valid = false;
+			}
+			parc.event = null;
+
+			if ((parc.prev == null) || (parc.next == null)) {
+				return;
+			}
+
+			final CircleResultPack result = circle(parc.prev.focus, parc.focus, parc.next.focus);
+			if (result.valid && result.rightmostX > xpos) {
+				// Create new event.
+				parc.event = new CircleEvent(result.rightmostX, result.center, parc);
+				events.offer(parc.event);
+			}
+
+		}
+
+		// Find the rightmost point on the circle through a,b,c.
+		CircleResultPack circle(ComparablePoint a, ComparablePoint b, ComparablePoint c) {
+			final CircleResultPack result = new CircleResultPack();
+
+			// Check that bc is a "right turn" from ab.
+			if ((b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y) > 0) {
+				result.valid = false;
+				return result;
+			}
+
+			// Algorithm from O'Rourke 2ed p. 189.
+			final double A = b.x - a.x;
+			final double B = b.y - a.y;
+			final double C = c.x - a.x;
+			final double D = c.y - a.y;
+			final double E = A * (a.x + b.x) + B * (a.y + b.y);
+			final double F = C * (a.x + c.x) + D * (a.y + c.y);
+			final double G = 2 * (A * (c.y - b.y) - B * (c.x - b.x));
+
+			if (G == 0) { // Points are co-linear.
+				result.valid = false;
+				return result;
+			}
+
+			// centerpoint of the circle.
+			final ComparablePoint o = new ComparablePoint((D * E - B * F) / G, (A * F - C * E) / G);
+			result.center = o;
+
+			// o.x plus radius equals max x coordinate.
+			result.rightmostX = o.x + Math.sqrt(Math.pow(a.x - o.x, 2.0) + Math.pow(a.y - o.y, 2.0));
+
+			result.valid = true;
+			return result;
+		}
+
+		// Will a new parabola at point p intersect with arc i?
+		CircleResultPack intersect(ComparablePoint p, Arc i) {
+			final CircleResultPack res = new CircleResultPack();
+			res.valid = false;
+			if (i.focus.x == p.x) {
+				return res;
+			}
+
+			double a = 0.0;
+			double b = 0.0;
+			if (i.prev != null) // Get the intersection of i->prev, i.
+			{
+				a = intersection(i.prev.focus, i.focus, p.x).y;
+			}
+			if (i.next != null) // Get the intersection of i->next, i.
+			{
+				b = intersection(i.focus, i.next.focus, p.x).y;
+			}
+
+			if ((i.prev == null || a <= p.y) && (i.next == null || p.y <= b)) {
+				res.center = new ComparablePoint(0, p.y);
+
+				// Plug it back into the parabola equation to get the x
+				// coordinate
+				res.center.x = (i.focus.x * i.focus.x + (i.focus.y - res.center.y) * (i.focus.y - res.center.y) - p.x
+						* p.x)
+						/ (2 * i.focus.x - 2 * p.x);
+
+				res.valid = true;
+				return res;
+			}
+			return res;
+		}
+
+		// Where do two parabolas intersect?
+		ComparablePoint intersection(ComparablePoint p0, ComparablePoint p1, double l) {
+			final ComparablePoint res = new ComparablePoint(0, 0);
+			ComparablePoint p = p0;
+
+			if (p0.x == p1.x) {
+				res.y = (p0.y + p1.y) / 2;
+			} else if (p1.x == l) {
+				res.y = p1.y;
+			} else if (p0.x == l) {
+				res.y = p0.y;
+				p = p1;
+			} else {
+				// Use the quadratic formula.
+				final double z0 = 2 * (p0.x - l);
+				final double z1 = 2 * (p1.x - l);
+
+				final double a = 1 / z0 - 1 / z1;
+				final double b = -2 * (p0.y / z0 - p1.y / z1);
+				final double c = (p0.y * p0.y + p0.x * p0.x - l * l) / z0 - (p1.y * p1.y + p1.x * p1.x - l * l) / z1;
+
+				res.y = (float) ((-b - Math.sqrt((b * b - 4 * a * c))) / (2 * a));
+			}
+			// Plug back into one of the parabola equations.
+			res.x = (float) ((p.x * p.x + (p.y - res.y) * (p.y - res.y) - l * l) / (2 * p.x - 2 * l));
+			return res;
+		}
+
+		void finishEdges() {
+			// Advance the sweep line so no parabolas can cross the bounding
+			// box.
+			final double l = width * 2 + height;
+
+			// Extend each remaining segment to the new parabola intersections.
+			Arc i = root;
+			while (i != null) {
+				if (i.edge1 != null) {
+					i.edge1.finish(intersection(i.focus, i.next.focus, l * 2));
+				}
+				i = i.next;
+			}
+		}
+
+		class ComparablePoint extends Point2dImpl implements Comparable<ComparablePoint> {
+			public ComparablePoint(double X, double Y) {
+				x = (float) X;
+				y = (float) Y;
+			}
+
+			public ComparablePoint(Point2d pt) {
+				this.x = pt.getX();
+				this.y = pt.getY();
+			}
+
+			@Override
+			public int compareTo(ComparablePoint foo) {
+				return ((Float) this.x).compareTo(foo.x);
+			}
+		}
+
+		private static class CircleEvent implements Comparable<CircleEvent> {
+			double xpos;
+			ComparablePoint p;
+			Arc a;
+			boolean valid;
+
+			CircleEvent(double X, ComparablePoint P, Arc A) {
+				xpos = X;
+				a = A;
+				p = P;
+				valid = true;
+			}
+
+			@Override
+			public int compareTo(CircleEvent foo) {
+				return ((Double) this.xpos).compareTo(foo.xpos);
+			}
+		}
+
+		private class Edge extends Line2d {
+			boolean done;
+
+			Edge(ComparablePoint p) {
+				begin = p;
+				end = new ComparablePoint(0, 0);
+				done = false;
+				output.add(this);
+			}
+
+			void finish(ComparablePoint p) {
+				if (done) {
+					return;
+				}
+				end = p;
+				done = true;
+			}
+		}
+
+		/**
+		 * Parabolic arc: the set of points equidistant from a focus point and
+		 * the beach line
+		 */
+		private static class Arc {
+			ComparablePoint focus;
+
+			// Arcs exist in a linked list
+			Arc next, prev;
+
+			CircleEvent event;
+
+			Edge edge0, edge1;
+
+			Arc(ComparablePoint p) {
+				focus = p;
+				next = null;
+				prev = null;
+				event = null;
+				edge0 = null;
+				edge1 = null;
+			}
+		}
+
+		private static class CircleResultPack {
+			boolean valid;
+			ComparablePoint center;
+			double rightmostX;
+		}
+	}
+}
