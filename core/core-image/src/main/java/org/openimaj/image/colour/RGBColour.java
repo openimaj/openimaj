b7920da99a6665dffb63c6aef0e981a78fa7c29b
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-02-05 16:50:13 +0000

    adding ability to generate colors from a colourmap

--- core/core-image/src/main/java/org/openimaj/image/colour/RGBColour.java
+++ core/core-image/src/main/java/org/openimaj/image/colour/RGBColour.java
@@ -35,119 +35,143 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Convenience constants and methods for RGB colours for 
- * use in MBFImages
+ * Convenience constants and methods for RGB colours for use in MBFImages
  * 
  * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
- *
+ * 
  */
 public class RGBColour {
 	/** White colour as RGB */
-	public final static Float[] WHITE 		= {1f, 1f, 1f};
+	public final static Float[] WHITE = { 1f, 1f, 1f };
 
 	/** Light gray colour as RGB */
-	public final static Float[] LIGHT_GRAY 	= {0.75f, 0.75f, 0.75f};
+	public final static Float[] LIGHT_GRAY = { 0.75f, 0.75f, 0.75f };
 
 	/** Gray colour as RGB */
-	public final static Float[] GRAY		= {0.5f, 0.5f, 0.5f};
+	public final static Float[] GRAY = { 0.5f, 0.5f, 0.5f };
 
 	/** Dark gray colour as RGB */
-	public final static Float[] DARK_GRAY  	= {0.25f, 0.25f, 0.25f};
+	public final static Float[] DARK_GRAY = { 0.25f, 0.25f, 0.25f };
 
 	/** Black colour as RGB */
-	public final static Float[] BLACK	 	= {0f, 0f, 0f};
+	public final static Float[] BLACK = { 0f, 0f, 0f };
 
 	/** Red colour as RGB */
-	public final static Float[] RED			= {1f, 0f, 0f};
+	public final static Float[] RED = { 1f, 0f, 0f };
 
 	/** Pink colour as RGB */
-	public final static Float[] PINK		= {1f, 175f/256f, 175f/256f};
+	public final static Float[] PINK = { 1f, 175f / 256f, 175f / 256f };
 
 	/** Orange colour as RGB */
-	public final static Float[] ORANGE 		= {1f, 200f/256f, 0f};
+	public final static Float[] ORANGE = { 1f, 200f / 256f, 0f };
 
 	/** Yellow colour as RGB */
-	public final static Float[] YELLOW	 	= {1f, 1f, 0f};
+	public final static Float[] YELLOW = { 1f, 1f, 0f };
 
 	/** Green colour as RGB */
-	public final static Float[] GREEN	 	= {0f, 1f, 0f};
+	public final static Float[] GREEN = { 0f, 1f, 0f };
 
 	/** Magenta colour as RGB */
-	public final static Float[] MAGENTA		= {1f, 0f, 1f};
+	public final static Float[] MAGENTA = { 1f, 0f, 1f };
 
 	/** Cyan colour as RGB */
-	public final static Float[] CYAN 		= {0f, 1f, 1f};    
+	public final static Float[] CYAN = { 0f, 1f, 1f };
 
 	/** Blue colour as RGB */
-	public final static Float[] BLUE	 	= {0f, 0f, 1f};
+	public final static Float[] BLUE = { 0f, 0f, 1f };
 
-	private RGBColour() {}
-	
-	/**
-	 * Make an OpenImaj colour from a java.awt.Color.
-	 * @param c the color to convert
-	 * @return a colour for using as RGB MBFImages
-	 */
-	public static Float[] fromColor(Color c) {
-		Float r = c.getRed() / 255f;
-		Float g = c.getRed() / 255f;
-		Float b = c.getRed() / 255f;
-
-		return new Float[] {r,g,b};
+	private RGBColour() {
 	}
 
 	/**
-	 * Get a colour from a string representation. The string may
-	 * contain any of the predefined colour names described in this
-	 * class (in upper, lower or mixed case), or it can be a comma
-	 * separated triplet of floating point values.
+	 * Make an OpenImaj colour from a java.awt.Color.
 	 * 
-	 * @param s the string
+	 * @param c
+	 *            the color to convert
+	 * @return a colour for using as RGB MBFImages
+	 */
+	public static Float[] fromColor(Color c) {
+		final Float r = c.getRed() / 255f;
+		final Float g = c.getRed() / 255f;
+		final Float b = c.getRed() / 255f;
+
+		return new Float[] { r, g, b };
+	}
+
+	/**
+	 * Get a colour from a string representation. The string may contain any of
+	 * the predefined colour names described in this class (in upper, lower or
+	 * mixed case), or it can be a comma separated triplet of floating point
+	 * values.
+	 * 
+	 * @param s
+	 *            the string
 	 * @return the colour described by the string, or BLACK if a problem occurs.
 	 */
 	public static Float[] fromString(String s) {
 		try {
 			if (s.contains(",")) {
-				String [] parts = s.split(",");
-				Float [] col = new Float[3];
-				for (int i=0; i<3; i++) {
+				final String[] parts = s.split(",");
+				final Float[] col = new Float[3];
+				for (int i = 0; i < 3; i++) {
 					col[i] = Float.parseFloat(parts[i].trim());
 				}
 				return col;
 			} else {
-				Field f = RGBColour.class.getField(s.toUpperCase());
+				final Field f = RGBColour.class.getField(s.toUpperCase());
 				return (Float[]) f.get(null);
 			}
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			return RGBColour.BLACK;
 		}
 	}
-	
+
 	/**
 	 * Generate a random colour
+	 * 
 	 * @return a random colour
 	 */
-	public static Float[] randomColour() { 
-		Float[] c = new Float[3];
+	public static Float[] randomColour() {
+		final Float[] c = new Float[3];
 
-		c[0] = (float)Math.random();
-		c[1] = (float)Math.random();
-		c[2] = (float)Math.random();
+		c[0] = (float) Math.random();
+		c[1] = (float) Math.random();
+		c[2] = (float) Math.random();
 
 		return c;
 	}
-	
+
 	/**
 	 * Generate a list of random colours.
-	 * @param n number of colours
+	 * 
+	 * @param n
+	 *            number of colours
 	 * @return list of randomly generated colours
 	 */
 	public static List<Float[]> randomColours(int n) {
-		List<Float[]> colours = new ArrayList<Float[]>();
-		
-		for (int i=0; i<n; i++)
+		final List<Float[]> colours = new ArrayList<Float[]>();
+
+		for (int i = 0; i < n; i++)
 			colours.add(randomColour());
-		
+
 		return colours;
+	}
+
+	/**
+	 * Generate a range of colours from a {@link ColourMap}.
+	 * 
+	 * @param cm
+	 *            the map
+	 * @param n
+	 *            number of colours
+	 * @return the colours
+	 */
+	public static Float[][] coloursFromMap(ColourMap cm, int n) {
+		final Float[][] cols = new Float[n][];
+		for (int i = 0; i < n; i++) {
+			final float frac = 1f / (n + 1);
+			cols[i] = cm.apply(frac);
+		}
+		return cols;
 	}
 }
