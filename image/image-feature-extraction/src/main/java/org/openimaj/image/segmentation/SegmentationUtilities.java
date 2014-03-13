8b049b54ee4e81554d3dcd16e036355f758bf839
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-03-13 15:29:46 +0000

    make segmentation utils work with pixelsets rather than CCs

--- image/image-feature-extraction/src/main/java/org/openimaj/image/segmentation/SegmentationUtilities.java
+++ image/image-feature-extraction/src/main/java/org/openimaj/image/segmentation/SegmentationUtilities.java
@@ -35,42 +35,49 @@ import org.openimaj.image.MBFImage;
 import org.openimaj.image.colour.ColourSpace;
 import org.openimaj.image.colour.RGBColour;
 import org.openimaj.image.pixel.ConnectedComponent;
+import org.openimaj.image.pixel.PixelSet;
 import org.openimaj.image.processor.connectedcomponent.render.BlobRenderer;
 
 /**
- * Some utility functions for dealing with segmented output 
+ * Some utility functions for dealing with segmented output
  * 
  * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
- *
+ * 
  */
 public class SegmentationUtilities {
-	private SegmentationUtilities() {}
-	
+	private SegmentationUtilities() {
+	}
+
 	/**
 	 * Render the components to the image with randomly assigned colours.
 	 * 
-	 * @param image Image to draw to
-	 * @param components the components
+	 * @param image
+	 *            Image to draw to
+	 * @param components
+	 *            the components
 	 * @return the image
 	 */
-	public static MBFImage renderSegments(MBFImage image, List<ConnectedComponent> components) {
-		for (ConnectedComponent cc : components) {
-			BlobRenderer<Float[]> br = new BlobRenderer<Float[]>(image, RGBColour.randomColour());
-			br.process(cc);
+	public static MBFImage renderSegments(MBFImage image, List<? extends PixelSet> components) {
+		for (final PixelSet cc : components) {
+			final BlobRenderer<Float[]> br = new BlobRenderer<Float[]>(image, RGBColour.randomColour());
+			br.process(new ConnectedComponent(cc.pixels));
 		}
-		
+
 		return image;
 	}
-	
+
 	/**
 	 * Render the components to an image with randomly assigned colours.
 	 * 
-	 * @param width Width of image.
-	 * @param height Height of image.
-	 * @param components the components.
+	 * @param width
+	 *            Width of image.
+	 * @param height
+	 *            Height of image.
+	 * @param components
+	 *            the components.
 	 * @return the rendered image.
 	 */
-	public static MBFImage renderSegments(int width, int height, List<ConnectedComponent> components) {
+	public static MBFImage renderSegments(int width, int height, List<? extends PixelSet> components) {
 		return renderSegments(new MBFImage(width, height, ColourSpace.RGB), components);
 	}
 }
