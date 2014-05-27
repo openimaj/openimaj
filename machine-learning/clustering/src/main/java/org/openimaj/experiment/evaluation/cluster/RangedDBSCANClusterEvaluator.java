7c08f2dcac98678214b8a5ed27070f36a42285cb
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-05-27 15:58:43 +0000

    removed DoubleRange and IntegerRange (use Uniform{Double,Integer}RangeIterable instead)

--- machine-learning/clustering/src/main/java/org/openimaj/experiment/evaluation/cluster/RangedDBSCANClusterEvaluator.java
+++ machine-learning/clustering/src/main/java/org/openimaj/experiment/evaluation/cluster/RangedDBSCANClusterEvaluator.java
@@ -35,62 +35,75 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.openimaj.data.DoubleRange;
 import org.openimaj.experiment.evaluation.AnalysisResult;
 import org.openimaj.experiment.evaluation.Evaluator;
 import org.openimaj.experiment.evaluation.cluster.analyser.ClusterAnalyser;
 import org.openimaj.ml.clustering.dbscan.SparseMatrixDBSCAN;
 import org.openimaj.util.function.Function;
+import org.openimaj.util.iterator.UniformDoubleRangeIterable;
 
 import ch.akuhn.matrix.SparseMatrix;
 
 /**
  * @author Sina Samangooei (ss@ecs.soton.ac.uk)
- *
- * @param <D> The type of data which the internal clusterer can cluster lists of
- * @param <T> The type of results the
+ * 
+ * @param <D>
+ *            The type of data which the internal clusterer can cluster lists of
+ * @param <T>
+ *            The type of results the
  */
-public class RangedDBSCANClusterEvaluator<D, T extends AnalysisResult> implements Evaluator<Map<Double,int[][]>,RangedAnalysisResult<Double,T>>{
+public class RangedDBSCANClusterEvaluator<D, T extends AnalysisResult>
+		implements
+			Evaluator<Map<Double, int[][]>, RangedAnalysisResult<Double, T>>
+{
 
 	private int[][] correct;
 	private ClusterAnalyser<T> analyser;
 	private SparseMatrixDBSCAN gen;
 	private SparseMatrix data;
-	DoubleRange r;
-	
-	
-	
+	UniformDoubleRangeIterable r;
+
 	/**
-	 * @param r the range of values for the {@link SparseMatrixDBSCAN} eps value
+	 * @param r
+	 *            the range of values for the {@link SparseMatrixDBSCAN} eps
+	 *            value
 	 * @param gen
-	 * @param data 
+	 * @param data
 	 * @param clusters
 	 * @param analyser
 	 */
-	public RangedDBSCANClusterEvaluator(DoubleRange r, SparseMatrixDBSCAN gen, SparseMatrix data, int[][] clusters, ClusterAnalyser<T> analyser) {
+	public RangedDBSCANClusterEvaluator(UniformDoubleRangeIterable r, SparseMatrixDBSCAN gen, SparseMatrix data,
+			int[][] clusters, ClusterAnalyser<T> analyser)
+	{
 		this.gen = gen;
 		this.correct = clusters;
 		this.analyser = analyser;
 		this.data = data;
 	}
-	
+
 	/**
-	 * @param r the range of values for the {@link SparseMatrixDBSCAN} eps value
+	 * @param r
+	 *            the range of values for the {@link SparseMatrixDBSCAN} eps
+	 *            value
 	 * @param gen
-	 * @param data 
-	 * @param dataset extract the elements of this map "in order" and build a ground truth. very dangerous.
+	 * @param data
+	 * @param dataset
+	 *            extract the elements of this map "in order" and build a ground
+	 *            truth. very dangerous.
 	 * @param analyser
 	 */
-	public <A,B> RangedDBSCANClusterEvaluator(DoubleRange r, SparseMatrixDBSCAN gen, SparseMatrix data, Map<A,? extends List<B>> dataset, ClusterAnalyser<T> analyser) {
+	public <A, B> RangedDBSCANClusterEvaluator(UniformDoubleRangeIterable r, SparseMatrixDBSCAN gen, SparseMatrix data,
+			Map<A, ? extends List<B>> dataset, ClusterAnalyser<T> analyser)
+	{
 		this.r = r;
 		this.gen = gen;
 		this.correct = new int[dataset.size()][];
 		int j = 0;
 		int k = 0;
-		for (Entry<A, ? extends List<B>> es : dataset.entrySet()) {
+		for (final Entry<A, ? extends List<B>> es : dataset.entrySet()) {
 			this.correct[j] = new int[es.getValue().size()];
 			int i = 0;
-			List<B> value = es.getValue();
+			final List<B> value = es.getValue();
 			for (int l = 0; l < value.size(); l++) {
 				this.correct[j][i++] = k;
 				k++;
@@ -100,31 +113,35 @@ public class RangedDBSCANClusterEvaluator<D, T extends AnalysisResult> implement
 		this.analyser = analyser;
 		this.data = data;
 	}
-	
+
 	/**
-	 * @param r the range of values for the {@link SparseMatrixDBSCAN} eps value
+	 * @param r
+	 *            the range of values for the {@link SparseMatrixDBSCAN} eps
+	 *            value
 	 * @param gen
-	 * @param data 
-	 * @param indexFunc given a data instance, return its index
-	 * @param dataset 
+	 * @param data
+	 * @param indexFunc
+	 *            given a data instance, return its index
+	 * @param dataset
 	 * @param analyser
 	 */
-	public <A,B> RangedDBSCANClusterEvaluator(
-			DoubleRange r, 
-			SparseMatrixDBSCAN gen, 
+	public <A, B> RangedDBSCANClusterEvaluator(
+			UniformDoubleRangeIterable r,
+			SparseMatrixDBSCAN gen,
 			SparseMatrix data,
-			Function<B,Integer> indexFunc,
-			Map<A,? extends List<B>> dataset, 
-			ClusterAnalyser<T> analyser) {
+			Function<B, Integer> indexFunc,
+			Map<A, ? extends List<B>> dataset,
+			ClusterAnalyser<T> analyser)
+	{
 		this.r = r;
 		this.gen = gen;
 		this.correct = new int[dataset.size()][];
 		int j = 0;
-		for (Entry<A, ? extends List<B>> es : dataset.entrySet()) {
+		for (final Entry<A, ? extends List<B>> es : dataset.entrySet()) {
 			this.correct[j] = new int[es.getValue().size()];
 			int i = 0;
-			List<B> value = es.getValue();
-			for (B b : value) {
+			final List<B> value = es.getValue();
+			for (final B b : value) {
 				this.correct[j][i++] = indexFunc.apply(b);
 			}
 			j++;
@@ -132,31 +149,35 @@ public class RangedDBSCANClusterEvaluator<D, T extends AnalysisResult> implement
 		this.analyser = analyser;
 		this.data = data;
 	}
-	
+
 	/**
-	 * @param r the range of values for the {@link SparseMatrixDBSCAN} eps value
+	 * @param r
+	 *            the range of values for the {@link SparseMatrixDBSCAN} eps
+	 *            value
 	 * @param gen
 	 * @param dataset
-	 * @param transform turn a list of dataset items into the required type for clustering
+	 * @param transform
+	 *            turn a list of dataset items into the required type for
+	 *            clustering
 	 * @param analyser
 	 */
-	public <A,B> RangedDBSCANClusterEvaluator(
-		DoubleRange r, 
-		SparseMatrixDBSCAN gen, 
-		Map<A,? extends List<B>> dataset, 
-		Function<List<B>,SparseMatrix> transform, 
-		ClusterAnalyser<T> analyser
-	) {
+	public <A, B> RangedDBSCANClusterEvaluator(
+			UniformDoubleRangeIterable r,
+			SparseMatrixDBSCAN gen,
+			Map<A, ? extends List<B>> dataset,
+			Function<List<B>, SparseMatrix> transform,
+			ClusterAnalyser<T> analyser)
+	{
 		this.r = r;
 		this.gen = gen;
 		this.analyser = analyser;
 		this.correct = new int[dataset.size()][];
 		int j = 0;
-		List<B> flattened = new ArrayList<B>();
-		for (Entry<A, ? extends List<B>> es : dataset.entrySet()) {
+		final List<B> flattened = new ArrayList<B>();
+		for (final Entry<A, ? extends List<B>> es : dataset.entrySet()) {
 			this.correct[j] = new int[es.getValue().size()];
 			int i = 0;
-			for (B b : es.getValue()) {
+			for (final B b : es.getValue()) {
 				this.correct[j][i++] = flattened.size();
 				flattened.add(b);
 			}
@@ -164,25 +185,21 @@ public class RangedDBSCANClusterEvaluator<D, T extends AnalysisResult> implement
 		}
 		this.data = transform.apply(flattened);
 	}
-	
-	
-	
-	
 
 	@Override
-	public Map<Double,int[][]> evaluate() {
-		Map<Double, int[][]> ret = new HashMap<Double, int[][]>();
-		for (Double eps : this.r) {
+	public Map<Double, int[][]> evaluate() {
+		final Map<Double, int[][]> ret = new HashMap<Double, int[][]>();
+		for (final Double eps : this.r) {
 			this.gen.setEps(eps);
-			ret.put(eps,new ClusterEvaluator<SparseMatrix, T>(gen, data, correct, analyser).evaluate());
+			ret.put(eps, new ClusterEvaluator<SparseMatrix, T>(gen, data, correct, analyser).evaluate());
 		}
 		return ret;
 	}
 
 	@Override
-	public RangedAnalysisResult<Double, T> analyse(Map<Double,int[][]> estimated) {
-		RangedAnalysisResult<Double, T> ret = new RangedAnalysisResult<Double, T>();
-		for (Entry<Double, int[][]> ent : estimated.entrySet()) {
+	public RangedAnalysisResult<Double, T> analyse(Map<Double, int[][]> estimated) {
+		final RangedAnalysisResult<Double, T> ret = new RangedAnalysisResult<Double, T>();
+		for (final Entry<Double, int[][]> ent : estimated.entrySet()) {
 			ret.put(ent.getKey(), this.analyser.analyse(correct, ent.getValue()));
 		}
 		return ret;
--- machine-learning/machine-learning/src/main/java/org/openimaj/ml/linear/experiments/sinabill/LambdaSearchAustrian.java
+++ machine-learning/machine-learning/src/main/java/org/openimaj/ml/linear/experiments/sinabill/LambdaSearchAustrian.java
@@ -42,21 +42,17 @@ import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
-import org.openimaj.data.DoubleRange;
-import org.openimaj.data.IntegerRange;
 import org.openimaj.io.IOUtils;
 import org.openimaj.math.matrix.CFMatrixUtils;
 import org.openimaj.ml.linear.data.BillMatlabFileDataGenerator;
 import org.openimaj.ml.linear.data.BillMatlabFileDataGenerator.Fold;
 import org.openimaj.ml.linear.data.BillMatlabFileDataGenerator.Mode;
-import org.openimaj.ml.linear.data.MatlabFileDataGenerator;
 import org.openimaj.ml.linear.evaluation.BilinearEvaluator;
 import org.openimaj.ml.linear.evaluation.RootMeanSumLossEvaluator;
 import org.openimaj.ml.linear.learner.BilinearLearnerParameters;
 import org.openimaj.ml.linear.learner.BilinearSparseOnlineLearner;
 import org.openimaj.ml.linear.learner.init.SparseZerosInitStrategy;
 import org.openimaj.ml.linear.learner.loss.MatSquareLossFunction;
-import org.openimaj.util.array.DoubleArrayView;
 import org.openimaj.util.pair.Pair;
 
 import com.google.common.primitives.Doubles;
@@ -65,14 +61,16 @@ import com.jmatio.types.MLArray;
 
 /**
  * Optimise lambda and eta0 and learning rates with a line search
+ * 
  * @author Sina Samangooei (ss@ecs.soton.ac.uk)
  */
 public class LambdaSearchAustrian {
-	
+
 	private static final int NFOLDS = 1;
 	private static final String ROOT = "/Users/ss/Experiments/bilinear/austrian/";
 	private static final String OUTPUT_ROOT = "/Users/ss/Dropbox/TrendMiner/Collaboration/StreamingBilinear2014/experiments";
 	private final Logger logger = Logger.getLogger(getClass());
+
 	/**
 	 * @param args
 	 * @throws IOException
@@ -83,50 +81,48 @@ public class LambdaSearchAustrian {
 	}
 
 	private long expStartTime = System.currentTimeMillis();
-	
+
 	/**
 	 * @throws IOException
 	 */
 	public void performExperiment() throws IOException {
-		List<BillMatlabFileDataGenerator.Fold> folds = prepareFolds();
+		final List<BillMatlabFileDataGenerator.Fold> folds = prepareFolds();
 		final BillMatlabFileDataGenerator bmfdg = new BillMatlabFileDataGenerator(
-			new File(dataFromRoot("normalised.mat")), "user_vsr_for_polls_SINA",
-			new File(dataFromRoot("unnormalised.mat")),
-			98, false,
-			folds
-		);
+				new File(dataFromRoot("normalised.mat")), "user_vsr_for_polls_SINA",
+				new File(dataFromRoot("unnormalised.mat")),
+				98, false,
+				folds
+				);
 		prepareExperimentLog();
 		final BilinearEvaluator eval = new RootMeanSumLossEvaluator();
 		for (int i = 0; i < bmfdg.nFolds(); i++) {
 			logger.info("Starting Fold: " + i);
-			BilinearSparseOnlineLearner best = lineSearchParams(i, bmfdg);
+			final BilinearSparseOnlineLearner best = lineSearchParams(i, bmfdg);
 			logger.debug("Best params found! Starting test...");
 			bmfdg.setFold(i, Mode.TEST);
 			eval.setLearner(best);
-			double ev = eval.evaluate(bmfdg.generateAll());
+			final double ev = eval.evaluate(bmfdg.generateAll());
 			logger.debug("Test RMSE: " + ev);
-			
+
 		}
 	}
-	
-	
 
 	private BilinearSparseOnlineLearner lineSearchParams(int fold, BillMatlabFileDataGenerator source) {
 		BilinearSparseOnlineLearner best = null;
 		double bestScore = Double.MAX_VALUE;
 		final BilinearEvaluator eval = new RootMeanSumLossEvaluator();
 		int j = 0;
-		List<BilinearLearnerParameters> parameterLineSearch = parameterLineSearch();
+		final List<BilinearLearnerParameters> parameterLineSearch = parameterLineSearch();
 		logger.info("Optimising params, searching: " + parameterLineSearch.size());
-		for (BilinearLearnerParameters next : parameterLineSearch) {
-			logger.info(String.format("Optimising params %d/%d",j+1,parameterLineSearch.size()));
+		for (final BilinearLearnerParameters next : parameterLineSearch) {
+			logger.info(String.format("Optimising params %d/%d", j + 1, parameterLineSearch.size()));
 			logger.debug("Current Params:\n" + next.toString());
-			BilinearSparseOnlineLearner learner = new BilinearSparseOnlineLearner(next);
+			final BilinearSparseOnlineLearner learner = new BilinearSparseOnlineLearner(next);
 			// Train the model with the new parameters
 			source.setFold(fold, Mode.TRAINING);
-			Pair<Matrix> pair= null;
+			Pair<Matrix> pair = null;
 			logger.debug("Training...");
-			while((pair = source.generate())!=null){
+			while ((pair = source.generate()) != null) {
 				learner.process(pair.firstObject(), pair.secondObject());
 			}
 			logger.debug("Generating score of validation set");
@@ -138,7 +134,7 @@ public class LambdaSearchAustrian {
 			logger.debug("U sparcity: " + CFMatrixUtils.sparsity(learner.getU()));
 			logger.debug("W sparcity: " + CFMatrixUtils.sparsity(learner.getW()));
 			// record the best
-			if(loss < bestScore){
+			if (loss < bestScore) {
 				logger.info("New best score detected!");
 				bestScore = loss;
 				best = learner;
@@ -151,67 +147,63 @@ public class LambdaSearchAustrian {
 		return best;
 	}
 
-
-
 	private void saveFoldParameterLearner(int fold, int j, BilinearSparseOnlineLearner learner) {
 		// save the state
-		final File learnerOut = new File(String.format("%s/fold_%d",currentOutputRoot(),fold), String.format("learner_%d", j));
-		final File learnerOutMat = new File(String.format("%s/fold_%d",currentOutputRoot(),fold), String.format("learner_%d.mat", j));
+		final File learnerOut = new File(String.format("%s/fold_%d", currentOutputRoot(), fold), String.format(
+				"learner_%d", j));
+		final File learnerOutMat = new File(String.format("%s/fold_%d", currentOutputRoot(), fold), String.format(
+				"learner_%d.mat", j));
 		learnerOut.getParentFile().mkdirs();
 		try {
 			IOUtils.writeBinary(learnerOut, learner);
-			Collection<MLArray> data = new ArrayList<MLArray>();
+			final Collection<MLArray> data = new ArrayList<MLArray>();
 			data.add(CFMatrixUtils.toMLArray("u", learner.getU()));
 			data.add(CFMatrixUtils.toMLArray("w", learner.getW()));
-			if(learner.getBias()!=null){
+			if (learner.getBias() != null) {
 				data.add(CFMatrixUtils.toMLArray("b", learner.getBias()));
 			}
-			MatFileWriter writer = new MatFileWriter(learnerOutMat, data);
-		} catch (IOException e) {
+			final MatFileWriter writer = new MatFileWriter(learnerOutMat, data);
+		} catch (final IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-
-
 	private List<BilinearLearnerParameters> parameterLineSearch() {
 		final BilinearLearnerParameters params = prepareParams();
-		BilinearLearnerParametersLineSearch iter = new BilinearLearnerParametersLineSearch(params);
-		
-		iter.addIteration(BilinearLearnerParameters.ETA0_U, Doubles.asList(new double[]{0.0001}));
-		iter.addIteration(BilinearLearnerParameters.ETA0_W, Doubles.asList(new double[]{0.005}));
-		iter.addIteration(BilinearLearnerParameters.ETA0_BIAS, Doubles.asList(new double[]{50}));
-		iter.addIteration(BilinearLearnerParameters.LAMBDA_U, Doubles.asList(new double[]{0.00001}));
-		iter.addIteration(BilinearLearnerParameters.LAMBDA_W, Doubles.asList(new double[]{0.00001}));
-		
-		List<BilinearLearnerParameters> ret = new ArrayList<BilinearLearnerParameters>();
-		for (BilinearLearnerParameters param : iter) {
+		final BilinearLearnerParametersLineSearch iter = new BilinearLearnerParametersLineSearch(params);
+
+		iter.addIteration(BilinearLearnerParameters.ETA0_U, Doubles.asList(new double[] { 0.0001 }));
+		iter.addIteration(BilinearLearnerParameters.ETA0_W, Doubles.asList(new double[] { 0.005 }));
+		iter.addIteration(BilinearLearnerParameters.ETA0_BIAS, Doubles.asList(new double[] { 50 }));
+		iter.addIteration(BilinearLearnerParameters.LAMBDA_U, Doubles.asList(new double[] { 0.00001 }));
+		iter.addIteration(BilinearLearnerParameters.LAMBDA_W, Doubles.asList(new double[] { 0.00001 }));
+
+		final List<BilinearLearnerParameters> ret = new ArrayList<BilinearLearnerParameters>();
+		for (final BilinearLearnerParameters param : iter) {
 			ret.add(param);
 		}
 		return ret;
 	}
 
-
-
 	private List<Fold> prepareFolds() {
-		List<Fold> set_fold = new ArrayList<BillMatlabFileDataGenerator.Fold>();
-		
-//		[24/02/2014 16:58:23] .@bill: 
-		int step = 5;               // % test_size
-		int t_size = 48;            // % training_size
-		int v_size = 8;
+		final List<Fold> set_fold = new ArrayList<BillMatlabFileDataGenerator.Fold>();
+
+		// [24/02/2014 16:58:23] .@bill:
+		final int step = 5; // % test_size
+		final int t_size = 48; // % training_size
+		final int v_size = 8;
 		for (int i = 0; i < NFOLDS; i++) {
-			int total = i * step + t_size;
-			int[] training = new int[total - v_size];
-			int[] test = new int[step];
-			int[] validation = new int[v_size];
+			final int total = i * step + t_size;
+			final int[] training = new int[total - v_size];
+			final int[] test = new int[step];
+			final int[] validation = new int[v_size];
 			int j = 0;
 			int traini = 0;
-			int tt = (int) Math.round(total/2.)-1;
-			for (; j < tt - v_size/2; j++, traini++) {
+			final int tt = (int) Math.round(total / 2.) - 1;
+			for (; j < tt - v_size / 2; j++, traini++) {
 				training[traini] = j;
 			}
-			for(int k = 0; k < validation.length; k++, j++){
+			for (int k = 0; k < validation.length; k++, j++) {
 				validation[k] = j;
 			}
 			for (; j < total; j++, traini++) {
@@ -220,22 +212,22 @@ public class LambdaSearchAustrian {
 			for (int k = 0; k < test.length; k++, j++) {
 				test[k] = j;
 			}
-			Fold foldi = new Fold(training, test, validation);			
+			final Fold foldi = new Fold(training, test, validation);
 			set_fold.add(foldi);
 		}
-//		[24/02/2014 16:59:07] .@bill: set_fold{1,1}
+		// [24/02/2014 16:59:07] .@bill: set_fold{1,1}
 		return set_fold;
 	}
 
 	private BilinearLearnerParameters prepareParams() {
 		final BilinearLearnerParameters params = new BilinearLearnerParameters();
-		
+
 		params.put(BilinearLearnerParameters.ETA0_U, null);
 		params.put(BilinearLearnerParameters.ETA0_W, null);
 		params.put(BilinearLearnerParameters.LAMBDA_U, null);
 		params.put(BilinearLearnerParameters.LAMBDA_W, null);
 		params.put(BilinearLearnerParameters.ETA0_BIAS, null);
-		
+
 		params.put(BilinearLearnerParameters.BICONVEX_TOL, 0.01);
 		params.put(BilinearLearnerParameters.BICONVEX_MAXITER, 10);
 		params.put(BilinearLearnerParameters.BIAS, true);
@@ -244,52 +236,56 @@ public class LambdaSearchAustrian {
 		params.put(BilinearLearnerParameters.LOSS, new MatSquareLossFunction());
 		return params;
 	}
-	
+
 	/**
 	 * @param data
 	 * @return the data file from the root
 	 */
-	public static String dataFromRoot(String data){
-		return String.format("%s/%s",ROOT,data);
+	public static String dataFromRoot(String data) {
+		return String.format("%s/%s", ROOT, data);
 	}
-	
+
 	protected void prepareExperimentLog() throws IOException {
-		ConsoleAppender console = new ConsoleAppender(); //create appender
-		//configure the appender
-		String PATTERN = "[%p->%C{1}] %m%n";
-		console.setLayout(new PatternLayout(PATTERN)); 
+		final ConsoleAppender console = new ConsoleAppender(); // create
+																// appender
+		// configure the appender
+		final String PATTERN = "[%p->%C{1}] %m%n";
+		console.setLayout(new PatternLayout(PATTERN));
 		console.setThreshold(Level.INFO);
 		console.activateOptions();
-	  	// add appender to any Logger (here is root)
+		// add appender to any Logger (here is root)
 		Logger.getRootLogger().addAppender(console);
-		File expRoot = prepareExperimentRoot();
-		
-		File logFile = new File(expRoot,"log");
-		if(logFile.exists())logFile.delete();
-		String TIMED_PATTERN = "[%d{HH:mm:ss} %p->%C{1}] %m%n";
-		FileAppender file = new FileAppender(new PatternLayout(TIMED_PATTERN), logFile.getAbsolutePath()); 
+		final File expRoot = prepareExperimentRoot();
+
+		final File logFile = new File(expRoot, "log");
+		if (logFile.exists())
+			logFile.delete();
+		final String TIMED_PATTERN = "[%d{HH:mm:ss} %p->%C{1}] %m%n";
+		final FileAppender file = new FileAppender(new PatternLayout(TIMED_PATTERN), logFile.getAbsolutePath());
 		file.setThreshold(Level.DEBUG);
 		file.activateOptions();
-		Logger.getRootLogger().addAppender(file );
+		Logger.getRootLogger().addAppender(file);
 		logger.info("Experiment root: " + expRoot);
-		
+
 	}
-	
+
 	/**
 	 * @return
 	 * @throws IOException
 	 */
 	public File prepareExperimentRoot() throws IOException {
-		String experimentRoot = currentOutputRoot();
-		File expRoot = new File(experimentRoot);
-		if(expRoot.exists() && expRoot.isDirectory()) return expRoot;
+		final String experimentRoot = currentOutputRoot();
+		final File expRoot = new File(experimentRoot);
+		if (expRoot.exists() && expRoot.isDirectory())
+			return expRoot;
 		logger.debug("Experiment root: " + expRoot);
-		if(!expRoot.mkdirs()) throw new IOException("Couldn't prepare experiment output");
+		if (!expRoot.mkdirs())
+			throw new IOException("Couldn't prepare experiment output");
 		return expRoot;
 	}
 
 	private String currentOutputRoot() {
-		return String.format("%s/%s/%s",OUTPUT_ROOT,getExperimentSetName(),""+currentExperimentTime());
+		return String.format("%s/%s/%s", OUTPUT_ROOT, getExperimentSetName(), "" + currentExperimentTime());
 	}
 
 	private long currentExperimentTime() {
