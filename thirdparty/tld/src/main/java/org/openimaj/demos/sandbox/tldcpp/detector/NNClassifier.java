776c1fba7cad779c0054956e3030349b7c206ad5
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-05-27 16:13:09 +0000

    fix for changes introduced during ResizeProcessor refactor

--- thirdparty/tld/src/main/java/org/openimaj/demos/sandbox/tldcpp/detector/NNClassifier.java
+++ thirdparty/tld/src/main/java/org/openimaj/demos/sandbox/tldcpp/detector/NNClassifier.java
@@ -8,26 +8,25 @@ import org.openimaj.image.analysis.algorithm.TemplateMatcher;
 import org.openimaj.math.geometry.shape.Rectangle;
 
 /**
- * The third and most powerful, but equally most slow parts of the {@link DetectorCascade}.
- * Holding a list of falsePositives and truePositives, a classification score can be ascribed
- * to a new patch which can be used as a confidence that a given patch is positive. This is 
- * calculated using the correlation between the new patch and the false positive and falst negatives
+ * The third and most powerful, but equally most slow parts of the
+ * {@link DetectorCascade}. Holding a list of falsePositives and truePositives,
+ * a classification score can be ascribed to a new patch which can be used as a
+ * confidence that a given patch is positive. This is calculated using the
+ * correlation between the new patch and the false positive and falst negatives
  * such that:
  * 
  * confidence = dP / (dN + dP)
  * 
- * and 
- * dP = max(corr(patch,truePositives))
- * dP = max(corr(patch,falsePositives))
+ * and dP = max(corr(patch,truePositives)) dP = max(corr(patch,falsePositives))
  * 
- * if no true positives have been seen, classify will always return 0
- * if not false positives have been seen, classify will always return 1
+ * if no true positives have been seen, classify will always return 0 if not
+ * false positives have been seen, classify will always return 1
  * 
- * classify is used by filter such that if the confidence of a patch is
- * larger than {@link #thetaTP} the patch is though to be a good patch for
- * the object.
+ * classify is used by filter such that if the confidence of a patch is larger
+ * than {@link #thetaTP} the patch is though to be a good patch for the object.
+ * 
  * @author Sina Samangooei (ss@ecs.soton.ac.uk)
- *
+ * 
  */
 public class NNClassifier {
 	/**
@@ -36,18 +35,20 @@ public class NNClassifier {
 	public boolean enabled;
 
 	/**
-	 * Used as the lower bound of a historysis threshold (i.e. if a detection is made with a confidence over {@link #thetaTP}, the next detection can be a little worse by matching this)
+	 * Used as the lower bound of a historysis threshold (i.e. if a detection is
+	 * made with a confidence over {@link #thetaTP}, the next detection can be a
+	 * little worse by matching this)
 	 */
 	public float thetaFP;
 	/**
-	 * Used as the upper bound threshold 
+	 * Used as the upper bound threshold
 	 */
 	public float thetaTP;
 	ScaleIndexRectangle[] windows;
 	DetectionResult detectionResult;
 	List<NormalizedPatch> falsePositives;
 	List<NormalizedPatch> truePositives;
-	
+
 	/**
 	 * Sets thetaFP as 0.5f and thetaTP as .65f
 	 */
@@ -59,7 +60,6 @@ public class NNClassifier {
 		falsePositives = new ArrayList<NormalizedPatch>();
 
 	}
-
 
 	/**
 	 * clear falst positives and true positives
@@ -73,60 +73,62 @@ public class NNClassifier {
 	 * 
 	 * @param f1
 	 * @param f2
-	 * @return correlation between two patches (assumed to be the same size) calculated using {@link TemplateMatcherMode}
+	 * @return correlation between two patches (assumed to be the same size)
+	 *         calculated using {@link TemplateMatcherMode}
 	 */
 	private float ncc(FImage f1, FImage f2) {
-		float normcorr = TemplateMatcher.Mode.NORM_CORRELATION.computeMatchScore(f1.pixels, 0, 0, f2.pixels, 0, 0, f1.width, f1.height);
+		final float normcorr = TemplateMatcher.Mode.NORM_CORRELATION.computeMatchScore(f1.pixels, 0, 0, f2.pixels, 0, 0,
+				f1.width, f1.height);
 		return normcorr;
 	}
 
 	/**
 	 * 
 	 * @param patch
-	 * @return The confidence that a given patch is a postive 
+	 * @return The confidence that a given patch is a postive
 	 */
 	public float classifyPatch(NormalizedPatch patch) {
 
-		if(truePositives.isEmpty()) {
+		if (truePositives.isEmpty()) {
 			return 0;
 		}
 
-		if(falsePositives.isEmpty()) {
+		if (falsePositives.isEmpty()) {
 			return 1;
 		}
 
 		float ccorr_max_p = 0;
-		//Compare patch to positive patches
-		for(int i = 0; i < truePositives.size(); i++) {
-			float ccorr = ncc(truePositives.get(i).normalisedPatch, patch.normalisedPatch);
-			if(ccorr > ccorr_max_p) {
+		// Compare patch to positive patches
+		for (int i = 0; i < truePositives.size(); i++) {
+			final float ccorr = ncc(truePositives.get(i).normalisedPatch, patch.normalisedPatch);
+			if (ccorr > ccorr_max_p) {
 				ccorr_max_p = ccorr;
 			}
 		}
 
 		float ccorr_max_n = 0;
-		//Compare patch to positive patches
-		for(int i = 0; i < falsePositives.size(); i++) {
-			float ccorr = ncc(falsePositives.get(i).normalisedPatch, patch.normalisedPatch);
-			if(ccorr > ccorr_max_n) {
+		// Compare patch to positive patches
+		for (int i = 0; i < falsePositives.size(); i++) {
+			final float ccorr = ncc(falsePositives.get(i).normalisedPatch, patch.normalisedPatch);
+			if (ccorr > ccorr_max_n) {
 				ccorr_max_n = ccorr;
 			}
 		}
 
-		float dN = 1-ccorr_max_n;
-		float dP = 1-ccorr_max_p;
+		final float dN = 1 - ccorr_max_n;
+		final float dP = 1 - ccorr_max_p;
 
-		float distance = dN/(dN+dP);
+		final float distance = dN / (dN + dP);
 		return distance;
 	}
- 
+
 	/**
 	 * @param img
 	 * @param bb
 	 * @return confidence of the bb in image
 	 */
 	public float classifyBB(FImage img, Rectangle bb) {
-		NormalizedPatch patch = new NormalizedPatch();
+		final NormalizedPatch patch = new NormalizedPatch();
 		patch.source = img;
 		patch.window = bb;
 		patch.prepareNormalisedPatch();
@@ -136,11 +138,12 @@ public class NNClassifier {
 
 	float classifyWindow(FImage img, int windowIdx) {
 
-		ScaleIndexRectangle bbox = windows[windowIdx];
-		NormalizedPatch patch = new NormalizedPatch();
+		final ScaleIndexRectangle bbox = windows[windowIdx];
+		final NormalizedPatch patch = new NormalizedPatch();
 		patch.window = bbox;
 		patch.source = img;
-		// here we reuse the scales images as the patch of the right width/height and just write into it.
+		// here we reuse the scales images as the patch of the right
+		// width/height and just write into it.
 		patch.normalisedPatch = patch.zoomAndNormaliseTo(NormalizedPatch.SLUT_WORKSPACE);
 
 		return classifyPatch(patch);
@@ -149,14 +152,16 @@ public class NNClassifier {
 	/**
 	 * @param img
 	 * @param windowIdx
-	 * @return Filter a window by getting its confidence and returning true if confidence > thetaTP
+	 * @return Filter a window by getting its confidence and returning true if
+	 *         confidence > thetaTP
 	 */
 	public boolean filter(FImage img, int windowIdx) {
-		if(!enabled) return true;
+		if (!enabled)
+			return true;
 
-		float conf = classifyWindow(img, windowIdx);
+		final float conf = classifyWindow(img, windowIdx);
 
-		if(conf < thetaTP) {
+		if (conf < thetaTP) {
 			return false;
 		}
 
@@ -164,32 +169,36 @@ public class NNClassifier {
 	}
 
 	/**
-	 * Given a list of patches, classify each patch. 
-	 * If the patch is said to be positive and has a confidence lower than {@link #thetaTP} add the patch to the true positives
-	 * If the patch is said to be negative and has a confidence higher than {@link #thetaFP} add the patch to the false positives
-	 *  
+	 * Given a list of patches, classify each patch. If the patch is said to be
+	 * positive and has a confidence lower than {@link #thetaTP} add the patch
+	 * to the true positives If the patch is said to be negative and has a
+	 * confidence higher than {@link #thetaFP} add the patch to the false
+	 * positives
+	 * 
 	 * @param patches
 	 */
 	public void learn(List<NormalizedPatch> patches) {
-		//TODO: Randomization might be a good idea here
-		for(int i = 0; i < patches.size(); i++) {
+		// TODO: Randomization might be a good idea here
+		for (int i = 0; i < patches.size(); i++) {
 
-			NormalizedPatch patch = patches.get(i);
-			// if the patch is a negative one, the image has not been normalised etc yet!
+			final NormalizedPatch patch = patches.get(i);
+			// if the patch is a negative one, the image has not been normalised
+			// etc yet!
 			// it uses the prepared windows, so a held scale patch can be used
-			if(!patch.positive){
+			if (!patch.positive) {
 				patch.normalisedPatch = patch.zoomAndNormaliseTo(NormalizedPatch.SLUT_WORKSPACE);
 			}
 
-			float conf = classifyPatch(patch);
+			final float conf = classifyPatch(patch);
 
-			if(patch.positive && conf <= thetaTP) {
+			if (patch.positive && conf <= thetaTP) {
 				truePositives.add(patch);
 			}
 
-			if(!patch.positive && conf >= thetaFP) {
+			if (!patch.positive && conf >= thetaFP) {
 				// We must handle the SLUT_WORKSPACE!
-				// If we're negative we are using the slut, if we're planning to keep this negative we must NOW clone the slut
+				// If we're negative we are using the slut, if we're planning to
+				// keep this negative we must NOW clone the slut
 				patch.normalisedPatch = patch.normalisedPatch.clone();
 				falsePositives.add(patch);
 			}
@@ -197,10 +206,11 @@ public class NNClassifier {
 
 	}
 
-
+	/**
+	 * @return the positively classified patches
+	 */
 	public List<NormalizedPatch> getPositivePatches() {
 		return this.truePositives;
 	}
-
 
 }
--- thirdparty/tld/src/main/java/org/openimaj/demos/sandbox/tldcpp/detector/NormalizedPatch.java
+++ thirdparty/tld/src/main/java/org/openimaj/demos/sandbox/tldcpp/detector/NormalizedPatch.java
@@ -2,16 +2,17 @@ package org.openimaj.demos.sandbox.tldcpp.detector;
 
 import org.openimaj.image.FImage;
 import org.openimaj.image.processing.algorithm.MeanCenter;
-import org.openimaj.image.processing.resize.BasicFilter;
 import org.openimaj.image.processing.resize.ResizeFilterFunction;
 import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.image.processing.resize.filters.TriangleFilter;
 import org.openimaj.math.geometry.shape.Rectangle;
 
 /**
- * Defines a intensity normalised patch extracted from an image.
- * Allowances are made for reuse of patches 
+ * Defines a intensity normalised patch extracted from an image. Allowances are
+ * made for reuse of patches
+ * 
  * @author Sina Samangooei (ss@ecs.soton.ac.uk)
- *
+ * 
  */
 public class NormalizedPatch {
 	private final static MeanCenter msp = new MeanCenter();
@@ -19,12 +20,13 @@ public class NormalizedPatch {
 	 * Normalised patch size
 	 */
 	public static final int TLD_PATCH_SIZE = 15;
-	private static final ResizeFilterFunction filter = new BasicFilter();
-	
+	private static final ResizeFilterFunction filter = TriangleFilter.INSTANCE;
+
 	/**
-	 * The slut workspace gets around a little bit. Use the slut workspace but don't expect it to be yours for long.
+	 * The slut workspace gets around a little bit. Use the slut workspace but
+	 * don't expect it to be yours for long.
 	 */
-	public static final FImage SLUT_WORKSPACE = new FImage(TLD_PATCH_SIZE,TLD_PATCH_SIZE);
+	public static final FImage SLUT_WORKSPACE = new FImage(TLD_PATCH_SIZE, TLD_PATCH_SIZE);
 	/**
 	 * Is this patch positive, i.e. representative of the object
 	 */
@@ -41,44 +43,52 @@ public class NormalizedPatch {
 	 * The extracted patch, might be null, might be the SLUT_WORKSPACE.
 	 */
 	public FImage normalisedPatch;
-	
+
 	/**
-	 * A function which uses {@link ResizeProcessor#zoom(FImage, Rectangle, FImage, Rectangle, ResizeFilterFunction, double)}
-	 * on a to put {@link NormalizedPatch#window} form {@link NormalizedPatch#source} into normalisedPatch.
+	 * A function which uses
+	 * {@link ResizeProcessor#zoom(FImage, Rectangle, FImage, Rectangle, ResizeFilterFunction)}
+	 * on a to put {@link NormalizedPatch#window} form
+	 * {@link NormalizedPatch#source} into normalisedPatch.
 	 * 
-	 * This is not a convenient function but it allows for very efficient resize/normalisation process (with minimal new stuff
-	 * constructed)
+	 * This is not a convenient function but it allows for very efficient
+	 * resize/normalisation process (with minimal new stuff constructed)
+	 * 
 	 * @param holder
-	 * @return the holder as a convenience 
+	 * @return the holder as a convenience
 	 */
-	public FImage zoomAndNormaliseTo(FImage holder){
-		ResizeProcessor.zoom(source, window,holder,holder.getBounds(),filter,filter.getDefaultSupport());
+	protected FImage zoomAndNormaliseTo(FImage holder) {
+		ResizeProcessor.zoom(source, window, holder, holder.getBounds(), filter);
 		return holder.processInplace(msp);
 	}
 
 	/**
 	 * calculate the variance, sets the valueImg if it is null
-	 * @return an inefficient way to calculate variance of this window, a new image is constructed!
+	 * 
+	 * @return an inefficient way to calculate variance of this window, a new
+	 *         image is constructed!
 	 */
 	public float calculateVariance() {
 		prepareNormalisedPatch();
-		float[][] value = normalisedPatch.pixels;
+		final float[][] value = normalisedPatch.pixels;
 		float temp = 0;
-	    int n = normalisedPatch.width * normalisedPatch.height;
-	    for(int y = 0; y < normalisedPatch.height; y++) {
-	    	for(int x = 0; x < normalisedPatch.width; x++) {
-	    		temp += (value[y][x] ) * (value[y][x] ); // There are two implied (- 0)'s here. these values are MEAN CENTERED 
-	    	}
-	    }
-	    return temp / n;
+		final int n = normalisedPatch.width * normalisedPatch.height;
+		for (int y = 0; y < normalisedPatch.height; y++) {
+			for (int x = 0; x < normalisedPatch.width; x++) {
+				temp += (value[y][x]) * (value[y][x]); // There are two implied
+														// (- 0)'s here. these
+														// values are MEAN
+														// CENTERED
+			}
+		}
+		return temp / n;
 	}
 
 	/**
 	 * for construction of a new normalised patch
 	 */
 	public void prepareNormalisedPatch() {
-		if(this.normalisedPatch == null){
-			this.normalisedPatch = new FImage(TLD_PATCH_SIZE,TLD_PATCH_SIZE);
+		if (this.normalisedPatch == null) {
+			this.normalisedPatch = new FImage(TLD_PATCH_SIZE, TLD_PATCH_SIZE);
 			zoomAndNormaliseTo(normalisedPatch);
 		}
 	}
