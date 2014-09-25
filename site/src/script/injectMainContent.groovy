e949dc0a6fe1aa057afd03fbde2640b21e3805c4
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-09-25 14:05:51 +0000

    auto-version the deps on the index page. fix the JTransforms pom.

--- site/src/script/injectMainContent.groovy
+++ site/src/script/injectMainContent.groovy
@@ -1,3 +1,10 @@
+@GrabResolver(name='groovy-template', root='http://artifacts.iteego.com/artifactory/public-release-local')
+@Grab('org.codehaus.groovy:groovy-streaming-template-engine:2.3.6.1')
+import groovy.text.StreamingTemplateEngine
+
+//groovy.grape.Grape.addResolver([name:'groovy-template', root:'http://artifacts.iteego.com/artifactory/public-release-local'])
+//groovy.grape.Grape.grab(group:'org.codehaus.groovy', module:'groovy-streaming-template-engine', version:'[2.3.6.1,)')
+
 /**
  * Script to patch static html pages into the maven site template, circumventing some 
  * of the limitations of doxia.
@@ -18,6 +25,8 @@ sourceDir.eachFile { file ->
 	File newFile = new File(targetDir, file.name);
 	File tFile = new File(targetDir, "template-"+file.name);
     String sourceContent = file.text;
+    //sourceContent = Class.forName("groovy.text.StreamingTemplateEngine").newInstance().createTemplate(sourceContent).make().writeTo(new StringWriter()).toString();
+    sourceContent = new groovy.text.StreamingTemplateEngine().createTemplate(sourceContent).make(binding.variables).writeTo(new StringWriter()).toString();
 
 	thisTemplateContent = templateContent;
 	if (tFile.exists()) {
