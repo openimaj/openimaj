4598b5dc5d1b57167fb63ffc00209ce258baadee
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2015-02-09 17:56:27 +0000

    adding work-arounds to bugs in java 7/8 on osx (and at the same time providing standard full-screen functionality on osx)

--- content/slideshow/src/main/java/org/openimaj/content/slideshow/FullscreenUtility.java
+++ content/slideshow/src/main/java/org/openimaj/content/slideshow/FullscreenUtility.java
@@ -31,6 +31,8 @@ package org.openimaj.content.slideshow;
 
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Window;
+import java.lang.reflect.Method;
 
 import javax.swing.JFrame;
 
@@ -38,9 +40,9 @@ import org.apache.commons.lang.SystemUtils;
 
 /**
  * Utility class for dealing with fullscreen Swing applications.
- * 
+ *
  * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
- * 
+ *
  */
 public class FullscreenUtility {
 	protected JFrame window;
@@ -49,75 +51,120 @@ public class FullscreenUtility {
 	/**
 	 * Construct with the given JFrame. The utility will allow the frame to be
 	 * toggled between windowed and fullscreen mode.
-	 * 
+	 *
 	 * @param frame
 	 *            The frame.
 	 */
 	public FullscreenUtility(JFrame frame) {
 		this.window = frame;
+
+		if (SystemUtils.IS_OS_MAC_OSX) {
+			// if we're on a mac, we'll add the fullscreen hint to the window so
+			// the standard controls work
+			try {
+				final Class<?> util = Class.forName("com.apple.eawt.FullScreenUtilities");
+				final Class<?> params[] = new Class[] { Window.class, Boolean.TYPE };
+				final Method method = util.getMethod("setWindowCanFullScreen", params);
+
+				method.invoke(util, window, true);
+			} catch (final Exception e) {
+				// ignore
+			}
+		}
 	}
 
 	/**
 	 * Method allows changing whether this window is displayed in fullscreen or
 	 * windowed mode.
-	 * 
+	 *
 	 * @param fullscreen
 	 *            true = change to fullscreen, false = change to windowed
 	 */
-	public void setFullscreen(boolean fullscreen)
+	public void setFullscreen(boolean fullscreen) {
+		if (this.fullscreen != fullscreen) {
+			if (SystemUtils.IS_OS_MAC_OSX) {
+				setFullscreenOSX(fullscreen);
+			} else {
+				setFullscreenAWT(fullscreen);
+			}
+		}
+	}
+
+	// See https://bugs.openjdk.java.net/browse/JDK-8013547
+	// basically AWT fullscreen has big problems (with comboboxes and
+	// keylisteners) on OSX with Java 7 & 8 (and maybe later versions of 6).
+	// We'll try and use the EAWT fullscreen stuff if possible and fall back if
+	// not...
+	private void setFullscreenOSX(boolean fullscreen) {
+		// change modes
+		this.fullscreen = fullscreen;
+
+		try {
+			final Class<?> appClz = Class.forName("com.apple.eawt.Application");
+			final Method getApp = appClz.getMethod("getApplication");
+			final Object app = getApp.invoke(appClz);
+			final Class<?> params[] = new Class[] { Window.class };
+			final Method reqFS = appClz.getMethod("requestToggleFullScreen", params);
+			reqFS.invoke(app, window);
+		} catch (final ClassNotFoundException e1) {
+		} catch (final Exception e) {
+			// revert mode change
+			this.fullscreen = !fullscreen;
+			setFullscreenAWT(fullscreen);
+		}
+	}
+
+	private void setFullscreenAWT(boolean fullscreen)
 	{
 		// get a reference to the device.
 		final GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 
-		if (this.fullscreen != fullscreen)
-		{ // are we actually changing modes.
-			// change modes.
-			this.fullscreen = fullscreen;
-			// toggle fullscreen mode
-			if (!fullscreen)
-			{
-				// hide the frame so we can change it.
-				window.setVisible(false);
-				// remove the frame from being displayable.
-				window.dispose();
-				// put the borders back on the frame.
-				window.setUndecorated(false);
-				// needed to unset this window as the fullscreen window.
-				device.setFullScreenWindow(null);
-				// recenter window
-				window.setLocationRelativeTo(null);
-				window.setResizable(true);
+		// change modes.
+		this.fullscreen = fullscreen;
+		// toggle fullscreen mode
+		if (!fullscreen)
+		{
+			// hide the frame so we can change it.
+			window.setVisible(false);
+			// remove the frame from being displayable.
+			window.dispose();
+			// put the borders back on the frame.
+			window.setUndecorated(false);
+			// needed to unset this window as the fullscreen window.
+			device.setFullScreenWindow(null);
+			// recenter window
+			window.setLocationRelativeTo(null);
+			window.setResizable(true);
 
-				// reset the display mode to what it was before
-				// we changed it.
-				window.setVisible(true);
-			}
-			else
-			{ // change to fullscreen.
-				// hide everything
-				window.setVisible(false);
-				// remove the frame from being displayable.
-				window.dispose();
-				// remove borders around the frame
-				window.setUndecorated(true);
-				// attempt to change the screen resolution.
-				window.setResizable(false);
-				window.setAlwaysOnTop(false);
-				// make the window fullscreen.
-				device.setFullScreenWindow(window);
-
-				if (SystemUtils.IS_JAVA_1_7 && SystemUtils.IS_OS_MAC_OSX) {
-					System.err.println("Applying first responder fix");
-					// OSX first responder bug:
-					// http://mail.openjdk.java.net/pipermail/macosx-port-dev/2012-November/005109.html
-					// unfortunately this might not be a complete fix...
-					window.setVisible(false);
-				}
-				// show the frame
-				window.setVisible(true);
-			}
-			// make sure that the screen is refreshed.
-			window.repaint();
+			// reset the display mode to what it was before
+			// we changed it.
+			window.setVisible(true);
 		}
+		else
+		{ // change to fullscreen.
+			// hide everything
+			window.setVisible(false);
+			// remove the frame from being displayable.
+			window.dispose();
+			// remove borders around the frame
+			window.setUndecorated(true);
+			// attempt to change the screen resolution.
+			window.setResizable(false);
+			window.setAlwaysOnTop(false);
+			// make the window fullscreen.
+			device.setFullScreenWindow(window);
+
+			if (SystemUtils.IS_JAVA_1_7 && SystemUtils.IS_OS_MAC_OSX) {
+				System.err.println("Applying first responder fix");
+				// OSX first responder bug:
+				// http://mail.openjdk.java.net/pipermail/macosx-port-dev/2012-November/005109.html
+				// unfortunately this might not be a complete fix...
+				window.setVisible(false);
+			}
+			// show the frame
+			window.setVisible(true);
+		}
+		// make sure that the screen is refreshed.
+		window.repaint();
 	}
 }
