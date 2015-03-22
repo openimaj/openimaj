bff62e4e98c32193afa3091b6fa261d6ddb19380
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2015-03-22 21:25:11 +0000

    playing

--- demos/sandbox/src/main/java/org/openimaj/workinprogress/BackgroundSubtractor.java
+++ demos/sandbox/src/main/java/org/openimaj/workinprogress/BackgroundSubtractor.java
@@ -36,11 +36,10 @@ import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
-import org.openimaj.image.processing.morphology.Dilate;
-import org.openimaj.image.processing.morphology.Erode;
+import org.openimaj.image.processing.algorithm.FilterSupport;
+import org.openimaj.image.processing.algorithm.MedianFilter;
 import org.openimaj.image.processing.resize.ResizeProcessor;
 import org.openimaj.video.xuggle.XuggleVideo;
-import org.openimaj.video.xuggle.XuggleVideoWriter;
 
 public class BackgroundSubtractor {
 	public static void main(String[] args) throws IOException {
@@ -49,16 +48,18 @@ public class BackgroundSubtractor {
 				ImageUtilities.readF(new File("/Users/jon/Desktop/merlin/tunnel-background.png"))
 				);
 
-		final XuggleVideoWriter xvw = new XuggleVideoWriter("/Users/jon/Desktop/merlin/tunnel-proc.mp4", bg.width,
-				bg.height, xv.getFPS());
+		// final XuggleVideoWriter xvw = new
+		// XuggleVideoWriter("/Users/jon/Desktop/merlin/tunnel-proc.mp4",
+		// bg.width,
+		// bg.height, xv.getFPS());
 		for (final MBFImage frc : xv) {
 			final FImage fr = ResizeProcessor.halfSize(frc.flatten());
 			final MBFImage diff = diff(bg, fr);
 
-			xvw.addFrame(diff);
+			// xvw.addFrame(diff);
 			DisplayUtilities.displayName(diff, "");
 		}
-		xvw.close();
+		// xvw.close();
 	}
 
 	static MBFImage diff(FImage bg, FImage fg) {
@@ -81,8 +82,10 @@ public class BackgroundSubtractor {
 			}
 		}
 
-		Dilate.dilate(df, 1);
-		Erode.erode(df, 2);
+		// Dilate.dilate(df, 1);
+		// Erode.erode(df, 2);
+		df.processInplace(new MedianFilter(FilterSupport.createBlockSupport(3, 3)));
+		df.processInplace(new MedianFilter(FilterSupport.createBlockSupport(3, 3)));
 
 		return df.toRGB();
 	}
