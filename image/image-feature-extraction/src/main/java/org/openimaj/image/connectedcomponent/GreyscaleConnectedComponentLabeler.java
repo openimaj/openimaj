33b202add79192e7d2b55b5ef45f2198e8650f2b
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2015-06-11 10:07:36 +0100

    Add missing check to ensure that already filled pixels are not refilled

--- image/image-feature-extraction/src/main/java/org/openimaj/image/connectedcomponent/GreyscaleConnectedComponentLabeler.java
+++ image/image-feature-extraction/src/main/java/org/openimaj/image/connectedcomponent/GreyscaleConnectedComponentLabeler.java
@@ -39,86 +39,97 @@ import org.openimaj.image.pixel.ConnectedComponent;
 import org.openimaj.image.pixel.Pixel;
 
 /**
- * A Connected Component Labeler for grey-level images. This class can be
- * used to transform an image that represents a map of labeled objects
- * into a list of {@link ConnectedComponent}s.  
+ * A Connected Component Labeler for grey-level images. This class can be used
+ * to transform an image that represents a map of labeled objects into a list of
+ * {@link ConnectedComponent}s.
  * <p>
- * Internally we use a flood-fill approach to finding the {@link ConnectedComponent}s.
- * 
+ * Internally we use a flood-fill approach to finding the
+ * {@link ConnectedComponent}s.
+ *
  * @author Jonathon Hare (jsh2@ecs.soton.ac.uk)
  */
 public class GreyscaleConnectedComponentLabeler implements ImageAnalyser<FImage> {
 	List<ConnectedComponent> components;
-	
+
 	/**
-	 * Syntactic sugar for calling {@link #analyseImage(FImage)} followed by 
+	 * Syntactic sugar for calling {@link #analyseImage(FImage)} followed by
 	 * {@link #getComponents()};
-	 *  
-	 * @param image the image to extract components from
+	 *
+	 * @param image
+	 *            the image to extract components from
 	 * @return the extracted components.
 	 */
 	public List<ConnectedComponent> findComponents(FImage image) {
 		analyseImage(image);
 		return components;
 	}
-	
+
 	protected ConnectedComponent floodFill(FImage image, Pixel start, int[][] output, int color) {
-		ConnectedComponent cc = new ConnectedComponent();
-//		Flood-fill (node, target-color, replacement-color):
-//			 1. Set Q to the empty queue.
-		//Queue<Pixel> queue = new LinkedList<Pixel>();
-		LinkedHashSet<Pixel> queue = new LinkedHashSet<Pixel>();
-		
-//			 2. If the color of node is not equal to target-color, return.
-		float targetColour = image.pixels[start.y][start.x];
-		
-//			 3. Add node to Q.
+		final ConnectedComponent cc = new ConnectedComponent();
+		// Flood-fill (node, target-color, replacement-color):
+		// 1. Set Q to the empty queue.
+		// Queue<Pixel> queue = new LinkedList<Pixel>();
+		final LinkedHashSet<Pixel> queue = new LinkedHashSet<Pixel>();
+
+		// 2. If the color of node is not equal to target-color, return.
+		final float targetColour = image.pixels[start.y][start.x];
+
+		// 3. Add node to Q.
 		queue.add(start);
-//			 4. For each element n of Q:
+		// 4. For each element n of Q:
 		while (queue.size() > 0) {
-			//Pixel n = queue.poll();
-			Pixel n = queue.iterator().next();
+			// Pixel n = queue.poll();
+			final Pixel n = queue.iterator().next();
 			queue.remove(n);
-			
-//			 5.  If the color of n is equal to target-color:
-			if (image.pixels[n.y][n.x] == targetColour) {
-//			 6.   Set w and e equal to n.
-				int e = n.x, w=n.x;
-//			 7.   Move w to the west until the color of the node to the west of w no longer matches target-color.
-				while (w>0 && image.pixels[n.y][w-1] == targetColour) w--;
-				
-//			 8.   Move e to the east until the color of the node to the east of e no longer matches target-color.
-				while (e<image.width-1 && image.pixels[n.y][e+1] == targetColour) e++;
-				
-//			 9.   Set the color of nodes between w and e to replacement-color.
-				for (int i=w; i<=e; i++) {
+
+			// 5. If the color of n is equal to target-color:
+			if (image.pixels[n.y][n.x] == targetColour && output[n.y][n.x] != color) {
+				// 6. Set w and e equal to n.
+				int e = n.x, w = n.x;
+				// 7. Move w to the west until the color of the node to the west
+				// of w no longer matches target-color.
+				while (w > 0 && image.pixels[n.y][w - 1] == targetColour)
+					w--;
+
+				// 8. Move e to the east until the color of the node to the east
+				// of e no longer matches target-color.
+				while (e < image.width - 1 && image.pixels[n.y][e + 1] == targetColour)
+					e++;
+
+				// 9. Set the color of nodes between w and e to
+				// replacement-color.
+				for (int i = w; i <= e; i++) {
 					output[n.y][i] = color;
 					cc.addPixel(i, n.y);
-					
-//			10.   For each node n between w and e:
-					int north = n.y - 1;
-					int south = n.y + 1;
-//			11.    If the color of the node to the north of n is target-color, add that node to Q.
-					if (north >= 0 && image.pixels[north][i] == targetColour && output[north][i] != color) queue.add(new Pixel(i, north));
-//			       If the color of the node to the south of n is target-color, add that node to Q.
-					if (south < image.height && image.pixels[south][i] == targetColour && output[south][i] != color) queue.add(new Pixel(i, south));
+
+					// 10. For each node n between w and e:
+					final int north = n.y - 1;
+					final int south = n.y + 1;
+					// 11. If the color of the node to the north of n is
+					// target-color, add that node to Q.
+					if (north >= 0 && image.pixels[north][i] == targetColour && output[north][i] != color)
+						queue.add(new Pixel(i, north));
+					// If the color of the node to the south of n is
+					// target-color, add that node to Q.
+					if (south < image.height && image.pixels[south][i] == targetColour && output[south][i] != color)
+						queue.add(new Pixel(i, south));
 				}
-//			12. Continue looping until Q is exhausted.
+				// 12. Continue looping until Q is exhausted.
 			}
 		}
-//			13. Return.
+		// 13. Return.
 		return cc;
 	}
 
 	@Override
 	public void analyseImage(FImage image) {
 		components = new ArrayList<ConnectedComponent>();
-		
-		int [][] labels = new int[image.height][image.width];
-		int nextColor=1;
-		
-		for (int y=0; y<image.height; y++) {
-			for (int x=0; x<image.width; x++) {
+
+		final int[][] labels = new int[image.height][image.width];
+		int nextColor = 1;
+
+		for (int y = 0; y < image.height; y++) {
+			for (int x = 0; x < image.width; x++) {
 				if (labels[y][x] == 0) {
 					components.add(floodFill(image, new Pixel(x, y), labels, nextColor));
 					nextColor++;
@@ -126,9 +137,10 @@ public class GreyscaleConnectedComponentLabeler implements ImageAnalyser<FImage>
 			}
 		}
 	}
-	
+
 	/**
-	 * @return the list of components found in the last call to {@link #analyseImage(FImage)}.
+	 * @return the list of components found in the last call to
+	 *         {@link #analyseImage(FImage)}.
 	 */
 	public List<ConnectedComponent> getComponents() {
 		return components;
