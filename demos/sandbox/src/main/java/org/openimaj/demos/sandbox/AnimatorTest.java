b4dde83bb1857557a494dbaaa80dab3ddea59cbe
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-10-04 18:54:36 +0100

    bump versions

--- demos/sandbox/src/main/java/org/openimaj/demos/sandbox/AnimatorTest.java
+++ demos/sandbox/src/main/java/org/openimaj/demos/sandbox/AnimatorTest.java
@@ -28,35 +28,30 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /**
- * 
+ *
  */
 package org.openimaj.demos.sandbox;
 
-import org.openimaj.content.animation.animator.LinearTimeBasedFloatValueAnimator;
-
-
 /**
- *	
+ * 
  *
- *	@author David Dupplaw (dpd@ecs.soton.ac.uk)
- *  @created 14 Aug 2012
- *	@version $Author$, $Revision$, $Date$
+ * @author David Dupplaw (dpd@ecs.soton.ac.uk)
+ * @created 14 Aug 2012
+ * @version $Author$, $Revision$, $Date$
  */
-public class AnimatorTest
-{
+public class AnimatorTest {
 	/**
-	 *	@param args
-	 *	@throws InterruptedException
+	 * @param args
+	 * @throws InterruptedException
 	 */
-	public static void main( final String[] args ) throws InterruptedException
-	{
-		final LinearTimeBasedFloatValueAnimator f = new 
-				LinearTimeBasedFloatValueAnimator( 0, 400, 2000 );
-		
-		for( int i = 0; i < 15; i++ )
-		{
-			System.out.println( f.nextValue() );
-			Thread.sleep( 200 );
-		}
+	public static void main(final String[] args) throws InterruptedException {
+		// final LinearTimeBasedFloatValueAnimator f = new
+		// LinearTimeBasedFloatValueAnimator( 0, 400, 2000 );
+		//
+		// for( int i = 0; i < 15; i++ )
+		// {
+		// System.out.println( f.nextValue() );
+		// Thread.sleep( 200 );
+		// }
 	}
 }
